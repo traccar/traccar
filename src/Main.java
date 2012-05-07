88544e0f367a5ab5a3522ef9cea1f78fda19d7da
Anton Tananaev <anton.tananaev@gmail.com>
2012-05-07 17:11:08 +0400

    Added shutdown hook

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Server service = new Server();
+        final Server service = new Server();
         service.init(args);
+
+        System.out.println("starting server...");
         service.start();
+
+        // Shutdown server properly
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("shutting down server...");
+                service.stop();
+            }
+        });
+
     }
 }
