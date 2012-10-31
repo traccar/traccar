4ad61d312d4e1c42de2269f1e70f2d556375a337
Anton Tananaev <anton.tananaev@gmail.com>
2012-10-31 20:58:54 +1300

    Fix meiligao protocol (fix #67)

--- src/org/traccar/protocol/MeiligaoProtocolDecoder.java
+++ src/org/traccar/protocol/MeiligaoProtocolDecoder.java
@@ -46,15 +46,15 @@ public class MeiligaoProtocolDecoder extends GenericProtocolDecoder {
      * Regular expressions pattern
      */
     static private Pattern pattern = Pattern.compile(
-            "([\\d]{2})([\\d]{2})([\\d]{2}).([\\d]{3})," + // Time (HHMMSS.SSS)
+            "(\\d{2})(\\d{2})(\\d{2})\\.(\\d{3})," + // Time (HHMMSS.SSS)
             "([AV])," +                         // Validity
-            "([\\d]{2})([\\d]{2}.[\\d]{4})," +  // Latitude (DDMM.MMMM)
+            "(\\d{2})(\\d{2}\\.\\d{4})," +      // Latitude (DDMM.MMMM)
             "([NS])," +
-            "([\\d]{3})([\\d]{2}.[\\d]{4})," +  // Longitude (DDDMM.MMMM)
+            "(\\d{3})(\\d{2}\\.\\d{4})," +      // Longitude (DDDMM.MMMM)
             "([EW])," +
-            "([\\d]+.[\\d]+)," +                // Speed
-            "([\\d]+.[\\d]+)?," +               // Course
-            "([\\d]{2})([\\d]{2})([\\d]{2})," + // Date (DDMMYY)
+            "(\\d+.\\d+)," +                    // Speed
+            "(\\d+\\.?\\d+)?," +                // Course
+            "(\\d{2})(\\d{2})(\\d{2})," +       // Date (DDMMYY)
             "[^\\|]+\\|(\\d+.\\d)\\|" +         // Dilution of precision
             "(\\d+)\\|" +                       // Altitude
             "([0-9a-fA-F]+)\\|" +               // State
