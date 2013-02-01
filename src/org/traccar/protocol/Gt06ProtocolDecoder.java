ff67008d5c9fb78f98c4bf821d0d8f8f064d5b5e
Anton Tananaev <anton.tananaev@gmail.com>
2013-02-01 20:19:12 +1300

    Fixed GT06 protocol

--- src/org/traccar/protocol/Gt06ProtocolDecoder.java
+++ src/org/traccar/protocol/Gt06ProtocolDecoder.java
@@ -28,16 +28,10 @@ import org.traccar.helper.Crc;
 import org.traccar.helper.Log;
 import org.traccar.model.Position;
 
-/**
- * T55 tracker protocol decoder
- */
 public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
 
     private Long deviceId;
 
-    /**
-     * Initialize
-     */
     public Gt06ProtocolDecoder(ServerManager serverManager) {
         super(serverManager);
     }
@@ -145,7 +139,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
             position.setCourse((double) (union & 0x03FF));
             position.setValid((union & 0x1000) != 0);
             if ((union & 0x0400) == 0) latitude = -latitude;
-            if ((union & 0x0800) == 0) longitude = -longitude;
+            if ((union & 0x0800) != 0) longitude = -longitude;
 
             position.setLatitude(latitude);
             position.setLongitude(longitude);
