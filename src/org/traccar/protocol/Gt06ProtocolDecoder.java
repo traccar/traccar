0070e2750587d3ef27f823b37b35d2582a79394c
Anton Tananaev <anton.tananaev@gmail.com>
2014-09-26 22:11:17 +1200

    Fix timezone parameter (fix #881)

--- src/org/traccar/protocol/Gt06ProtocolDecoder.java
+++ src/org/traccar/protocol/Gt06ProtocolDecoder.java
@@ -39,7 +39,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
         
         if (serverManager != null) {
             Properties p = getServerManager().getProperties();
-            if (p.contains("gt06.timezone")) {
+            if (p.containsKey("gt06.timezone")) {
                 timeZone.setRawOffset(
                         Integer.valueOf(p.getProperty("gt06.timezone")) * 1000);
             }
@@ -130,7 +130,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
             
         }
 
-        else if (deviceId != null && (
+        else if (/*deviceId != null && */(
                  type == MSG_GPS ||
                  type == MSG_GPS_LBS ||
                  type == MSG_GPS_LBS_STATUS ||
