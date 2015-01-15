e6762fdb368ed639e1726c5f423512b38d397580
Anton Tananaev <anton.tananaev@gmail.com>
2015-01-15 13:42:58 +1300

    Fix xexun server init method

--- src/org/traccar/ServerManager.java
+++ src/org/traccar/ServerManager.java
@@ -324,7 +324,7 @@ public class ServerManager {
             serverList.add(new TrackerServer(this, new ServerBootstrap(), protocol) {
                 @Override
                 protected void addSpecificHandlers(ChannelPipeline pipeline) {
-                    if (Boolean.valueOf(properties.containsKey(protocol + ".extended"))) {
+                    if (Boolean.valueOf(properties.getProperty(protocol + ".extended"))) {
                         pipeline.addLast("frameDecoder", new LineBasedFrameDecoder(1024)); // tracker bug \n\r
                         pipeline.addLast("stringDecoder", new StringDecoder());
                         pipeline.addLast("objectDecoder", new Xexun2ProtocolDecoder(dataManager, protocol, properties));
