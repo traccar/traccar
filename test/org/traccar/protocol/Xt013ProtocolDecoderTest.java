08e6d78e4f623b790f65cd4147d55dd7c7a4c6c2
Anton Tananaev <anton.tananaev@gmail.com>
2015-02-01 11:04:49 +1300

    Add XT013 test case

--- test/org/traccar/protocol/Xt013ProtocolDecoderTest.java
+++ test/org/traccar/protocol/Xt013ProtocolDecoderTest.java
@@ -13,6 +13,9 @@ public class Xt013ProtocolDecoderTest {
         Xt013ProtocolDecoder decoder = new Xt013ProtocolDecoder(new TestDataManager(), null, null);
         
         verify(decoder.decode(null, null,
+                "TK,862950021650364,150131090859,+53.267863,+5.767363,0,38,12,0,F,204,08,C94,336C,24,,4.09,1,,,,,,,,"));
+        
+        verify(decoder.decode(null, null,
                 "TK,862950021650364,150118113832,+53.267722,+5.767143,0,86,12,0,F,204,08,C94,336C,22,,4.21,1,,,,,,,,"));
         
         verify(decoder.decode(null, null,
