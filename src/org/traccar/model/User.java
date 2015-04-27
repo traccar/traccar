c727f09de176f569e67550964c65a63d4dcdea6b
Anton Tananaev <anton.tananaev@gmail.com>
2015-04-27 12:02:14 +1200

    Add user model class

---
+++ src/org/traccar/model/User.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.model;
+
+public class User {
+
+    private long id;
+    
+    private String email;
+    
+    private String password;
+    
+    private boolean readonly;
+    
+    private boolean admin;
+    
+    private String map;
+    
+    private String language;
+    
+    private String distance_unit;
+    
+    private String speed_unit;
+    
+    private double latitude;
+    
+    private double longitude;
+    
+    private int zoom;
+    
+}
