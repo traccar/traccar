3979daaa4278ec535fdb4dbfb46288435a8b0e4d
Anton Tananaev <anton.tananaev@gmail.com>
2015-05-23 14:53:11 +1200

    Add Aplicom unit test

--- test/org/traccar/protocol/AplicomProtocolDecoderTest.java
+++ test/org/traccar/protocol/AplicomProtocolDecoderTest.java
@@ -1,9 +1,9 @@
 package org.traccar.protocol;
 
 import org.jboss.netty.buffer.ChannelBuffers;
-import static org.traccar.helper.DecoderVerifier.verify;
 import org.junit.Test;
 import org.traccar.helper.ChannelBufferTools;
+import static org.traccar.helper.DecoderVerifier.verify;
 
 public class AplicomProtocolDecoderTest extends ProtocolDecoderTest {
 
@@ -11,6 +11,9 @@ public class AplicomProtocolDecoderTest extends ProtocolDecoderTest {
     public void testDecode() throws Exception {
 
         AplicomProtocolDecoder decoder = new AplicomProtocolDecoder(null);
+        
+        verify(decoder.decode(null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
+                "44c20144563508385a009500b09f7700c0555ea99e555ea9b103bb569f01883ff50b00002a30f000000000000013074349460108010007555ea99e000000000000003f0000ae017605b3ff00000000010000006700d900d500000003000000000000006700d900d500000087002500c4ff0000435020150000000040512001000000000000020d0000030d0000040c0000040d0000050c0000050d0000058c0000060c"))));
 
         verify(decoder.decode(null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
                 "44C20146B710C158DA009500B09F7700C054CA0EA454CA0EA403BE0BF6015D706B070000142A600000000000000002434946010801000754CA0EA4000000000000008400000000000000000000000000000000300000FE00FE0000000000000000000000000000000000000000000000000000000000000000000040502035000000000000020D0000030D0000040C0000040D0000050C0000050D0000058C0000060C"))));
