d8e21f956d2ef8ba0c2fc363209f8f2327f9cce3
Anton Tananaev <anton.tananaev@gmail.com>
2015-05-24 20:11:11 +1200

    Add Ruptela unit test

--- test/org/traccar/protocol/RuptelaProtocolDecoderTest.java
+++ test/org/traccar/protocol/RuptelaProtocolDecoderTest.java
@@ -1,10 +1,9 @@
 package org.traccar.protocol;
 
-import org.traccar.helper.TestDataManager;
 import org.jboss.netty.buffer.ChannelBuffers;
-import static org.traccar.helper.DecoderVerifier.verify;
 import org.junit.Test;
 import org.traccar.helper.ChannelBufferTools;
+import static org.traccar.helper.DecoderVerifier.verify;
 
 public class RuptelaProtocolDecoderTest extends ProtocolDecoderTest {
 
@@ -12,6 +11,9 @@ public class RuptelaProtocolDecoderTest extends ProtocolDecoderTest {
     public void testDecode() throws Exception {
 
         RuptelaProtocolDecoder decoder = new RuptelaProtocolDecoder(null);
+        
+        verify(decoder.decode(null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
+                "00a10003116e7438a7a5010002553dddbe000020fddaff0f12289b007200000600000c070805011b18cf00020003001c01201dad01041d32d81e0d7d160000c50000047200000000d000000000410000b1ae960000a5a300553dddd4000020fdd96f0f122bfe005c16f80700050b090805011b18cf00020003001c01201ead01041d338a1e0d8d160000c50000047200000000d000000000410000b1bd960000a5a3001681"))));
 
         verify(decoder.decode(null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
                 "007900000b1a2a5585c30100024e9c036900000f101733208ff45e07b31b570a001009090605011b1a020003001c01ad01021d338e16000002960000601a41014bc16d004e9c038400000f104fdf20900d20075103b00a001308090605011b1a020003001c01ad01021d33b116000002960000601a41014bc1ea0028f9"))));
