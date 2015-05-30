2aee0b69909be1bf850f70df6f2ee500915edac1
Anton Tananaev <anton.tananaev@gmail.com>
2015-05-30 20:09:19 +1200

    Add Tytan test case

--- test/org/traccar/protocol/TytanProtocolDecoderTest.java
+++ test/org/traccar/protocol/TytanProtocolDecoderTest.java
@@ -1,7 +1,6 @@
 package org.traccar.protocol;
 
 import org.jboss.netty.buffer.ChannelBuffers;
-import static org.junit.Assert.assertNull;
 import org.junit.Test;
 import org.traccar.helper.ChannelBufferTools;
 import static org.traccar.helper.DecoderVerifier.verify;
@@ -12,6 +11,9 @@ public class TytanProtocolDecoderTest extends ProtocolDecoderTest {
     public void testDecode() throws Exception {
 
         TytanProtocolDecoder decoder = new TytanProtocolDecoder(null);
+        
+        verify(decoder.decode(null, null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
+                "d5300000689d65556877fbd437a09d8ea89360005a23124e410201213704000907000009ffffffffffffffff0affffffffffffffff18ff63ff7f7fff65ff66ff67ff68ff69ff6b00ff6cffffffff6dff7fffffffff81ffffffff82ffff83ffffffffffffffff88ffff9600"))));
         
         verify(decoder.decode(null, null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
                 "d7700000689d205544713bd3378f2c8e9fe181005affdf9041020000000602a1b7563b0700002055447159d3378f2d8e9fe281005affdf9041020000000602a1b7563b0700002055447177d3378f2e8e9fe181005a861a9141020000000602a1b7563b0700002055447195f3378f2d8e9fe281005a1ce78d41020000000602a1b7563b07000020554471b3d3378f2b8e9fe281005a58d88b41020000000602a1b7563b07000020554471d1f3378f2b8e9fe281005a58d88b41020000000602a1b7563b07000020554471efd3378f2a8e9fe381005a94ac8d41020000000602a1b7563b070000205544720df3378f2c8e9fe281005a1ce78d41020000000602a1b7563b070000205544722bd3378f2d8e9fe281005a48638b41020000000602a1b7563b0700002055447249f3378f308e9fe181005adf128c41020000000602a1b7563b070000"))));
