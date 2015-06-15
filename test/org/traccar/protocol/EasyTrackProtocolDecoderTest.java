8ff799f9d16715259131cd535f7f918823f161f9
Anton Tananaev <anton.tananaev@gmail.com>
2015-06-16 09:32:55 +1200

    Add Easy Track unit tests

--- test/org/traccar/protocol/EasyTrackProtocolDecoderTest.java
+++ test/org/traccar/protocol/EasyTrackProtocolDecoderTest.java
@@ -18,16 +18,22 @@ public class EasyTrackProtocolDecoderTest extends ProtocolDecoderTest {
                 "*ET,135790246811221,DW,A,0A090D,101C0D,00CF27C6,0413FA4E,0000,0000,00000000,20,4,0000,00F123"));
 
         verify(decoder.decode(null, null,
+                "*ET,135790246811221,DW,A,050915,0C2A27,00CE5954,04132263,0000,0000,01000000,20,4,0000,001254"));
+
+        verify(decoder.decode(null, null,
                 "*ET,135790246811221,DW,A,0A090D,101C0D,00CF27C6,0413FA4E,0000,0000,00000000,20,4,0000,00F123,100"));
 
         verify(decoder.decode(null, null,
                 "*ET,135790246811221,DW,A,0A090D,101C0D,00CF27C6,8413FA4E,0000,0000,00000000,20,4,0000,00F123,100"));
-        
+
         verify(decoder.decode(null, null,
                 "*ET,358155100003016,HB,A,0d081e,07381e,8038ee09,03d2e9be,004f,0000,40c00000,0f,100,0000,00037c,29"));
         
         verify(decoder.decode(null, null,
                 "*ET,358155100003016,HB,A,0d081e,073900,8038ee2f,03d2e9fd,0114,0000,40c00000,12,100,0000,00037c,32"));
+
+        verify(decoder.decode(null, null,
+                "*ET,135790246811221,HB,A,050915,0C2A27,00CE5954,04132263,0000,0000,01000000,20,4,0000,00F123,100,200"));
 
     }
 
