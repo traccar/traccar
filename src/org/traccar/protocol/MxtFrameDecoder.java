f47ab14095137689e1c9040ab5add2f6e3174023
Anton Tananaev <anton.tananaev@gmail.com>
2015-06-26 11:14:32 +1200

    Change buffer byte order

--- src/org/traccar/protocol/MxtFrameDecoder.java
+++ src/org/traccar/protocol/MxtFrameDecoder.java
@@ -21,6 +21,8 @@ import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.handler.codec.frame.FrameDecoder;
 
+import java.nio.ByteOrder;
+
 public class MxtFrameDecoder extends FrameDecoder {
 
     @Override
@@ -35,7 +37,7 @@ public class MxtFrameDecoder extends FrameDecoder {
 
         int index = buf.indexOf(buf.readerIndex() + 1, buf.writerIndex(), (byte) 0x04);
         if (index != -1) {
-            ChannelBuffer result = ChannelBuffers.buffer(index + 1 - buf.readerIndex());
+            ChannelBuffer result = ChannelBuffers.buffer(ByteOrder.LITTLE_ENDIAN, index + 1 - buf.readerIndex());
 
             while (buf.readerIndex() <= index) {
                 int b = buf.readUnsignedByte();
@@ -45,6 +47,8 @@ public class MxtFrameDecoder extends FrameDecoder {
                     result.writeByte(b);
                 }
             }
+
+            String s = ChannelBuffers.hexDump(result);
 
             return result;
         }
