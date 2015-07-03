ccd93309bdcd282abb14e68b462c97960ccf2f96
Anton Tananaev <anton.tananaev@gmail.com>
2015-07-03 23:26:42 +1200

    Add Aplicom unit test casex

--- test/org/traccar/protocol/AplicomProtocolDecoderTest.java
+++ test/org/traccar/protocol/AplicomProtocolDecoderTest.java
@@ -13,6 +13,9 @@ public class AplicomProtocolDecoderTest extends ProtocolDecoderTest {
         AplicomProtocolDecoder decoder = new AplicomProtocolDecoder(new AplicomProtocol());
         
         verify(decoder.decode(null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
+                "44c3014645e8e9d29a002d0022ff6d00f455893b4d55893b4c027a7e1500189d710800009e0000000000000000000000023300000000000000009d"))));
+        
+        verify(decoder.decode(null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
                 "44C20146B710C158DA002100B09F0700C054CA0EA254CA0E9C03BE0BF6015D7069070000142A600000000000000001"))));
 
         verify(decoder.decode(null, null, ChannelBuffers.wrappedBuffer(ChannelBufferTools.convertHexString(
