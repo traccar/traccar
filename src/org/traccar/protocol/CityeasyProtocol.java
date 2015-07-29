c534819d12b984e845f41a95efa3f0ef7b6ebbe6
Anton Tananaev <anton.tananaev@gmail.com>
2015-07-29 15:25:14 +1200

    Fix Cityeasy frame decoder

--- src/org/traccar/protocol/CityeasyProtocol.java
+++ src/org/traccar/protocol/CityeasyProtocol.java
@@ -34,7 +34,7 @@ public class CityeasyProtocol extends BaseProtocol {
         serverList.add(new TrackerServer(new ServerBootstrap(), this.getName()) {
             @Override
             protected void addSpecificHandlers(ChannelPipeline pipeline) {
-                pipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(1024, 2, 2, -2, 0));
+                pipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(1024, 2, 2, -4, 0));
                 pipeline.addLast("objectDecoder", new CityeasyProtocolDecoder(CityeasyProtocol.this));
             }
         });
