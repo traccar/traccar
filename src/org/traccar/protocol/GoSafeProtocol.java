348af61e012cd645a253b54a3938fbcaef205dba
Anton Tananaev <anton.tananaev@gmail.com>
2015-08-04 09:35:29 +1200

    Fix GoSafe channel pipeline

--- src/org/traccar/protocol/GoSafeProtocol.java
+++ src/org/traccar/protocol/GoSafeProtocol.java
@@ -19,6 +19,7 @@ import java.util.List;
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.jboss.netty.handler.codec.string.StringEncoder;
 import org.traccar.BaseProtocol;
 import org.traccar.CharacterDelimiterFrameDecoder;
 import org.traccar.TrackerServer;
@@ -36,7 +37,7 @@ public class GoSafeProtocol extends BaseProtocol {
             protected void addSpecificHandlers(ChannelPipeline pipeline) {
                 pipeline.addLast("frameDecoder", new CharacterDelimiterFrameDecoder(1024, '#'));
                 pipeline.addLast("stringDecoder", new StringDecoder());
-                pipeline.addLast("stringEncoder", new StringDecoder());
+                pipeline.addLast("stringEncoder", new StringEncoder());
                 pipeline.addLast("objectDecoder", new GoSafeProtocolDecoder(GoSafeProtocol.this));
             }
         });
