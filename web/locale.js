a1f3f689e0397050af67c5eaf1279af226da5fa2
MRokas <rokasmuningis@gmail.com>
2015-08-25 14:58:58 +0300

    Updated locale.js
    Added "Lietuvių" l10n

--- web/locale.js
+++ web/locale.js
@@ -29,7 +29,8 @@ var availableLanguages = {
     'pt': { name: 'Português', code: 'pt' },
     'ru': { name: 'Русский', code: 'ru' },
     'th': { name: 'ไทย', code: 'th' },
-    'zh': { name: '中文', code: 'zh_CN' }
+    'zh': { name: '中文', code: 'zh_CN' },
+    'lt': { name: 'Lietuvių', code: 'lt' }
 };
 
 var language = Ext.Object.fromQueryString(window.location.search.substring(1)).locale;
