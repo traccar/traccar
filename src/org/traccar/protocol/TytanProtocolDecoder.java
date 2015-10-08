8e555ab17b231d93ab51713d6cb25510c0f92476
Anton Tananaev <anton.tananaev@gmail.com>
2015-10-09 10:11:28 +1300

    Fix minor issues in Tytan decoder

--- src/org/traccar/protocol/TytanProtocolDecoder.java
+++ src/org/traccar/protocol/TytanProtocolDecoder.java
@@ -115,7 +115,7 @@ public class TytanProtocolDecoder extends BaseProtocolDecoder {
 
                 switch (type) {
                     case 2:
-                        position.set(Event.KEY_ODOMETER, buf.readUnsignedMedium() * 5);
+                        position.set(Event.KEY_ODOMETER, buf.readUnsignedMedium());
                         break;
                     case 5:
                         position.set(Event.KEY_INPUT, buf.readUnsignedByte());
@@ -179,15 +179,15 @@ public class TytanProtocolDecoder extends BaseProtocolDecoder {
                     case 107:
                         {
                             int fuel = buf.readUnsignedShort();
-                            switch (fuel >> 16) {
+                            switch (fuel >> 14) {
                                 case 1:
-                                    position.set(Event.KEY_FUEL, (buf.readUnsignedShort() & 0x3fff) * 0.4 + "%");
+                                    position.set(Event.KEY_FUEL, (fuel & 0x3fff) * 0.4 + "%");
                                     break;
                                 case 2:
-                                    position.set(Event.KEY_FUEL, (buf.readUnsignedShort() & 0x3fff) * 0.5 + "l");
+                                    position.set(Event.KEY_FUEL, (fuel & 0x3fff) * 0.5 + " l");
                                     break;
                                 case 3:
-                                    position.set(Event.KEY_FUEL, (buf.readUnsignedShort() & 0x3fff) * -0.5 + "l");
+                                    position.set(Event.KEY_FUEL, (fuel & 0x3fff) * -0.5 + " l");
                                     break;
                             }
                         }
