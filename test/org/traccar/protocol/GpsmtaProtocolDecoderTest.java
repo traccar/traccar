5550458fdc7b2a960abe8102da71f64678aedf5f
Anton Tananaev <anton.tananaev@gmail.com>
2015-10-30 00:25:53 +1300

    Add GPSMTA unit test case

--- test/org/traccar/protocol/GpsmtaProtocolDecoderTest.java
+++ test/org/traccar/protocol/GpsmtaProtocolDecoderTest.java
@@ -11,6 +11,9 @@ public class GpsmtaProtocolDecoderTest extends ProtocolDecoderTest {
         GpsmtaProtocolDecoder decoder = new GpsmtaProtocolDecoder(new GpsmtaProtocol());
 
         verifyPosition(decoder, text(
+                "864528021249771 1446116686 49.85073 24.004438 0 217 6 338 00 59 27 0 0"));
+
+        verifyPosition(decoder, text(
                 "359144048138856 1442932957 49.85064 24.003979 1 0 40 0 10 110 26 0 0"));
 
     }
