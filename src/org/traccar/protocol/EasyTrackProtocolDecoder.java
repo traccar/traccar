e7b586030222ed2bae3f9f2f3f86486aeabc7794
Anton Tananaev <anton.tananaev@gmail.com>
2015-11-04 12:01:08 +1300

    Refactor EasyTrack protocol decoder

--- src/org/traccar/protocol/EasyTrackProtocolDecoder.java
+++ src/org/traccar/protocol/EasyTrackProtocolDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Anton Tananaev (anton.tananaev@gmail.com)
+ * Copyright 2013 - 2015 Anton Tananaev (anton.tananaev@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,13 @@
 package org.traccar.protocol;
 
 import java.net.SocketAddress;
-import java.util.Calendar;
-import java.util.TimeZone;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.jboss.netty.channel.Channel;
 import org.traccar.BaseProtocolDecoder;
+import org.traccar.helper.BitUtil;
+import org.traccar.helper.DateBuilder;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
 import org.traccar.model.Event;
 import org.traccar.model.Position;
 
@@ -31,105 +32,81 @@ public class EasyTrackProtocolDecoder extends BaseProtocolDecoder {
         super(protocol);
     }
 
-    private static final Pattern PATTERN = Pattern.compile(
-            "\\*..," +                          // Manufacturer
-            "(\\d+)," +                         // IMEI
-            "([^,]{2})," +                      // Command
-            "([AV])," +                         // Validity
-            "(\\p{XDigit}{2})" +                // Year
-            "(\\p{XDigit}{2})" +                // Month
-            "(\\p{XDigit}{2})," +               // Day
-            "(\\p{XDigit}{2})" +                // Hours
-            "(\\p{XDigit}{2})" +                // Minutes
-            "(\\p{XDigit}{2})," +               // Seconds
-            "(\\p{XDigit})" +
-            "(\\p{XDigit}{7})," +               // Latitude
-            "(\\p{XDigit})" +
-            "(\\p{XDigit}{7})," +               // Longitude
-            "(\\p{XDigit}{4})," +               // Speed
-            "(\\p{XDigit}{4})," +               // Course
-            "(\\p{XDigit}{8})," +               // Status
-            "(\\p{XDigit}+)," +                 // Signal
-            "(\\d+)," +                         // Power
-            "(\\p{XDigit}{4})," +               // Oil
-            "(\\p{XDigit}+),?" +                // Odometer
-            "(\\d+)?" +                         // Altitude
-            ".*");
+    private static final Pattern PATTERN = new PatternBuilder()
+            .text("*").expression("..,")         // manufacturer
+            .number("(d+),")                     // imei
+            .expression("([^,]{2}),")            // command
+            .expression("([AV]),")               // validity
+            .number("(xx)")                      // year
+            .number("(xx)")                      // month
+            .number("(xx),")                     // day
+            .number("(xx)")                      // hours
+            .number("(xx)")                      // minutes
+            .number("(xx),")                     // seconds
+            .number("(x)")
+            .number("(x{7}),")                   // latitude
+            .number("(x)")
+            .number("(x{7}),")                   // longitude
+            .number("(x{4}),")                   // speed
+            .number("(x{4}),")                   // course
+            .number("(x{8}),")                   // status
+            .number("(x+),")                     // signal
+            .number("(d+),")                     // power
+            .number("(x{4}),")                   // oil
+            .number("(x+),?")                    // odometer
+            .number("(d+)?")                     // altitude
+            .any()
+            .compile();
 
     @Override
     protected Object decode(
-            Channel channel, SocketAddress remoteAddress, Object msg)
-            throws Exception {
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
 
-        String sentence = (String) msg;
-
-        // Parse message
-        Matcher parser = PATTERN.matcher(sentence);
+        Parser parser = new Parser(PATTERN, (String) msg);
         if (!parser.matches()) {
             return null;
         }
 
-        // Create new position
         Position position = new Position();
         position.setProtocol(getProtocolName());
 
-        Integer index = 1;
-
-        // Get device by IMEI
-        if (!identify(parser.group(index++), channel)) {
+        if (!identify(parser.next(), channel)) {
             return null;
         }
         position.setDeviceId(getDeviceId());
 
-        // Command
-        position.set("command", parser.group(index++));
+        position.set("command", parser.next());
 
-        // Validity
-        position.setValid(parser.group(index++).compareTo("A") == 0);
+        position.setValid(parser.next().equals("A"));
 
-        // Date
-        Calendar time = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        time.clear();
-        time.set(Calendar.YEAR, 2000 + Integer.parseInt(parser.group(index++), 16));
-        time.set(Calendar.MONTH, Integer.parseInt(parser.group(index++), 16) - 1);
-        time.set(Calendar.DAY_OF_MONTH, Integer.parseInt(parser.group(index++), 16));
-        time.set(Calendar.HOUR_OF_DAY, Integer.parseInt(parser.group(index++), 16));
-        time.set(Calendar.MINUTE, Integer.parseInt(parser.group(index++), 16));
-        time.set(Calendar.SECOND, Integer.parseInt(parser.group(index++), 16));
-        position.setTime(time.getTime());
+        DateBuilder dateBuilder = new DateBuilder()
+                .setDate(parser.nextInt(16), parser.nextInt(16), parser.nextInt(16))
+                .setTime(parser.nextInt(16), parser.nextInt(16), parser.nextInt(16));
+        position.setTime(dateBuilder.getDate());
 
-        // Location
-        int hemisphere = parser.group(index++).equals("8") ? -1 : 1;
-        position.setLatitude(
-                hemisphere * Integer.parseInt(parser.group(index++), 16) / 600000.0);
-
-        hemisphere = parser.group(index++).equals("8") ? -1 : 1;
-        position.setLongitude(
-                hemisphere * Integer.parseInt(parser.group(index++), 16) / 600000.0);
-
-        position.setSpeed(Integer.parseInt(parser.group(index++), 16) / 100.0);
-        position.setCourse(Integer.parseInt(parser.group(index++), 16) / 100.0);
-
-        // Status
-        position.set(Event.KEY_STATUS, parser.group(index++));
-
-        // Signal
-        position.set("signal", parser.group(index++));
-
-        // Power
-        position.set(Event.KEY_POWER, Double.parseDouble(parser.group(index++)));
-
-        // Oil
-        position.set("oil", Integer.parseInt(parser.group(index++), 16));
-
-        // Odometer
-        position.set(Event.KEY_ODOMETER, Integer.parseInt(parser.group(index++), 16));
-
-        // Altitude
-        String altitude = parser.group(index++);
-        if (altitude != null) {
-            position.setAltitude(Double.parseDouble(altitude));
+        if (BitUtil.check(parser.nextInt(16), 7)) {
+            position.setLatitude(-parser.nextInt(16) / 600000.0);
+        } else {
+            position.setLatitude(parser.nextInt(16) / 600000.0);
         }
+
+        if (BitUtil.check(parser.nextInt(16), 7)) {
+            position.setLongitude(-parser.nextInt(16) / 600000.0);
+        } else {
+            position.setLongitude(parser.nextInt(16) / 600000.0);
+        }
+
+        position.setSpeed(parser.nextInt(16) / 100.0);
+        position.setCourse(parser.nextInt(16) / 100.0);
+
+        position.set(Event.KEY_STATUS, parser.next());
+        position.set("signal", parser.next());
+        position.set(Event.KEY_POWER, parser.nextDouble());
+        position.set("oil", parser.nextInt(16));
+        position.set(Event.KEY_ODOMETER, parser.nextInt(16));
+
+        position.setAltitude(parser.nextDouble());
+
         return position;
     }
 
--- test/org/traccar/protocol/EasyTrackProtocolDecoderTest.java
+++ test/org/traccar/protocol/EasyTrackProtocolDecoderTest.java
@@ -14,7 +14,8 @@ public class EasyTrackProtocolDecoderTest extends ProtocolDecoderTest {
                 "*ET,135790246811221,GZ,0001,0005"));
 
         verifyPosition(decoder, text(
-                "*ET,135790246811221,DW,A,0A090D,101C0D,00CF27C6,0413FA4E,0000,0000,00000000,20,4,0000,00F123"));
+                "*ET,135790246811221,DW,A,0A090D,101C0D,00CF27C6,0413FA4E,0000,0000,00000000,20,4,0000,00F123"),
+                position("2010-09-13 16:28:13.000", true, 22.62689, 114.03021));
 
         verifyPosition(decoder, text(
                 "*ET,135790246811221,DW,A,050915,0C2A27,00CE5954,04132263,0000,0000,01000000,20,4,0000,001254"));
@@ -27,7 +28,7 @@ public class EasyTrackProtocolDecoderTest extends ProtocolDecoderTest {
 
         verifyPosition(decoder, text(
                 "*ET,358155100003016,HB,A,0d081e,07381e,8038ee09,03d2e9be,004f,0000,40c00000,0f,100,0000,00037c,29"));
-        
+
         verifyPosition(decoder, text(
                 "*ET,358155100003016,HB,A,0d081e,073900,8038ee2f,03d2e9fd,0114,0000,40c00000,12,100,0000,00037c,32"));
 
