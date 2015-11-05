8d178a2e33b18b2b236a305b03663c6777679c3b
Anton Tananaev <anton.tananaev@gmail.com>
2015-11-06 09:59:54 +1300

    Implement TRV communication protocol

---
+++ src/org/traccar/protocol/TrvProtocolDecoder.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2015 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import org.jboss.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.helper.DateBuilder;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.helper.UnitsConverter;
+import org.traccar.model.Event;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class TrvProtocolDecoder extends BaseProtocolDecoder {
+
+    public TrvProtocolDecoder(TrvProtocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .text("TRV")
+            .number("APdd")
+            .number("(dd)(dd)(dd)")              // date
+            .expression("([AV])")                // validity
+            .number("(dd)(dd.d+)")               // latitude
+            .expression("([NS])")
+            .number("(ddd)(dd.d+)")              // longitude
+            .expression("([EW])")
+            .number("(ddd.d)")                   // speed
+            .number("(dd)(dd)(dd)")              // time
+            .number("(ddd.dd)")                  // course
+            .number("(ddd)")                     // gsm
+            .number("(ddd)")                     // satellites
+            .number("(ddd)")                     // battery
+            .number("(d)")                       // acc
+            .number("dd")                        // arm status
+            .number("dd,")                       // working mode
+            .number("(d+),")                     // mcc
+            .number("(d+),")                     // mnc
+            .number("(d+),")                     // lac
+            .number("(d+)")                      // cell
+            .any()
+            .compile();
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        String sentence = (String) msg;
+
+        String type = sentence.substring(3, 7);
+        if (channel != null) {
+            channel.write("B" + type.substring(1)); // response
+        }
+
+        if (type.equals("AP00")) {
+            identify(sentence.substring(7), channel);
+            return null;
+        }
+
+        if (!hasDeviceId()) {
+            return null;
+        }
+
+        if (type.equals("AP01") || type.equals("AP10")) {
+
+            Parser parser = new Parser(PATTERN, sentence);
+            if (!parser.matches()) {
+                return null;
+            }
+
+            Position position = new Position();
+            position.setProtocol(getProtocolName());
+            position.setDeviceId(getDeviceId());
+
+            DateBuilder dateBuilder = new DateBuilder()
+                    .setDate(parser.nextInt(), parser.nextInt(), parser.nextInt());
+
+            position.setValid(parser.next().equals("A"));
+            position.setLatitude(parser.nextCoordinate());
+            position.setLongitude(parser.nextCoordinate());
+            position.setSpeed(UnitsConverter.knotsFromKph(parser.nextDouble()));
+
+            dateBuilder.setTime(parser.nextInt(), parser.nextInt(), parser.nextInt());
+            position.setTime(dateBuilder.getDate());
+
+            position.setCourse(parser.nextDouble());
+
+            position.set(Event.KEY_GSM, parser.nextInt());
+            position.set(Event.KEY_SATELLITES, parser.nextInt());
+            position.set(Event.KEY_BATTERY, parser.nextInt());
+
+            int acc = parser.nextInt();
+            if (acc != 0) {
+                position.set(Event.KEY_IGNITION, acc == 1);
+            }
+
+            position.set(Event.KEY_MCC, parser.nextInt());
+            position.set(Event.KEY_MNC, parser.nextInt());
+            position.set(Event.KEY_LAC, parser.nextInt());
+            position.set(Event.KEY_CELL, parser.nextInt());
+
+            return position;
+        }
+
+        return null;
+    }
+
+}
---
+++ test/org/traccar/protocol/TrvProtocolDecoderTest.java
@@ -0,0 +1,28 @@
+package org.traccar.protocol;
+
+import org.junit.Test;
+import org.traccar.ProtocolDecoderTest;
+
+public class TrvProtocolDecoderTest extends ProtocolDecoderTest {
+
+    @Test
+    public void testDecode() throws Exception {
+
+        TrvProtocolDecoder decoder = new TrvProtocolDecoder(new TrvProtocol());
+
+        verifyNothing(decoder, text(
+                "TRVAP00353456789012345"));
+
+        verifyPosition(decoder, text(
+                "TRVAP01080524A2232.9806N11404.9355E000.1061830323.8706000908000102,460,0,9520,3671"));
+
+        verifyPosition(decoder, text(
+                "TRVAP01080524A2232.9806N11404.9355E000.1061830323.8706000908000102,460,0,9520,3671"),
+                position("2008-05-24 06:18:30.000", true, 22.54968, 114.08226));
+
+        verifyPosition(decoder, text(
+                "TRVAP10080524A2232.9806N11404.9355E000.1061830323.8706000908000502,460,0,9520,3671,00,zh-cn,00"));
+
+    }
+
+}
