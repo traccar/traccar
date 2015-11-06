1261fb0bc31be9b89740f7e9799179d57118d127
Anton Tananaev <anton.tananaev@gmail.com>
2015-11-07 11:16:53 +1300

    Implement Huabao frame decoder

---
+++ test/org/traccar/protocol/HuabaoFrameDecoderTest.java
@@ -0,0 +1,20 @@
+package org.traccar.protocol;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.traccar.ProtocolDecoderTest;
+
+public class HuabaoFrameDecoderTest extends ProtocolDecoderTest {
+
+    @Test
+    public void testDecode() throws Exception {
+
+        HuabaoFrameDecoder decoder = new HuabaoFrameDecoder();
+
+        Assert.assertEquals(
+                binary("7e307e087d557e"),
+                decoder.decode(null, null, binary("7e307d02087d01557e")));
+
+    }
+
+}
