ba3cfd639270dd3115eedd29c832322b01b5b032
Anton Tananaev <anton.tananaev@gmail.com>
2016-01-02 11:36:43 +1300

    Implement NVS GNSS communication protocol

---
+++ src/org/traccar/protocol/NvsProtocolDecoder.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2016 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.helper.BitUtil;
+import org.traccar.helper.UnitsConverter;
+import org.traccar.model.Event;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NvsProtocolDecoder extends BaseProtocolDecoder {
+
+    public NvsProtocolDecoder(NvsProtocol protocol) {
+        super(protocol);
+    }
+
+    private void sendResponse(Channel channel, String response) {
+        if (channel != null) {
+            channel.write(response);
+        }
+    }
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        ChannelBuffer buf = (ChannelBuffer) msg;
+
+        buf.skipBytes(4); // marker
+        int length = buf.readUnsignedShort();
+
+        if (length == 15 + 3) {
+
+            String imei = buf.toString(buf.readerIndex(), 15, Charset.defaultCharset());
+
+            if (!identify(imei, channel, remoteAddress)) {
+                sendResponse(channel, "NO01");
+            } else {
+                sendResponse(channel, "OK");
+            }
+
+        } else if (hasDeviceId()) {
+
+            List<Position> positions = new LinkedList<>();
+
+            buf.readLong(); // imei
+            buf.readUnsignedByte(); // codec
+            int count = buf.readUnsignedByte();
+
+            for (int i = 0; i < count; i++) {
+                Position position = new Position();
+                position.setProtocol(getProtocolName());
+                position.setDeviceId(getDeviceId());
+
+                position.setTime(new Date(buf.readUnsignedInt() * 1000));
+
+                position.set("reason", buf.readUnsignedByte());
+
+                position.setLongitude(buf.readInt() / 10000000.0);
+                position.setLatitude(buf.readInt() / 10000000.0);
+                position.setAltitude(buf.readShort());
+                position.setCourse(buf.readUnsignedShort());
+
+                position.set(Event.KEY_SATELLITES, buf.readUnsignedByte());
+
+                position.setSpeed(UnitsConverter.knotsFromKph(buf.readUnsignedShort()));
+                position.setValid(buf.readUnsignedByte() != 0);
+
+                buf.readUnsignedByte(); // used systems
+
+                buf.readUnsignedByte(); // cause element id
+
+                // Read 1 byte data
+                int cnt = buf.readUnsignedByte();
+                for (int j = 0; j < cnt; j++) {
+                    position.set(Event.PREFIX_IO + buf.readUnsignedByte(), buf.readUnsignedByte());
+                }
+
+                // Read 2 byte data
+                cnt = buf.readUnsignedByte();
+                for (int j = 0; j < cnt; j++) {
+                    position.set(Event.PREFIX_IO + buf.readUnsignedByte(), buf.readUnsignedShort());
+                }
+
+                // Read 4 byte data
+                cnt = buf.readUnsignedByte();
+                for (int j = 0; j < cnt; j++) {
+                    position.set(Event.PREFIX_IO + buf.readUnsignedByte(), buf.readUnsignedInt());
+                }
+
+                // Read 8 byte data
+                cnt = buf.readUnsignedByte();
+                for (int j = 0; j < cnt; j++) {
+                    position.set(Event.PREFIX_IO + buf.readUnsignedByte(), buf.readLong());
+                }
+
+                positions.add(position);
+            }
+
+            return positions;
+
+        }
+
+        return null;
+    }
+
+}
---
+++ test/org/traccar/protocol/NvsProtocolDecoderTest.java
@@ -0,0 +1,21 @@
+package org.traccar.protocol;
+
+import org.junit.Test;
+import org.traccar.ProtocolTest;
+
+public class NvsProtocolDecoderTest extends ProtocolTest {
+
+    @Test
+    public void testDecode() throws Exception {
+
+        NvsProtocolDecoder decoder = new NvsProtocolDecoder(new NvsProtocol());
+
+        verifyNothing(decoder, binary(
+                "cccccccc0012313233343536373839303132333435312E310000"));
+
+        verifyPositions(decoder, binary(
+                "CCCCCCCC00FE00007048860DDF79020446a6f1ce010f14f650209cca80006f00d6040004010300030101150316030001460000015d0046a6f1dc0d0f14ffe0209cc580006e00c7050001010300030101150316010001460000015e0046a6f1ea0e0f150f00209cd20000950108040000010300030101150016030001460000015d0046a6f1ff0b0f150a50209cccc000930068040000010300030101150016030001460000015b006123"));
+
+    }
+
+}
