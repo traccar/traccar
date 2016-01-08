0d021b7125d59ab6c6a826ed199c95596ca736f5
Anton Tananaev <anton.tananaev@gmail.com>
2016-01-09 10:02:35 +1300

    Fix unit test for T800x decoder

--- test/org/traccar/protocol/T800xProtocolDecoderTest.java
+++ test/org/traccar/protocol/T800xProtocolDecoderTest.java
@@ -16,7 +16,7 @@ public class T800xProtocolDecoderTest extends ProtocolTest {
         verifyNothing(decoder, binary(
                 "232303000f00000880316890202968"));
 
-        verifyPosition(decoder, binary(
+        verifyAttributes(decoder, binary(
                 "232302004200000880316890202968001e02582d00000000000000050000320000018901920000001dc1e2001601081154255d0202005a0053875a00a57e5a00af80"));
 
         verifyNothing(decoder, binary(
