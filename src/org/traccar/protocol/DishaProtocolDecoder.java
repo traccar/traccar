8c7173a6e6ae0b81d96009b86cb88460daab2f4e
Anton Tananaev <anton.tananaev@gmail.com>
2016-01-11 09:39:05 +1300

    Fix Disha coordinates decoding issue

--- src/org/traccar/protocol/DishaProtocolDecoder.java
+++ src/org/traccar/protocol/DishaProtocolDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Anton Tananaev (anton.tananaev@gmail.com)
+ * Copyright 2015 - 2016 Anton Tananaev (anton.tananaev@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,7 +82,7 @@ public class DishaProtocolDecoder extends BaseProtocolDecoder {
         position.setTime(dateBuilder.getDate());
 
         position.setLatitude(parser.nextCoordinate());
-        position.setLatitude(parser.nextCoordinate());
+        position.setLongitude(parser.nextCoordinate());
 
         position.setSpeed(parser.nextDouble());
         position.setCourse(parser.nextDouble());
