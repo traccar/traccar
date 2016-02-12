5a4562f8e53e4e00a610c9715786f70a743067b5
Hans van den Elsen <hans.elsen@esds.nl>
2016-02-12 13:22:16 +0100

    Reverted customizations for own API

--- src/org/traccar/web/WebServer.java
+++ src/org/traccar/web/WebServer.java
@@ -40,7 +40,13 @@ import org.traccar.api.CorsResponseFilter;
 import org.traccar.api.ObjectMapperProvider;
 import org.traccar.api.ResourceErrorHandler;
 import org.traccar.api.SecurityRequestFilter;
-import org.traccar.api.resource.*;
+import org.traccar.api.resource.CommandResource;
+import org.traccar.api.resource.DeviceResource;
+import org.traccar.api.resource.PermissionResource;
+import org.traccar.api.resource.PositionResource;
+import org.traccar.api.resource.ServerResource;
+import org.traccar.api.resource.SessionResource;
+import org.traccar.api.resource.UserResource;
 import org.traccar.helper.Log;
 
 public class WebServer {
