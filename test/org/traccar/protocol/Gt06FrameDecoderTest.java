6ea8778be3d7426fa9281f9260081e1863fae4bd
Anton Tananaev <anton.tananaev@gmail.com>
2016-02-17 22:10:52 +1100

    Add GT06 frame decoder tests

--- test/org/traccar/protocol/Gt06FrameDecoderTest.java
+++ test/org/traccar/protocol/Gt06FrameDecoderTest.java
@@ -12,6 +12,18 @@ public class Gt06FrameDecoderTest extends ProtocolTest {
         Gt06FrameDecoder decoder = new Gt06FrameDecoder();
 
         Assert.assertEquals(
+                binary("78781f1210020e140613cc04770690003e3f2e3414b20000000000000000044c446a0d0a"),
+                decoder.decode(null, null, binary("78781f1210020e140613cc04770690003e3f2e3414b20000000000000000044c446a0d0a")));
+
+        Assert.assertNotEquals(
+                binary("787808134606020002044dc5050d0a"),
+                decoder.decode(null, null, binary("787808134606020002044dc5050d0a")));
+
+        Assert.assertEquals(
+                binary("78781f1210020e14061dcc0476fcd0003e3faf3e14b20000000000000000044ef6740d0a"),
+                decoder.decode(null, null, binary("78781f1210020e14061dcc0476fcd0003e3faf3e14b20000000000000000044ef6740d0a")));
+
+        Assert.assertEquals(
                 binary("78780d010352887071911998000479d00d0a"),
                 decoder.decode(null, null, binary("78780d010352887071911998000479d00d0a")));
 
