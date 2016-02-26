6710bc0d1f0667ea2a3e9e3c038d9412dd6195f1
Anton Tananaev <anton.tananaev@gmail.com>
2016-02-27 09:52:19 +1300

    Update Xexun decoder regex pattern

--- src/org/traccar/protocol/XexunProtocolDecoder.java
+++ src/org/traccar/protocol/XexunProtocolDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 - 2015 Anton Tananaev (anton.tananaev@gmail.com)
+ * Copyright 2012 - 2016 Anton Tananaev (anton.tananaev@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,8 +38,8 @@ public class XexunProtocolDecoder extends BaseProtocolDecoder {
             .expression("G[PN]RMC,")
             .number("(dd)(dd)(dd).(d+),")        // time
             .expression("([AV]),")               // validity
-            .number("(d+?)?(d?d.d+),([NS]),")    // latitude
-            .number("(d+?)?(d?d.d+),([EW])?,")   // longitude
+            .number("(d*?)(d?d.d+),([NS]),")     // latitude
+            .number("(d*?)(d?d.d+),([EW])?,")    // longitude
             .number("(d+.?d*),")                 // speed
             .number("(d+.?d*)?,")                // course
             .number("(dd)(dd)(dd),")             // date
