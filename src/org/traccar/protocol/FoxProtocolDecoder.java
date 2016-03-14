40bf728938311fa9619ad2bb0eb89b21c16e9585
Anton Tananaev <anton.tananaev@gmail.com>
2016-03-14 14:24:41 +1300

    Implement Fox Lite protocol

---
+++ src/org/traccar/protocol/FoxProtocolDecoder.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2016 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import org.jboss.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.helper.DateBuilder;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.helper.UnitsConverter;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class FoxProtocolDecoder extends BaseProtocolDecoder {
+
+    public FoxProtocolDecoder(FoxProtocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .number("d+,")                       // status id
+            .expression("([AV]),")               // validity
+            .number("(dd)(dd)(dd),")             // date
+            .number("(dd)(dd)(dd),")             // time
+            .number("(dd)(dd.d+),")              // latitude
+            .expression("([NS]),")
+            .number("(ddd)(dd.d+),")             // longitude
+            .expression("([EW]),")
+            .number("(d+.?d*)?,")                // speed
+            .number("(d+.?d*)?,")                // course
+            .any()
+            .compile();
+
+    private String getAttribute(String xml, String key) {
+        int start = xml.indexOf(key + "=\"");
+        if (start != -1) {
+            start += key.length() + 2;
+            int end = xml.indexOf("\"", start);
+            if (end != -1) {
+                return xml.substring(start, end);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        String xml = (String) msg;
+        String id = getAttribute(xml, "id");
+        String data = getAttribute(xml, "data");
+
+        if (id != null && data != null) {
+
+            if (!identify(id, channel, remoteAddress)) {
+                return null;
+            }
+
+            Parser parser = new Parser(PATTERN, data);
+            if (!parser.matches()) {
+                return null;
+            }
+
+            Position position = new Position();
+            position.setProtocol(getProtocolName());
+            position.setDeviceId(getDeviceId());
+
+            position.setValid(parser.next().equals("A"));
+
+            DateBuilder dateBuilder = new DateBuilder()
+                    .setDateReverse(parser.nextInt(), parser.nextInt(), parser.nextInt())
+                    .setTime(parser.nextInt(), parser.nextInt(), parser.nextInt());
+            position.setTime(dateBuilder.getDate());
+
+            position.setLatitude(parser.nextCoordinate());
+            position.setLongitude(parser.nextCoordinate());
+            position.setSpeed(UnitsConverter.knotsFromKph(parser.nextDouble()));
+            position.setCourse(parser.nextDouble());
+
+            return position;
+
+        }
+
+        return null;
+    }
+
+}
