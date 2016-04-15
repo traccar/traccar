6a827ed071b0ff48264758632afc4965f0c46150
Anton Tananaev <anton.tananaev@gmail.com>
2016-04-16 00:38:58 +1200

    Temporary workaround for store update

--- web/app/view/DevicesController.js
+++ web/app/view/DevicesController.js
@@ -30,8 +30,20 @@ Ext.define('Traccar.view.DevicesController', {
                     selectDevice: 'selectDevice',
                     selectReport: 'selectReport'
                 }
+            },
+            store: {
+                '#Groups': {
+                    update: 'reloadList'
+                },
+                '#Devices': {
+                    add: 'reloadList'
+                }
             }
         }
+    },
+
+    reloadList: function() {
+        Ext.getStore('Devices').reload();
     },
 
     init: function () {
