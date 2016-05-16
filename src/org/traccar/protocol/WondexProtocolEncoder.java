79fd341d2ee1d6ff2ce9139e84dab0ad6820a1ec
Abyss777 <abyss@fox5.ru>
2016-05-16 10:43:03 +0500

    Implemented few commands for Wondex protocol.

---
+++ src/org/traccar/protocol/WondexProtocolEncoder.java
@@ -0,0 +1,29 @@
+package org.traccar.protocol;
+
+import org.traccar.StringProtocolEncoder;
+import org.traccar.helper.Log;
+import org.traccar.model.Command;;
+
+public class WondexProtocolEncoder extends StringProtocolEncoder {
+    @Override
+    protected Object encodeCommand(Command command) {
+    	
+    	// Temporary put default password
+        command.set(Command.KEY_DEVICE_PASSWORD, "0000");
+
+        switch (command.getType()) {
+        case Command.TYPE_REBOOT_DEVICE:
+            return formatCommand(command, "$WP+REBOOT={%s}", Command.KEY_DEVICE_PASSWORD);
+        case Command.TYPE_POSITION_SINGLE:
+            return formatCommand(command, "$WP+GETLOCATION={%s}", Command.KEY_DEVICE_PASSWORD);
+        case Command.TYPE_CUSTOM:
+            return formatCommand(command, Command.KEY_DATA);
+        default:
+            Log.warning(new UnsupportedOperationException(command.getType()));
+            break;
+        }
+
+        return null;
+    }
+
+}
