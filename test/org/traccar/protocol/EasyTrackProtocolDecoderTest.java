824a01c093700929ec3d2e581fef5e252f64e129
Anton Tananaev <anton.tananaev@gmail.com>
2016-06-04 10:51:32 +1200

    Add easytrack decoder test case

--- test/org/traccar/protocol/EasyTrackProtocolDecoderTest.java
+++ test/org/traccar/protocol/EasyTrackProtocolDecoderTest.java
@@ -17,6 +17,9 @@ public class EasyTrackProtocolDecoderTest extends ProtocolTest {
                 "*ET,135790246811221,DW,A,0A090D,101C0D,00CF27C6,0413FA4E,0000,0000,00000000,20,4,0000,00F123"),
                 position("2010-09-13 16:28:13.000", true, 22.62689, 114.03021));
 
+        verifyNothing(decoder, text(
+                "*ET,358155100048430,CC,0.0,V,100603,141817,80d77ae8,81ab1ffd,0000,6b08,40000000,19,99,0000,fa9,918"));
+
         verifyPosition(decoder, text(
                 "*ET,135790246811221,DW,A,050915,0C2A27,00CE5954,04132263,0000,0000,01000000,20,4,0000,001254"));
 
