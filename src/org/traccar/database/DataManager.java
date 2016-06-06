d1f602835eff3df33261ce16620c51fb291c0763
drecchia <danilo@deltatecnologia.com>
2016-06-06 09:11:00 -0300

    - Fix hikari connection check sql.

--- src/org/traccar/database/DataManager.java
+++ src/org/traccar/database/DataManager.java
@@ -117,7 +117,7 @@ public class DataManager implements IdentityManager {
             hikariConfig.setJdbcUrl(config.getString("database.url"));
             hikariConfig.setUsername(config.getString("database.user"));
             hikariConfig.setPassword(config.getString("database.password"));
-            hikariConfig.setConnectionInitSql("SELECT 1 FROM DUAL");
+            hikariConfig.setConnectionInitSql(config.getString("database.checkConnection"));
             hikariConfig.setIdleTimeout(600000);
 
             int maxPoolSize = config.getInteger("database.maxPoolSize");
