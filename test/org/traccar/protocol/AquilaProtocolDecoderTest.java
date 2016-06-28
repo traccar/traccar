d71fe8f7c6a8e8dd86992842060ab6dbac8b12f4
Anton Tananaev <anton.tananaev@gmail.com>
2016-06-28 22:32:35 +1200

    Add a couple of Aquila test cases

--- test/org/traccar/protocol/AquilaProtocolDecoderTest.java
+++ test/org/traccar/protocol/AquilaProtocolDecoderTest.java
@@ -9,7 +9,13 @@ public class AquilaProtocolDecoderTest extends ProtocolTest {
     public void testDecode() throws Exception {
 
         AquilaProtocolDecoder decoder = new AquilaProtocolDecoder(new AquilaProtocol());
-        
+
+        verifyPosition(decoder, text(
+                "$$CLIENT_1DT,151028368,1,19.108438,72.925308,160628154920,A,22,0,0,131,3503,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,*1D"));
+
+        verifyNothing(decoder, text(
+                "$$CLIENT_1DT,160319372,1,28.549541,77.249802,160628140743,A,23,0,-65025,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,*0D"));
+
         verifyPosition(decoder, text(
                 "$$SRINI_1MS,141214807,1,12.963515,77.533844,150925161628,A,27,0,8,0,68,0,0,0,0,0,0,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,*43"));
 
