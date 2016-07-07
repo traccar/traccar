d3e0224d6f5b1d4765ce9952db19a00af59f7f83
Anton Tananaev <anton.tananaev@gmail.com>
2016-07-08 11:28:07 +1200

    Fix PT502 unit tests

--- test/org/traccar/protocol/Pt502ProtocolDecoderTest.java
+++ test/org/traccar/protocol/Pt502ProtocolDecoderTest.java
@@ -29,7 +29,7 @@ public class Pt502ProtocolDecoderTest extends ProtocolTest {
         verifyPosition(decoder, text(
                 "$POS,6095,233344.000,V,0933.0451,N,06912.3360,W,,,151112,,,N/00000,00000/0/1677600/"));
 
-        verifyPosition(decoder, text(
+        verifyNothing(decoder, text(
                 "$PHO0,6091,233606.000,A,0902.9855,N,06944.3654,W,0.0,43.8,141112,,,A/00010,00000/0/224000//"));
         
         verifyPosition(decoder, text(
