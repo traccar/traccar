5acf3e52e76c8249bb3dffb9593baf4a27d7953d
Abyss777 <abyss@fox5.ru>
2016-07-07 16:56:03 +0500

    Fix typo in GranitProtocolDecoder

--- src/org/traccar/protocol/GranitProtocolDecoder.java
+++ src/org/traccar/protocol/GranitProtocolDecoder.java
@@ -85,7 +85,7 @@ public class GranitProtocolDecoder extends BaseProtocolDecoder {
         double latitude = latDegrees + latMinutes / 60000.0;
         double longitude = lonDegrees + lonMinutes / 60000.0;
         if (!BitUtil.check(flags, 4)) {
-            latitude = -latDegrees;
+            latitude = -latitude;
         }
         if (!BitUtil.check(flags, 5)) {
             longitude = -longitude;
