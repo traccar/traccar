e1c811133ba207d2233ac7f9ef531102a91eb4eb
drecchia <danilo@deltatecnologia.com>
2016-07-13 10:43:22 -0300

    - Removed trailing space.

--- src/org/traccar/protocol/SuntechProtocolEncoder.java
+++ src/org/traccar/protocol/SuntechProtocolEncoder.java
@@ -30,7 +30,7 @@ public class SuntechProtocolEncoder extends StringProtocolEncoder {
             case Command.TYPE_POSITION_SINGLE:
                 return formatCommand(command, "SA200GTR;{%s};02;\r", Command.KEY_UNIQUE_ID);
             case Command.TYPE_OUTPUT_CONTROL:
-                return formatCommand(command, "SA200CMD;{%s};0{%s};Enable{%s}\r", 
+                return formatCommand(command, "SA200CMD;{%s};0{%s};Enable{%s}\r",
                         Command.KEY_UNIQUE_ID, Command.KEY_DATA, Command.KEY_DATA);
             case Command.TYPE_ENGINE_STOP:
                 return formatCommand(command, "SA200CMD;{%s};02;Enable1\r", Command.KEY_UNIQUE_ID);
