e9e587ab105f8741770b8313f8406a28a2e02704
Anton Tananaev <anton.tananaev@gmail.com>
2016-08-30 08:16:22 +1200

    Add missing copyright header

--- src/org/traccar/notification/EventForwarder.java
+++ src/org/traccar/notification/EventForwarder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.traccar.notification;
 
 import java.nio.charset.StandardCharsets;
@@ -32,7 +47,6 @@ public final class EventForwarder {
     private static final String KEY_DEVICE = "device";
 
     public void forwardEvent(Event event, Position position) {
-
 
         BoundRequestBuilder requestBuilder = Context.getAsyncHttpClient().preparePost(url);
 
@@ -72,4 +86,5 @@ public final class EventForwarder {
         }
         return json.build().toString().getBytes(StandardCharsets.UTF_8);
     }
+
 }
