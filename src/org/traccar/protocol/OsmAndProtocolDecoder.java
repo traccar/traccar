d8166cc1b1db9fca92b801804831dc8c20ab7415
Damien alias Dough <dough29@gmail.com>
2016-09-04 10:04:11 +0200

    Clean unused import Arrays in OsmAndProtocolDecoder

--- src/org/traccar/protocol/OsmAndProtocolDecoder.java
+++ src/org/traccar/protocol/OsmAndProtocolDecoder.java
@@ -32,7 +32,6 @@ import java.net.SocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
