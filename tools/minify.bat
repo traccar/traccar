6f71ca7e88fe0ddd94849aab2c5f8350e2f7c94a
ninioe <ninioe@gmail.com>
2016-09-20 09:52:55 +0300

    remove web & tools from main repo
    because of a licence issue

--- tools/minify.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd C:\[traccar path]\traccar\web
-set SDK=C:\[sencha path]\ext-6.0.0
-
-sencha -sdk %SDK% compile -classpath=app.js,app,%SDK%\packages\core\src,%SDK%\packages\core\overrides,%SDK%\classic\classic\src,%SDK%\classic\classic\overrides exclude -all and include -recursive -file app.js and exclude -namespace=Ext and concatenate -closure app.min.js
