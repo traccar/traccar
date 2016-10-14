974fccb9b808f2ff615f9c9e04f3b722301d2ace
Anton Tananaev <anton.tananaev@gmail.com>
2016-10-15 08:14:36 +1300

    Add u-pro unit test case

--- test/org/traccar/protocol/UproProtocolDecoderTest.java
+++ test/org/traccar/protocol/UproProtocolDecoderTest.java
@@ -10,6 +10,9 @@ public class UproProtocolDecoderTest extends ProtocolTest {
 
         UproProtocolDecoder decoder = new UproProtocolDecoder(new UproProtocol());
 
+        verifyNothing(decoder, text(
+                "*AI2000905447674,BA&A2003064913201201845107561627121016&B0100000000&C05>8=961&F0333&K023101002154A7#"));
+
         verifyPosition(decoder, text(
                 "*AI200905300036,AH&A0317264913209801844913060000251115&B0500000000&C0;4?72:9&F0000#"));
 
