45a0a7ff45edf661f6f9a78fafb63f31207fbf43
Anton Tananaev <anton.tananaev@gmail.com>
2016-10-25 04:07:22 +1300

    Fix unit test issue

--- test/org/traccar/protocol/UproProtocolDecoderTest.java
+++ test/org/traccar/protocol/UproProtocolDecoderTest.java
@@ -10,7 +10,7 @@ public class UproProtocolDecoderTest extends ProtocolTest {
 
         UproProtocolDecoder decoder = new UproProtocolDecoder(new UproProtocol());
 
-        verifyNothing(decoder, text(
+        verifyPosition(decoder, text(
                 "*AI2000905447674,BA&A2003064913201201845107561627121016&B0100000000&C05>8=961&F0333&K023101002154A7#"));
 
         verifyPosition(decoder, text(
