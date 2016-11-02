eba56dfeceee5c77eac64b7da8abc8a7e8b70415
Anton Tananaev <anton.tananaev@gmail.com>
2016-11-03 02:40:13 +1300

    Include version in server model

--- src/org/traccar/model/Server.java
+++ src/org/traccar/model/Server.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Anton Tananaev (anton@traccar.org)
+ * Copyright 2015 - 2016 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,16 @@
  */
 package org.traccar.model;
 
+import org.traccar.helper.Log;
+
 public class Server extends Extensible {
+
+    public String getVersion() {
+        return Log.getAppVersion();
+    }
+
+    public void setVersion(String version) {
+    }
 
     private boolean registration;
 
