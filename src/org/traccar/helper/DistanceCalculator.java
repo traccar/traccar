c814f3f3556422d9e7e49d6b958621058293c1ac
Abyss777 <abyss@fox5.ru>
2016-11-26 20:06:35 +0700

    Add license header

--- src/org/traccar/helper/DistanceCalculator.java
+++ src/org/traccar/helper/DistanceCalculator.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2014 Anton Tananaev (anton@traccar.org)
+ * Copyright 2014 - 2016 Anton Tananaev (anton@traccar.org)
+ * Copyright 2016 Andrey Kunitsyn (andrey@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
