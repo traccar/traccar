7844532ac883784c50ef7979a57d87ce35045006
Anton Tananaev <anton.tananaev@gmail.com>
2017-01-13 03:18:43 +1300

    Disable LBS for H02 GPS message

--- src/org/traccar/protocol/H02ProtocolDecoder.java
+++ src/org/traccar/protocol/H02ProtocolDecoder.java
@@ -244,16 +244,6 @@ public class H02ProtocolDecoder extends BaseProtocolDecoder {
 
         processStatus(position, parser.nextLong(16));
 
-        if (parser.hasNext(4)) {
-            int mcc = parser.nextInt(16);
-            int mnc = parser.nextInt(16);
-            int lac = parser.nextInt(16);
-            int cid = parser.nextInt(16);
-            if (mcc != 0 && mnc != 0 && lac != 0 && cid != 0) {
-                position.setNetwork(new Network(CellTower.from(mcc, mnc, lac, cid)));
-            }
-        }
-
         return position;
     }
 
@@ -304,20 +294,20 @@ public class H02ProtocolDecoder extends BaseProtocolDecoder {
         ChannelBuffer buf = (ChannelBuffer) msg;
         String marker = buf.toString(0, 1, StandardCharsets.US_ASCII);
 
-        if (marker.equals("*")) {
-            String sentence = buf.toString(StandardCharsets.US_ASCII);
-            if (sentence.contains(",NBR,")) {
-                return decodeLbs(sentence, channel, remoteAddress);
-            } else {
-                return decodeText(sentence, channel, remoteAddress);
-            }
-        } else if (marker.equals("$")) {
-            return decodeBinary(buf, channel, remoteAddress);
-        } else if (marker.equals("X")) {
-            return null; // X mode
+        switch (marker) {
+            case "*":
+                String sentence = buf.toString(StandardCharsets.US_ASCII);
+                if (sentence.contains(",NBR,")) {
+                    return decodeLbs(sentence, channel, remoteAddress);
+                } else {
+                    return decodeText(sentence, channel, remoteAddress);
+                }
+            case "$":
+                return decodeBinary(buf, channel, remoteAddress);
+            case "X":
+            default:
+                return null;
         }
-
-        return null;
     }
 
 }
