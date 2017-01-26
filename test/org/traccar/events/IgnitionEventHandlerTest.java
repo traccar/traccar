d0cb885b152d5db3913936c8f1b57e690a47a067
Abyss777 <abyss@fox5.ru>
2017-01-26 10:31:35 +0500

    Fix ignition test

--- test/org/traccar/events/IgnitionEventHandlerTest.java
+++ test/org/traccar/events/IgnitionEventHandlerTest.java
@@ -1,7 +1,6 @@
 package org.traccar.events;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.util.Collection;
 
@@ -21,9 +20,7 @@ public class IgnitionEventHandlerTest extends BaseTest {
         position.set(Position.KEY_IGNITION, true);
         position.setValid(true);
         Collection<Event> events = ignitionEventHandler.analyzePosition(position);
-        assertNotNull(events);
-        Event event = (Event) events.toArray()[0];
-        assertEquals(Event.TYPE_IGNITION_ON, event.getType());
+        assertEquals(events, null);
     }
 
 }
