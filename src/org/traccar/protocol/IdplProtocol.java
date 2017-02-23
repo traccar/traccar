981ab9d051d0b84cdc256fa48883f2c214e8bc3c
Anton Tananaev <anton.tananaev@gmail.com>
2017-02-23 22:57:36 +1300

    Fix protocol name typo

--- src/org/traccar/protocol/IdplProtocol.java
+++ src/org/traccar/protocol/IdplProtocol.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Anton Tananaev (anton@traccar.org)
+ * Copyright 2016 - 2017 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import org.traccar.TrackerServer;
 public class IdplProtocol extends BaseProtocol {
 
     public IdplProtocol() {
-        super("ldpl");
+        super("idpl");
     }
 
     @Override
