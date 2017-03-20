9ba0e8f826c9edb78beb234192c694b881a4418a
Anton Tananaev <anton.tananaev@gmail.com>
2017-03-21 02:45:07 +1300

    Fix unit tests

--- test/org/traccar/protocol/Gl200ProtocolDecoderTest.java
+++ test/org/traccar/protocol/Gl200ProtocolDecoderTest.java
@@ -10,7 +10,7 @@ public class Gl200ProtocolDecoderTest extends ProtocolTest {
 
         Gl200ProtocolDecoder decoder = new Gl200ProtocolDecoder(new Gl200Protocol());
 
-        verifyAttributes(decoder, text(
+        verifyPositions(decoder, text(
                 "+RESP:GTFRI,210102,354524044925825,,1,1,1,29,2.8,0,133.7,-90.203063,32.265473,20170318005208,,,,,10800,4,20170318005208,0002$"));
 
         verifyAttributes(decoder, text(
