8ab5a5a461ed35cd938aa59bb4070e82fbd9b93e
Anton Tananaev <anton.tananaev@gmail.com>
2017-03-21 19:38:05 +1300

    Fix Ruptela unit tests

--- test/org/traccar/protocol/RuptelaProtocolDecoderTest.java
+++ test/org/traccar/protocol/RuptelaProtocolDecoderTest.java
@@ -10,7 +10,7 @@ public class RuptelaProtocolDecoderTest extends ProtocolTest {
 
         RuptelaProtocolDecoder decoder = new RuptelaProtocolDecoder(new RuptelaProtocol());
 
-        verifyPosition(decoder, binary(
+        verifyAttributes(decoder, binary(
                 "0011000315A07F440B1D07534554494f20636f6e66696775726174696f6e2064617461206f6b341C"));
 
         verifyPositions(decoder, binary(
