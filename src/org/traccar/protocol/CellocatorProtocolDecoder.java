1a0ee4171d4f3dbddcbffc65f254fae2bd4632e0
Anton Tananaev <anton.tananaev@gmail.com>
2017-04-06 01:41:54 +1200

    Fix Cellocator UDP acknowledgement

--- src/org/traccar/protocol/CellocatorProtocolDecoder.java
+++ src/org/traccar/protocol/CellocatorProtocolDecoder.java
@@ -43,7 +43,7 @@ public class CellocatorProtocolDecoder extends BaseProtocolDecoder {
 
     private byte commandCount;
 
-    private void sendReply(Channel channel, long deviceId, byte packetNumber) {
+    private void sendReply(Channel channel, SocketAddress remoteAddress, long deviceId, byte packetNumber) {
         ChannelBuffer reply = ChannelBuffers.directBuffer(ByteOrder.LITTLE_ENDIAN, 28);
         reply.writeByte('M');
         reply.writeByte('C');
@@ -64,7 +64,7 @@ public class CellocatorProtocolDecoder extends BaseProtocolDecoder {
         reply.writeByte(checksum);
 
         if (channel != null) {
-            channel.write(reply);
+            channel.write(reply, remoteAddress);
         }
     }
 
@@ -96,7 +96,7 @@ public class CellocatorProtocolDecoder extends BaseProtocolDecoder {
         }
         byte packetNumber = buf.readByte();
 
-        sendReply(channel, deviceUniqueId, packetNumber);
+        sendReply(channel, remoteAddress, deviceUniqueId, packetNumber);
 
         if (type == MSG_CLIENT_STATUS) {
 
