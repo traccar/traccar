09c18dc8f3015258a09cf488dde25f12eb224ea8
Anton Tananaev <anton.tananaev@gmail.com>
2017-05-20 22:16:35 +1200

    Fix DMT HTTP coordinates decoding

--- src/org/traccar/protocol/DmtHttpProtocolDecoder.java
+++ src/org/traccar/protocol/DmtHttpProtocolDecoder.java
@@ -95,7 +95,7 @@ public class DmtHttpProtocolDecoder extends BaseProtocolDecoder {
                     case 0:
                         position.setFixTime(dateFormat.parse(field.getString("GpsUTC")));
                         position.setLatitude(field.getJsonNumber("Lat").doubleValue());
-                        position.setLatitude(field.getJsonNumber("Long").doubleValue());
+                        position.setLongitude(field.getJsonNumber("Long").doubleValue());
                         position.setAltitude(field.getInt("Alt"));
                         position.setSpeed(UnitsConverter.knotsFromCps(field.getInt("Spd")));
                         position.setCourse(field.getInt("Head"));
