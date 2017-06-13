96f37bd4206ad2f2034c026881230ca40436253d
Jan-Piet Mens <jpmens@gmail.com>
2017-06-13 22:13:03 +0200

    add unit tests

--- test/org/traccar/protocol/OwnTracksProtocolDecoderTest.java
+++ test/org/traccar/protocol/OwnTracksProtocolDecoderTest.java
@@ -1,5 +1,6 @@
 package org.traccar.protocol;
 
+import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.junit.Test;
 import org.traccar.ProtocolTest;
 
@@ -10,16 +11,11 @@ public class OwnTracksProtocolDecoderTest extends ProtocolTest {
 
         OwnTracksProtocolDecoder decoder = new OwnTracksProtocolDecoder(new OwnTracksProtocol());
 
-/*
- * I don'w know how to get JSON into that ...
- *
-        verifyNull(decoder, request(
-                "{ \"id\" : \"hello\" }"));
+	verifyPosition(decoder, request(HttpMethod.POST, "/",
+	                buffer(text("{\"lon\":2.29513,\"lat\":48.85833,\"tst\":1497349316,\"_type\":\"location\",\"tid\":\"JJ\"}"))));
 
-        verifyPosition(decoder, request(
-                "{\"cog\":271,\"lon\":2.29513,\"acc\":5,\"vel\":61,\"vac\":21,\"lat\":48.85833,\"tst\":1497349316,\"alt\":167,\"_type\":\"location\",\"tid\":\"JJ\"}"));
-
-*/
+	verifyPosition(decoder, request(HttpMethod.POST, "/",
+	                buffer(text("{\"cog\":271,\"lon\":2.29513,\"acc\":5,\"vel\":61,\"vac\":21,\"lat\":48.85833,\"tst\":1497349316,\"alt\":167,\"_type\":\"location\",\"tid\":\"JJ\",\"t\":\"u\",\"batt\":67}"))));
 
     }
 
