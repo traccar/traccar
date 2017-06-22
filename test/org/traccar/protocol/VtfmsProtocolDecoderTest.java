798e1b7bb58b82945a7a009ae8009f6d8bd00d96
Anton Tananaev <anton.tananaev@gmail.com>
2017-06-22 22:46:20 +1200

    Fix VTFMS protocol unit tests

--- test/org/traccar/protocol/VtfmsProtocolDecoderTest.java
+++ test/org/traccar/protocol/VtfmsProtocolDecoderTest.java
@@ -10,7 +10,7 @@ public class VtfmsProtocolDecoderTest extends ProtocolTest {
 
         VtfmsProtocolDecoder decoder = new VtfmsProtocolDecoder(new VtfmsProtocol());
 
-        verifyNull(decoder, text(
+        verifyPosition(decoder, text(
                 "(865733028143493,00I76,00,000,,,,,A,133755,210617,10.57354,077.24912,SW,000,00598,00000,K,0017368,1,12.7,,,0.000,,,0,0,0,0,1,1,0,,)074"));
 
         verifyPosition(decoder, text(
