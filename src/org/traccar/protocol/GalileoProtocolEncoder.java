96b48f5cb2bf214059de2875194634b0801fcc94
Anton Tananaev <anton.tananaev@gmail.com>
2017-07-11 04:29:08 +1200

    Fix Galileo command encoder

--- src/org/traccar/protocol/GalileoProtocolEncoder.java
+++ src/org/traccar/protocol/GalileoProtocolEncoder.java
@@ -57,9 +57,9 @@ public class GalileoProtocolEncoder extends BaseProtocolEncoder {
 
         switch (command.getType()) {
             case Command.TYPE_CUSTOM:
-                return encodeText(getUniqueId(command.getId()), command.getString(Command.KEY_DATA));
+                return encodeText(getUniqueId(command.getDeviceId()), command.getString(Command.KEY_DATA));
             case Command.TYPE_OUTPUT_CONTROL:
-                return encodeText(getUniqueId(command.getId()),
+                return encodeText(getUniqueId(command.getDeviceId()),
                         "Out " + command.getInteger(Command.KEY_INDEX) + "," + command.getString(Command.KEY_DATA));
             default:
                 Log.warning(new UnsupportedOperationException(command.getType()));
