2e157393d036f765687d5ff38797e6e600d986e5
Anton Tananaev <anton.tananaev@gmail.com>
2017-07-22 12:24:55 +1200

    Add binary INF support

--- src/org/traccar/protocol/Gl200BinaryProtocolDecoder.java
+++ src/org/traccar/protocol/Gl200BinaryProtocolDecoder.java
@@ -27,11 +27,149 @@ import org.traccar.model.Position;
 
 import java.net.SocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class Gl200BinaryProtocolDecoder extends BaseProtocolDecoder {
 
     public Gl200BinaryProtocolDecoder(Gl200Protocol protocol) {
         super(protocol);
+    }
+
+    private Date decodeTime(ChannelBuffer buf) {
+        DateBuilder dateBuilder = new DateBuilder()
+                .setDate(buf.readUnsignedShort(), buf.readUnsignedByte(), buf.readUnsignedByte())
+                .setTime(buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte());
+        return dateBuilder.getDate();
+    }
+
+    private Position decodeEvent(Channel channel, SocketAddress remoteAddress, ChannelBuffer buf) {
+
+        Position position = new Position();
+        position.setProtocol(getProtocolName());
+
+        buf.readUnsignedByte(); // message type
+        buf.readUnsignedInt(); // mask
+        buf.readUnsignedShort(); // length
+        buf.readUnsignedByte(); // device type
+        buf.readUnsignedShort(); // protocol version
+
+        position.set(Position.KEY_VERSION_FW, String.valueOf(buf.readUnsignedShort()));
+
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, String.format("%015d", buf.readLong()));
+        if (deviceSession == null) {
+            return null;
+        }
+        position.setDeviceId(deviceSession.getDeviceId());
+
+        position.set(Position.KEY_BATTERY_LEVEL, buf.readUnsignedByte());
+        position.set(Position.KEY_POWER, buf.readUnsignedShort());
+
+        buf.readUnsignedByte(); // motion status
+
+        position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
+        position.set(Position.KEY_INDEX, buf.readUnsignedByte());
+
+        int hdop = buf.readUnsignedByte();
+        position.setValid(hdop > 0);
+        position.set(Position.KEY_HDOP, hdop);
+
+        position.setSpeed(UnitsConverter.knotsFromKph(buf.readUnsignedMedium()));
+        position.setCourse(buf.readUnsignedShort());
+        position.setAltitude(buf.readShort());
+        position.setLongitude(buf.readInt() * 0.000001);
+        position.setLatitude(buf.readInt() * 0.000001);
+
+        position.setTime(decodeTime(buf));
+
+        position.setNetwork(new Network(CellTower.from(
+                buf.readUnsignedShort(), buf.readUnsignedShort(),
+                buf.readUnsignedShort(), buf.readUnsignedShort())));
+
+        buf.readUnsignedByte(); // reserved
+
+        return position;
+    }
+
+    public static final int MSG_INF_GPS = 2;
+    public static final int MSG_INF_CID = 4;
+    public static final int MSG_INF_CSQ = 5;
+    public static final int MSG_INF_VER = 6;
+    public static final int MSG_INF_BAT = 7;
+    public static final int MSG_INF_TMZ = 9;
+    public static final int MSG_INF_GIR = 10;
+
+    private Position decodeInformation(Channel channel, SocketAddress remoteAddress, ChannelBuffer buf) {
+
+        Position position = new Position();
+        position.setProtocol(getProtocolName());
+
+        int type = buf.readUnsignedByte();
+
+        buf.readUnsignedInt(); // mask
+        buf.readUnsignedShort(); // length
+
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, String.format("%015d", buf.readLong()));
+        if (deviceSession == null) {
+            return null;
+        }
+        position.setDeviceId(deviceSession.getDeviceId());
+
+        buf.readUnsignedByte(); // device type
+        buf.readUnsignedShort(); // protocol version
+
+        position.set(Position.KEY_VERSION_FW, String.valueOf(buf.readUnsignedShort()));
+
+        if (type == MSG_INF_VER) {
+            buf.readUnsignedShort(); // hardware version
+            buf.readUnsignedShort(); // mcu version
+            buf.readUnsignedShort(); // reserved
+        }
+
+        buf.readUnsignedByte(); // motion status
+        buf.readUnsignedByte(); // reserved
+
+        position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
+
+        buf.readUnsignedByte(); // mode
+        buf.skipBytes(7); // last fix time
+        buf.readUnsignedByte(); // reserved
+        buf.readUnsignedByte();
+        buf.readUnsignedShort(); // response report mask
+        buf.readUnsignedShort(); // ign interval
+        buf.readUnsignedShort(); // igf interval
+        buf.readUnsignedInt(); // reserved
+        buf.readUnsignedByte(); // reserved
+
+        if (type == MSG_INF_BAT) {
+            position.set(Position.KEY_CHARGE, buf.readUnsignedByte() != 0);
+            position.set(Position.KEY_POWER, buf.readUnsignedShort() * 0.001);
+            position.set(Position.KEY_BATTERY, buf.readUnsignedShort() * 0.001);
+            position.set(Position.KEY_BATTERY_LEVEL, buf.readUnsignedByte());
+        }
+
+        buf.skipBytes(10); // iccid
+
+        if (type == MSG_INF_CSQ) {
+            position.set(Position.KEY_RSSI, buf.readUnsignedByte());
+            buf.readUnsignedByte();
+        }
+
+        buf.readUnsignedByte(); // time zone flags
+        buf.readUnsignedShort(); // time zone offset
+
+        if (type == MSG_INF_GIR) {
+            buf.readUnsignedByte(); // gir trigger
+            buf.readUnsignedByte(); // cell number
+            position.setNetwork(new Network(CellTower.from(
+                    buf.readUnsignedShort(), buf.readUnsignedShort(),
+                    buf.readUnsignedShort(), buf.readUnsignedShort())));
+            buf.readUnsignedByte(); // ta
+            buf.readUnsignedByte(); // rx level
+        }
+
+        getLastLocation(position, decodeTime(buf));
+
+        return position;
     }
 
     @Override
@@ -40,64 +178,14 @@ public class Gl200BinaryProtocolDecoder extends BaseProtocolDecoder {
 
         ChannelBuffer buf = (ChannelBuffer) msg;
 
-        String header = buf.readBytes(4).toString(StandardCharsets.US_ASCII);
-
-        if (header.equals("+EVT")) {
-
-            Position position = new Position();
-            position.setProtocol(getProtocolName());
-
-            buf.readUnsignedByte(); // message type
-
-            buf.readUnsignedInt(); // mask
-
-            buf.readUnsignedShort(); // length
-            buf.readUnsignedByte(); // device type
-            buf.readUnsignedShort(); // protocol version
-
-            position.set(Position.KEY_VERSION_FW, String.valueOf(buf.readUnsignedShort()));
-
-            DeviceSession deviceSession = getDeviceSession(
-                    channel, remoteAddress, String.format("%015d", buf.readLong()));
-            if (deviceSession == null) {
+        switch (buf.readBytes(4).toString(StandardCharsets.US_ASCII)) {
+            case "+INF":
+                return decodeInformation(channel, remoteAddress, buf);
+            case "+EVT":
+                return decodeEvent(channel, remoteAddress, buf);
+            default:
                 return null;
-            }
-            position.setDeviceId(deviceSession.getDeviceId());
-
-            position.set(Position.KEY_BATTERY_LEVEL, buf.readUnsignedByte());
-            position.set(Position.KEY_POWER, buf.readUnsignedShort());
-
-            buf.readUnsignedByte(); // motion status
-
-            position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
-            position.set(Position.KEY_INDEX, buf.readUnsignedByte());
-
-            int hdop = buf.readUnsignedByte();
-            position.setValid(hdop > 0);
-            position.set(Position.KEY_HDOP, hdop);
-
-            position.setSpeed(UnitsConverter.knotsFromKph(buf.readUnsignedMedium()));
-            position.setCourse(buf.readUnsignedShort());
-            position.setAltitude(buf.readShort());
-            position.setLongitude(buf.readInt() * 0.000001);
-            position.setLatitude(buf.readInt() * 0.000001);
-
-            DateBuilder dateBuilder = new DateBuilder()
-                    .setDate(buf.readUnsignedShort(), buf.readUnsignedByte(), buf.readUnsignedByte())
-                    .setTime(buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte());
-            position.setTime(dateBuilder.getDate());
-
-            position.setNetwork(new Network(CellTower.from(
-                    buf.readUnsignedShort(), buf.readUnsignedShort(),
-                    buf.readUnsignedShort(), buf.readUnsignedShort())));
-
-            buf.readUnsignedByte(); // reserved
-
-            return position;
-
         }
-
-        return null;
     }
 
 }
