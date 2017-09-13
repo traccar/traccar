811adaf1b070334fa289f226730d547ef09da174
Abyss777 <abyss@fox5.ru>
2017-09-13 09:44:50 +0500

    Fix security check

--- src/org/traccar/database/CommandsManager.java
+++ src/org/traccar/database/CommandsManager.java
@@ -39,7 +39,7 @@ public class CommandsManager  extends ExtendedObjectManager<Command> {
     }
 
     public boolean checkDeviceCommand(long deviceId, long commandId) {
-        return getAllDeviceItems(deviceId).contains(commandId);
+        return !getAllDeviceItems(deviceId).contains(commandId);
     }
 
     public void sendCommand(Command command) throws Exception {
