75aee70ad55c40cdfdf7dc0e8868864d923b9ee0
Abyss777 <abyss@fox5.ru>
2017-09-20 13:26:21 +0500

    Move Commandtypes API to Commands

--- src/org/traccar/api/resource/CommandResource.java
+++ src/org/traccar/api/resource/CommandResource.java
@@ -19,6 +19,7 @@ import org.traccar.Context;
 import org.traccar.api.ExtendedObjectResource;
 import org.traccar.database.CommandsManager;
 import org.traccar.model.Command;
+import org.traccar.model.Typed;
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -72,4 +73,15 @@ public class CommandResource extends ExtendedObjectResource<Command> {
         return Response.ok(entity).build();
     }
 
+    @GET
+    @Path("types")
+    public Collection<Typed> get(@QueryParam("deviceId") long deviceId,
+            @QueryParam("textChannel") boolean textChannel) {
+        if (deviceId != 0) {
+            Context.getPermissionsManager().checkDevice(getUserId(), deviceId);
+            return Context.getCommandsManager().getCommandTypes(deviceId, textChannel);
+        } else {
+            return Context.getCommandsManager().getAllCommandTypes();
+        }
+    }
 }
