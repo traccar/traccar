9d2d248fe97e2a180b249b753743bc2127a30b02
Abyss777 <abyss@fox5.ru>
2017-09-26 11:57:08 +0500

    Improve Teltonika IO decoding

--- test/org/traccar/ProtocolTest.java
+++ test/org/traccar/ProtocolTest.java
@@ -257,6 +257,10 @@ public class ProtocolTest extends BaseTest {
             Assert.assertTrue(attributes.get(Position.KEY_DRIVER_UNIQUE_ID) instanceof String);
         }
 
+        if (attributes.containsKey(Position.KEY_STEPS)) {
+            Assert.assertTrue(attributes.get(Position.KEY_STEPS) instanceof Number);
+        }
+
         if (position.getNetwork() != null && position.getNetwork().getCellTowers() != null) {
             for (CellTower cellTower : position.getNetwork().getCellTowers()) {
                 checkInteger(cellTower.getMobileCountryCode(), 0, 999);
