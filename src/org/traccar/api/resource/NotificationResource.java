bebe4454d56114e7a290a03cfc05274ca8b62a46
parveenkumaryadav <parveenkumardeeva@gmail.com>
2017-12-23 11:21:36 +0530

    FCM implementation (checkstyle compatible)

--- src/org/traccar/api/resource/NotificationResource.java
+++ src/org/traccar/api/resource/NotificationResource.java
@@ -29,7 +29,11 @@ import org.traccar.notification.NotificationSms;
 import org.traccar.web.NotificationWeb;
 
 import javax.mail.MessagingException;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
