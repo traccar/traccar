8733832c8b8cb50528e14c0f7db845384787ac61
Anton Tananaev <anton.tananaev@gmail.com>
2018-01-17 05:09:42 +1300

    Use converter to decode speed

--- src/org/traccar/protocol/GalileoProtocolDecoder.java
+++ src/org/traccar/protocol/GalileoProtocolDecoder.java
@@ -20,6 +20,7 @@ import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 import org.traccar.BaseProtocolDecoder;
 import org.traccar.DeviceSession;
+import org.traccar.helper.UnitsConverter;
 import org.traccar.model.Position;
 
 import java.net.SocketAddress;
@@ -117,7 +118,7 @@ public class GalileoProtocolDecoder extends BaseProtocolDecoder {
                 position.setTime(new Date(buf.readUnsignedInt() * 1000));
                 break;
             case 0x33:
-                position.setSpeed(buf.readUnsignedShort() * 0.0539957);
+                position.setSpeed(UnitsConverter.knotsFromKph(buf.readUnsignedShort() * 0.1));
                 position.setCourse(buf.readUnsignedShort() * 0.1);
                 break;
             case 0x34:
