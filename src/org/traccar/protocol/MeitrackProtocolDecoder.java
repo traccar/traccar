acc6a57605c81e9bb4c897b3da95aa8c875237f8
Anton Tananaev <anton.tananaev@gmail.com>
2018-03-23 00:55:47 +1300

    Fix Meitrack taximeter decoding

--- src/org/traccar/protocol/MeitrackProtocolDecoder.java
+++ src/org/traccar/protocol/MeitrackProtocolDecoder.java
@@ -256,7 +256,7 @@ public class MeitrackProtocolDecoder extends BaseProtocolDecoder {
 
         if (values.length > 5 && !values[5].isEmpty()) {
             String[] data = values[5].split("\\|");
-            boolean started = data[0].charAt(0) == '0';
+            boolean started = data[0].charAt(1) == '0';
             position.set("taximeterOn", started);
             position.set("taximeterStart", data[1]);
             if (data.length > 2) {
