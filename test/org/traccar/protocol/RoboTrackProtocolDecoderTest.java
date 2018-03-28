eac3b3804d8bbabde8b5ac8c4661bf0fec469868
Anton Tananaev <anton.tananaev@gmail.com>
2018-03-28 19:51:31 +1300

    Add RoboTrack location test cases

--- test/org/traccar/protocol/RoboTrackProtocolDecoderTest.java
+++ test/org/traccar/protocol/RoboTrackProtocolDecoderTest.java
@@ -3,6 +3,8 @@ package org.traccar.protocol;
 import org.junit.Test;
 import org.traccar.ProtocolTest;
 
+import java.nio.ByteOrder;
+
 public class RoboTrackProtocolDecoderTest extends ProtocolTest {
 
     @Test
@@ -10,8 +12,14 @@ public class RoboTrackProtocolDecoderTest extends ProtocolTest {
 
         RoboTrackProtocolDecoder decoder = new RoboTrackProtocolDecoder(new RoboTrackProtocol());
 
-        verifyNull(decoder, binary(
+        verifyNull(decoder, binary(ByteOrder.LITTLE_ENDIAN,
                 "00524f424f545241434b00000000000000383638323034303032323533343136313233343536373839303132312e313261000000312e353761000000312e3030000000003e"));
+
+        verifyPosition(decoder, binary(ByteOrder.LITTLE_ENDIAN,
+                "03e020bb5a034409034862120210a9e105000000000000b9"));
+
+        verifyPosition(decoder, binary(ByteOrder.LITTLE_ENDIAN,
+                "03f120bb5a30460903426312021798e105000000000000cd"));
 
     }
 
