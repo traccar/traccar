9ff9232e779f7e137fa8b0cba2e0b424ff4879f9
Ivan Martinez <ivanfmartinez@users.noreply.github.com>
2018-04-08 18:53:02 -0300

    just consider zero to check if defined

--- src/org/traccar/model/Geofence.java
+++ src/org/traccar/model/Geofence.java
@@ -66,7 +66,7 @@ public class Geofence extends ScheduledModel {
             geometry = new GeofencePolygon(area);
         } else if (area.startsWith("LINESTRING")) {
             final double distance = getDouble("polylineDistance");
-            geometry = new GeofencePolyline(area, distance >= 1 ? distance
+            geometry = new GeofencePolyline(area, distance > 0 ? distance
                     : Context.getConfig().getDouble("geofence.polylineDistance", 25));
         } else {
             throw new ParseException("Unknown geometry type", 0);
