053e03307f851f3d990caf8ba95dad4ea8836977
Anton Tananaev <anton.tananaev@gmail.com>
2018-04-18 15:26:22 +1200

    Atrack frame decoder to handle text format

--- src/org/traccar/protocol/AtrackFrameDecoder.java
+++ src/org/traccar/protocol/AtrackFrameDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Anton Tananaev (anton@traccar.org)
+ * Copyright 2017 - 2018 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ public class AtrackFrameDecoder extends FrameDecoder {
                     return buf.readBytes(KEEPALIVE_LENGTH);
                 }
 
-            } else if (buf.getUnsignedShort(buf.readerIndex()) == 0x4050) {
+            } else if (buf.getUnsignedShort(buf.readerIndex()) == 0x4050 && buf.getByte(buf.readerIndex() + 2) != ',') {
 
                 if (buf.readableBytes() > 6) {
                     int length = buf.getUnsignedShort(buf.readerIndex() + 4) + 4 + 2;
