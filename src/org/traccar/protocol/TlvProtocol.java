86ac75eb6e10d9161a9d29dab47f3da8356549d0
Anton Tananaev <anton.tananaev@gmail.com>
2018-07-02 11:31:14 -0700

    Fix issue with TLV frame decoder

--- src/org/traccar/protocol/TlvProtocol.java
+++ src/org/traccar/protocol/TlvProtocol.java
@@ -33,7 +33,7 @@ public class TlvProtocol extends BaseProtocol {
         serverList.add(new TrackerServer(false, getName()) {
             @Override
             protected void addProtocolHandlers(PipelineBuilder pipeline) {
-                pipeline.addLast("frameDecoder", new CharacterDelimiterFrameDecoder('\0'));
+                pipeline.addLast("frameDecoder", new CharacterDelimiterFrameDecoder(1024, '\0'));
                 pipeline.addLast("objectDecoder", new TlvProtocolDecoder(TlvProtocol.this));
             }
         });
