8c76bf440cc83e4c35548a56da5cf7c5e44d0545
Anton Tananaev <anton.tananaev@gmail.com>
2018-07-26 01:37:55 +1200

    Remove unused import

--- src/org/traccar/protocol/HuabaoProtocolDecoder.java
+++ src/org/traccar/protocol/HuabaoProtocolDecoder.java
@@ -33,7 +33,6 @@ import java.net.SocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.TimeZone;
 
 public class HuabaoProtocolDecoder extends BaseProtocolDecoder {
 
