94e9d158b7c93af339ae5ce478764b7ae378e15d
danglotb <bdanglot@gmail.com>
2018-08-16 15:05:28 +0200

    test: ProgressProtocolDecoderTest should thrown an exception in case of illegal hexa char

--- test/org/traccar/protocol/ProgressProtocolDecoderTest.java
+++ test/org/traccar/protocol/ProgressProtocolDecoderTest.java
@@ -3,6 +3,9 @@ package org.traccar.protocol;
 import org.junit.Test;
 import org.traccar.ProtocolTest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class ProgressProtocolDecoderTest extends ProtocolTest {
 
     @Test
@@ -13,6 +16,16 @@ public class ProgressProtocolDecoderTest extends ProtocolTest {
         verifyNull(decoder, binary(
                 "020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6"));
 
+    }
+
+    @Test
+    public void testDecodeShouldThrowAnException() throws Exception {
+        try {
+            binary("++");
+            fail("should have thrown RuntimeException");
+        } catch (RuntimeException expected) {
+            assertEquals("org.apache.commons.codec.DecoderException: Illegal hexadecimal character + at index 0", expected.getMessage());
+        }
     }
 
 }
