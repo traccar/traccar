1432d04a7be8397becd5c88f9e1f075861acbfdc
Anton Tananaev <anton.tananaev@gmail.com>
2018-09-17 21:22:34 +1200

    Update packaging scripts

--- setup/package.sh
+++ setup/package.sh
@@ -22,15 +22,30 @@ check_requirement () {
   fi 
 }
 
-check_requirement "ls ../../ext-6.2.0" "Missing ../../ext-6.2.0 (https://www.sencha.com/legal/GPL/)"
-check_requirement "ls innosetup-*.exe" "Missing isetup-*.exe (http://www.jrsoftware.org/isdl.php)"
+check_requirement "ls ../../ext-6.2.0" "Missing ExtJS (https://www.sencha.com/legal/GPL/)"
+check_requirement "ls innosetup-*.exe" "Missing Inno Setup (http://www.jrsoftware.org/isdl.php)"
+check_requirement "ls java-*.windows.x86_64.zip" "Missing Windows 64 Java (https://github.com/ojdkbuild/ojdkbuild)"
+check_requirement "ls jdk-*-linux-x64.zip" "Missing Linux 64 Java (https://github.com/ojdkbuild/contrib_jdk10u-ci/releases)"
+check_requirement "ls jdk-*-linux-armhf.zip" "Missing Linux ARM Java (https://github.com/ojdkbuild/contrib_jdk10u-aarch32-ci/releases)"
 check_requirement "which sencha" "Missing sencha cmd package (https://www.sencha.com/products/extjs/cmd-download/)"
-check_requirement "which wine" "Missing wine package"
-check_requirement "which innoextract" "Missing innoextract package"
-check_requirement "which makeself" "Missing makeself package"
+check_requirement "which unzip" "Missing unzip"
+check_requirement "which wine" "Missing wine"
+check_requirement "which innoextract" "Missing innoextract"
+check_requirement "which makeself" "Missing makeself"
+check_requirement "which jlink" "Missing jlink"
 
 prepare () {
   ../traccar-web/tools/minify.sh
+
+  mkdir -p out/{conf,data,lib,logs,web,schema,templates}
+
+  cp ../target/tracker-server.jar out
+  cp ../target/lib/* out/lib
+  cp ../schema/* out/schema
+  cp -r ../templates/* out/templates
+  cp -r ../traccar-web/web/* out/web
+  cp default.xml out/conf
+  cp traccar.xml out/conf
 
   innoextract innosetup-*.exe
   echo "If you got any errors here try isetup version 5.5.5 (or check supported versions using 'innoextract -v')"
@@ -39,66 +54,57 @@ prepare () {
 cleanup () {
   rm ../traccar-web/web/app.min.js
 
-  rm -r app/
-}
-
-copy_files () {
-  cp ../target/tracker-server.jar out
-  cp ../target/lib/* out/lib
-  cp ../schema/* out/schema
-  cp -r ../templates/* out/templates
-  cp -r ../traccar-web/web/* out/web
-  cp default.xml out/conf
-  cp traccar.xml out/conf
-}
-
-package_windows () {
-  mkdir -p out/{conf,data,lib,logs,web,schema,templates}
-
-  copy_files
-
-  wine app/ISCC.exe traccar.iss
-
-  zip -j traccar-windows-$VERSION.zip Output/traccar-setup.exe README.txt
-
-  rm -r Output
+  rm -r out
   rm -r tmp
-  rm -r out
+  rm -r app
 }
 
-package_unix () {
-  mkdir -p out/{conf,data,lib,logs,web,schema,templates}
-  copy_files
-
-  cp java-test/test.jar out
-  cp setup.sh out
-  cp other/traccar.service out
-  makeself --notemp out traccar.run "traccar" ./setup.sh
-
-  zip -j traccar-linux-$VERSION.zip traccar.run README.txt
-
-  rm traccar.run
-  rm -r out
-}
-
-package_universal () {
-  mkdir -p out/{conf,data,lib,logs,web,schema,templates}
-
-  copy_files
-
+package_other () {
   cp README.txt out
-
   cd out
   zip -r ../traccar-other-$VERSION.zip *
   cd ..
+  rm out/README.txt
+}
 
-  rm -rf out/
+package_windows () {
+  unzip -o java-*.windows.x86_64.zip
+  jlink --module-path java-*.windows.x86_64/jmods --add-modules java.se.ee --output out/jre
+  rm -rf java-*.windows.x86_64
+  wine app/ISCC.exe traccar.iss
+  rm -rf out/jre
+  zip -j traccar-windows-64-$VERSION.zip Output/traccar-setup.exe README.txt
+  rm -r Output
+}
+
+package_unix () {
+  cp setup.sh out
+  cp traccar.service out
+
+  unzip -o jdk-*-linux-x64.zip
+  jlink --module-path jdk-*-linux-x64/jmods --add-modules java.se.ee --output out/jre
+  rm -rf jdk-*-linux-x64
+  makeself --notemp out traccar.run "traccar" ./setup.sh
+  rm -rf out/jre
+  zip -j traccar-linux-64-$VERSION.zip traccar.run README.txt
+  rm traccar.run
+
+  unzip -o jdk-*-linux-armhf.zip
+  jlink --module-path jdk-*-linux-armhf/jmods --add-modules java.se.ee --output out/jre
+  rm -rf jdk-*-linux-armhf
+  makeself --notemp out traccar.run "traccar" ./setup.sh
+  rm -rf out/jre
+  zip -j traccar-linux-arm-$VERSION.zip traccar.run README.txt
+  rm traccar.run
+
+  rm out/setup.sh
+  rm out/traccar.service
 }
 
 prepare
 
+package_other
 package_windows
 package_unix
-package_universal
 
 cleanup
--- setup/traccar.iss
+++ setup/traccar.iss
@@ -2,50 +2,18 @@
 AppName=Traccar
 AppVersion=4.0
 DefaultDirName={pf}\Traccar
-AlwaysRestart=yes
 OutputBaseFilename=traccar-setup
 ArchitecturesInstallIn64BitMode=x64
 
 [Dirs]
-Name: "{app}\conf"
 Name: "{app}\data"
-Name: "{app}\lib"
 Name: "{app}\logs"
-Name: "{app}\web"
-Name: "{app}\schema"
-Name: "{app}\templates"
 
 [Files]
 Source: "out\*"; DestDir: "{app}"; Flags: recursesubdirs
 
 [Run]
-Filename: "java.exe"; Parameters: "-jar ""{app}\tracker-server.jar"" --install .\conf\traccar.xml"; Flags: runhidden
+Filename: "{app}\jre\bin\java.exe"; Parameters: "-jar ""{app}\tracker-server.jar"" --install .\conf\traccar.xml"; Flags: runhidden
 
 [UninstallRun]
-Filename: "java.exe"; Parameters: "-jar ""{app}\tracker-server.jar"" --uninstall"; Flags: runhidden
-
-[Code]
-function GetLocalMachine(): Integer;
-begin
-  if IsWin64 then
-  begin
-    Result := HKLM64;
-  end
-  else
-  begin
-    Result := HKEY_LOCAL_MACHINE;
-  end;
-end;
-
-function InitializeSetup(): Boolean;
-begin
-  if RegKeyExists(GetLocalMachine(), 'SOFTWARE\JavaSoft') then
-  begin
-    Result := true;
-  end
-  else
-  begin
-    Result := false;
-    MsgBox('This application requires Java Runtime Environment version 7 or later. Please download and install the JRE and run this setup again. If you have Java installed and still get this error, you need to re-install it from offline installer (for more info see https://www.traccar.org/windows/).', mbCriticalError, MB_OK);
-  end;
-end;
+Filename: "{app}\jre\bin\java.exe"; Parameters: "-jar ""{app}\tracker-server.jar"" --uninstall"; Flags: runhidden
