902135f2821c86f650ef13dbfe27ae6ef00ece61
Anton Tananaev <anton.tananaev@gmail.com>
2018-09-21 16:06:34 +1200

    Update Milesmate protocol

--- test/org/traccar/protocol/MilesmateProtocolDecoderTest.java
+++ test/org/traccar/protocol/MilesmateProtocolDecoderTest.java
@@ -11,6 +11,9 @@ public class MilesmateProtocolDecoderTest extends ProtocolTest {
         MilesmateProtocolDecoder decoder = new MilesmateProtocolDecoder(new MilesmateProtocol());
 
         verifyPosition(decoder, text(
+                "ApiString={A:861359037373030,B:09.8,C:00.0,D:083506,E:2838.5529N,F:07717.8049E,G:000.00,H:170918,I:G,J:00004100,K:0000000A,L:1234,M:126.86}"));
+
+        verifyPosition(decoder, text(
                 "ApiString={A:861359037496211,B:12.7,C:06.0,D:060218,E:2837.1003N,F:07723.3162E,G:016.80,H:310818,I:G,J:10010100,K:0000000A,L:1234,M:358.33}"),
                 position("2018-08-31 06:02:18.000", true, 28.61834, 77.38860));
 
