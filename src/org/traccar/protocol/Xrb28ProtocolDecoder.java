c327cac973c40af28151319badc94229b56ecfa2
Anton Tananaev <anton.tananaev@gmail.com>
2018-11-10 12:14:22 +1300

    Implement XRB 28 protocol

---
+++ src/org/traccar/protocol/Xrb28ProtocolDecoder.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2018 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import io.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.DeviceSession;
+import org.traccar.NetworkMessage;
+import org.traccar.Protocol;
+import org.traccar.helper.DateBuilder;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.model.Command;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class Xrb28ProtocolDecoder extends BaseProtocolDecoder {
+
+    private String pendingCommand;
+
+    public void setPendingCommand(String pendingCommand) {
+        this.pendingCommand = pendingCommand;
+    }
+
+    public Xrb28ProtocolDecoder(Protocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .text("*HBCR,")
+            .expression("..,")                   // vendor
+            .number("d{15},")                    // imei
+            .expression("..,")                   // type
+            .number("0,")                        // reserved
+            .number("(dd)(dd)(dd).d+,")          // time (hhmmss)
+            .expression("([AV]),")               // validity
+            .number("(dd)(dd.d+),")              // latitude
+            .expression("([NS]),")
+            .number("(d{2,3})(dd.d+),")          // longitude
+            .expression("([EW]),")
+            .number("(d+),")                     // satellites
+            .number("(d+.d+),")                  // hdop
+            .number("(dd)(dd)(dd),")             // date (ddmmyy)
+            .number("(-?d+),")                   // altitude
+            .expression(".,")                    // height unit
+            .expression(".#")                    // mode
+            .compile();
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        String sentence = (String) msg;
+
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, sentence.substring(10, 26));
+        if (deviceSession == null) {
+            return null;
+        }
+
+        String type = sentence.substring(27, 29);
+        if (channel != null) {
+            if (type.matches("L0|L1|W0|E1")) {
+                channel.write(new NetworkMessage(
+                        sentence.substring(0, 29) + "#\n", remoteAddress));
+            } else if (type.equals("R0") && pendingCommand != null) {
+                String command = pendingCommand.equals(Command.TYPE_ALARM_ARM) ? "L1," : "L0,";
+                channel.write(new NetworkMessage(
+                        sentence.substring(0, 27) + command + sentence.substring(32) + "\n", remoteAddress));
+                pendingCommand = null;
+            }
+        }
+
+        Parser parser = new Parser(PATTERN, sentence.substring(2));
+        if (!parser.matches()) {
+            return null;
+        }
+
+        Position position = new Position(getProtocolName());
+        position.setDeviceId(deviceSession.getDeviceId());
+
+        DateBuilder dateBuilder = new DateBuilder()
+                .setTime(parser.nextInt(), parser.nextInt(), parser.nextInt());
+
+        position.setValid(parser.next().equals("A"));
+        position.setLatitude(parser.nextCoordinate());
+        position.setLongitude(parser.nextCoordinate());
+
+        position.set(Position.KEY_SATELLITES, parser.nextInt());
+        position.set(Position.KEY_HDOP, parser.nextDouble());
+
+        dateBuilder.setDateReverse(parser.nextInt(), parser.nextInt(), parser.nextInt());
+        position.setTime(dateBuilder.getDate());
+
+        position.setAltitude(parser.nextInt());
+
+        return position;
+    }
+
+}
