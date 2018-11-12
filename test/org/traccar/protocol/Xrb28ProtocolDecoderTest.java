9d714e91b9f7e1fa8b0b94f0d9aaebcce825c4cb
Anton Tananaev <anton.tananaev@gmail.com>
2018-11-13 08:30:09 +1300

    Fix issue with XRB 28 decoder

--- test/org/traccar/protocol/Xrb28ProtocolDecoderTest.java
+++ test/org/traccar/protocol/Xrb28ProtocolDecoderTest.java
@@ -11,13 +11,19 @@ public class Xrb28ProtocolDecoderTest extends ProtocolTest {
         Xrb28ProtocolDecoder decoder = new Xrb28ProtocolDecoder(null);
 
         verifyNull(decoder, text(
-                "\u00ff\u00ff*HBCR,OM,123456789123456,Q0,412,80#"));
+                "*SCOR,OM,863158022988725,H0,0,412,28,80,0#"));
 
         verifyNull(decoder, text(
-                "\u00ff\u00ff*HBCR,OM,123456789123456,R0,0,55,1234,1497689816#"));
+                "*HBCR,OM,123456789123456,Q0,412,80#"));
+
+        verifyNull(decoder, text(
+                "*HBCR,OM,123456789123456,R0,0,55,1234,1497689816#"));
 
         verifyPosition(decoder, text(
-                "\u00ff\u00ff*HBCR,OM,123456789123456,D0,0,124458.00,A,2237.7514,N,11408.6214,E,6,0.21,151216,10,M,A#"));
+                "*HBCR,OM,123456789123456,D0,0,124458.00,A,2237.7514,N,11408.6214,E,6,0.21,151216,10,M,A#"));
+
+        verifyPosition(decoder, text(
+                "*SCOR,OM,863158022988725,D0,0,124458.00,A,2237.7514,N,11408.6214,E,6,0.21,151216,10,M,A#"));
 
     }
 
