65d0cdf1a38861ac4a53b9c7fa6489c20c179df6
Anton Tananaev <anton.tananaev@gmail.com>
2018-11-22 17:01:51 +1300

    Support H002 protocol

--- src/org/traccar/protocol/Pt60ProtocolDecoder.java
+++ src/org/traccar/protocol/Pt60ProtocolDecoder.java
@@ -25,7 +25,6 @@ import org.traccar.helper.PatternBuilder;
 import org.traccar.model.Position;
 
 import java.net.SocketAddress;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.regex.Pattern;
@@ -36,26 +35,37 @@ public class Pt60ProtocolDecoder extends BaseProtocolDecoder {
         super(protocol);
     }
 
-    public static final int MSG_TRACK = 6;
-    public static final int MSG_STEP_COUNT = 13;
-    public static final int MSG_HEART_RATE = 14;
+    public static final int MSG_G_TRACK = 6;
+    public static final int MSG_G_STEP_COUNT = 13;
+    public static final int MSG_G_HEART_RATE = 14;
+
+    public static final int MSG_B_POSITION = 1;
 
     private static final Pattern PATTERN = new PatternBuilder()
-            .text("@G#@,")                       // header
-            .number("Vdd,")                      // protocol version
-            .number("(d+),")                     // type
-            .number("(d+),")                     // imei
-            .number("(d+),")                     // imsi
+            .expression("@(.)#@[,|]")            // header
+            .number("V?dd[,|]")                  // protocol version
+            .number("(d+)[,|]")                  // type
+            .number("(d+)[,|]")                  // imei
+            .number("d+[,|]")                    // imsi
+            .groupBegin()
+            .number("[01][,|]")                  // state
+            .number("d+[,|]")                    // battery
+            .groupEnd("?")
             .number("(dddd)(dd)(dd)")            // date (yyyymmdd)
-            .number("(dd)(dd)(dd),")             // time (hhmmss)
+            .number("(dd)(dd)(dd)[,|]")          // time (hhmmss)
             .expression("(.*)")                  // data
             .compile();
 
-    private void sendResponse(Channel channel, SocketAddress remoteAddress) {
+    private void sendResponse(Channel channel, SocketAddress remoteAddress, String format, int type, String imei) {
         if (channel != null) {
-            DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-            channel.writeAndFlush(new NetworkMessage(
-                    "@G#@,V01,38," + dateFormat.format(new Date()) + ",@R#@", remoteAddress));
+            String message;
+            String time = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+            if (format.equals("G")) {
+                message = String.format("@G#@,V01,38,%s,@R#@", time);
+            } else {
+                message = String.format("@B#@|01|%03d|%s|0|%s|@E#@", type + 1, imei, time);
+            }
+            channel.writeAndFlush(new NetworkMessage(message, remoteAddress));
         }
     }
 
@@ -63,59 +73,84 @@ public class Pt60ProtocolDecoder extends BaseProtocolDecoder {
     protected Object decode(
             Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
 
-        sendResponse(channel, remoteAddress);
-
         Parser parser = new Parser(PATTERN, (String) msg);
         if (!parser.matches()) {
             return null;
         }
 
+        String format = parser.next();
         int type = parser.nextInt();
+        String imei = parser.next();
 
-        if (type != MSG_TRACK && type != MSG_STEP_COUNT && type != MSG_HEART_RATE) {
-            return null;
-        }
-
-        Position position = new Position(getProtocolName());
-
-        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, parser.next(), parser.next());
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, imei);
         if (deviceSession == null) {
             return null;
         }
-        position.setDeviceId(deviceSession.getDeviceId());
 
-        position.setDeviceTime(parser.nextDateTime());
+        sendResponse(channel, remoteAddress, format, type, imei);
 
-        String[] values = parser.next().split(",");
+        if (format.equals("G")) {
 
-        if (type == MSG_TRACK) {
+            if (type != MSG_G_TRACK && type != MSG_G_STEP_COUNT && type != MSG_G_HEART_RATE) {
+                return null;
+            }
+
+            Position position = new Position(getProtocolName());
+            position.setDeviceId(deviceSession.getDeviceId());
+            position.setDeviceTime(parser.nextDateTime());
+
+            String[] values = parser.next().split(",");
+
+            if (type == MSG_G_TRACK) {
+
+                position.setValid(true);
+                position.setFixTime(position.getDeviceTime());
+
+                String[] coordinates = values[0].split(";");
+                position.setLatitude(Double.parseDouble(coordinates[0]));
+                position.setLongitude(Double.parseDouble(coordinates[1]));
+
+            } else {
+
+                getLastLocation(position, position.getDeviceTime());
+
+                switch (type) {
+                    case MSG_G_STEP_COUNT:
+                        position.set(Position.KEY_STEPS, Integer.parseInt(values[0]));
+                        break;
+                    case MSG_G_HEART_RATE:
+                        position.set(Position.KEY_HEART_RATE, Integer.parseInt(values[0]));
+                        position.set(Position.KEY_BATTERY, Integer.parseInt(values[1]));
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            return position;
+
+        } else {
+
+            if (type != MSG_B_POSITION) {
+                return null;
+            }
+
+            Position position = new Position(getProtocolName());
+            position.setDeviceId(deviceSession.getDeviceId());
+            position.setDeviceTime(parser.nextDateTime());
+
+            String[] values = parser.next().split("\\|");
 
             position.setValid(true);
             position.setFixTime(position.getDeviceTime());
 
-            String[] coordinates = values[0].split(";");
-            position.setLatitude(Double.parseDouble(coordinates[0]));
-            position.setLongitude(Double.parseDouble(coordinates[1]));
+            position.setLongitude(Double.parseDouble(values[0]));
+            position.setLatitude(Double.parseDouble(values[1]));
 
-        } else {
-
-            getLastLocation(position, position.getDeviceTime());
-
-            switch (type) {
-                case MSG_STEP_COUNT:
-                    position.set(Position.KEY_STEPS, Integer.parseInt(values[0]));
-                    break;
-                case MSG_HEART_RATE:
-                    position.set(Position.KEY_HEART_RATE, Integer.parseInt(values[0]));
-                    position.set(Position.KEY_BATTERY, Integer.parseInt(values[1]));
-                    break;
-                default:
-                    break;
-            }
+            return position;
 
         }
-
-        return position;
     }
 
 }
