56d33dbe0e79789fc1ee78e3358c3531c2f16381
Anton Tananaev <anton.tananaev@gmail.com>
2018-12-15 11:25:21 -0800

    Implement HERE reverse geocoder (fix #4142)

--- src/org/traccar/Context.java
+++ src/org/traccar/Context.java
@@ -56,6 +56,7 @@ import org.traccar.geocoder.GeocodeXyzGeocoder;
 import org.traccar.geocoder.GisgraphyGeocoder;
 import org.traccar.geocoder.BanGeocoder;
 import org.traccar.geocoder.GoogleGeocoder;
+import org.traccar.geocoder.HereGeocoder;
 import org.traccar.geocoder.MapQuestGeocoder;
 import org.traccar.geocoder.NominatimGeocoder;
 import org.traccar.geocoder.OpenCageGeocoder;
@@ -316,6 +317,7 @@ public final class Context {
     public static Geocoder initGeocoder() {
         String type = config.getString("geocoder.type", "google");
         String url = config.getString("geocoder.url");
+        String id = config.getString("geocoder.id");
         String key = config.getString("geocoder.key");
         String language = config.getString("geocoder.language");
 
@@ -347,6 +349,8 @@ public final class Context {
                 return new GeocodeXyzGeocoder(key, cacheSize, addressFormat);
             case "ban":
                 return new BanGeocoder(cacheSize, addressFormat);
+            case "here":
+                return new HereGeocoder(id, key, language, cacheSize, addressFormat);
             default:
                 return new GoogleGeocoder(key, language, cacheSize, addressFormat);
         }
