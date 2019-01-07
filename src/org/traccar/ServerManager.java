96878a4baa6706e1903cde05e23cd21bc31348f4
Gavriel Fleischer <flocsy@gmail.com>
2019-01-07 22:12:06 +0200

    log port in case of conflict

--- src/org/traccar/ServerManager.java
+++ src/org/traccar/ServerManager.java
@@ -88,7 +88,7 @@ public class ServerManager {
             try {
                 server.start();
             } catch (BindException e) {
-                LOGGER.warn("One of the protocols is disabled due to port conflict");
+                LOGGER.warn("One of the protocols is disabled due to port conflict: " + server.getPort());
             }
         }
     }
