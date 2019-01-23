3fdf7c71df04dc327b692360a8560a0d6c125ded
Evgeny S Maksimov <me@vragam.net>
2019-01-23 22:54:12 +0300

    Minor fix

--- src/org/traccar/protocol/NavisProtocolDecoder.java
+++ src/org/traccar/protocol/NavisProtocolDecoder.java
@@ -163,7 +163,7 @@ public class NavisProtocolDecoder extends BaseProtocolDecoder {
                 position.set(Position.PREFIX_IN + 5, BitUtil.between(input, 4, 7));
                 position.set(Position.PREFIX_IN + 6, BitUtil.between(input, 7, 10));
                 position.set(Position.PREFIX_IN + 7, BitUtil.between(input, 10, 12));
-                position.set(Position.PREFIX_IN + 5, BitUtil.between(input, 12, 14));
+                position.set(Position.PREFIX_IN + 8, BitUtil.between(input, 12, 14));
             }
         } else if (isFormat(format, F50, F51, F52, F60)) {
             short input = buf.readUnsignedByte();
