5bf343a179e9565f78903ae63a002522373163a8
Gavriel Fleischer <flocsy@gmail.com>
2019-01-29 23:36:01 +0200

    fixed message encoding

--- src/org/traccar/protocol/WatchProtocolEncoder.java
+++ src/org/traccar/protocol/WatchProtocolEncoder.java
@@ -37,7 +37,7 @@ public class WatchProtocolEncoder extends StringProtocolEncoder implements Strin
             DecimalFormat fmt = new DecimalFormat("+#.##;-#.##", DecimalFormatSymbols.getInstance(Locale.US));
             return fmt.format(offset);
         } else if (key.equals(Command.KEY_MESSAGE)) {
-            return DataConverter.printHex(value.toString().getBytes());
+            return DataConverter.printHex(value.toString().getBytes(StandardCharsets.UTF_16));
         }
 
         return null;
