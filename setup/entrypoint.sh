81791bcea85daf6743abde3a64719bae05a6188e
Godwin peter .O <godwin@peter.com.ng>
2019-01-31 22:11:34 +0100

    Enviromental variable update
    Enviromental variable update for more options...

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+##Set for production mode
 sed -i 's/GPSTRACK/'"$SETUP_TITLE"'/g' /opt/traccar/web/release.html
 sed -i 's/ATTRIB_NAME/'"$ATTRIB_NAME"'/g' /opt/traccar/web/release.html
 sed -i 's#ATTRIB_LINK#'"$ATTRIB_LINK"'#g' /opt/traccar/web/release.html
-sed -i 's#PLAY_LINK#'"$ATTRIB_LINK"'#g' /opt/traccar/web/release.html
 
 ##Set for debug mode
-
 sed -i 's/GPSTRACK/'"$SETUP_TITLE"'/g' /opt/traccar/web/debug.html
 sed -i 's/ATTRIB_NAME/'"$ATTRIB_NAME"'/g' /opt/traccar/web/debug.html
 sed -i 's#ATTRIB_LINK#'"$ATTRIB_LINK"'#g' /opt/traccar/web/debug.html
-sed -i 's#PLAY_LINK#'"$ATTRIB_LINK"'#g' /opt/traccar/web/debug.html
 
+##Set for styles..
+sed -i 's/808080/'"$SETUP_COLOR"'/g' /opt/traccar/web/assets/mod.css
+
+##Set for address search
+sed -i 's#https://gcode.mcpl.ga#'"$GEOCODE_LINK"'#g' /opt/traccar/web/assets/address.ol.js
+
+##Set for play store link
+sed -i 's#PLAY_LINK#'"$PLAY_LINK"'#g' /opt/traccar/web/release.html
+sed -i 's#PLAY_LINK#'"$PLAY_LINK"'#g' /opt/traccar/web/debug.html
 
 ##Set login page logo
 sed -i 's/122/'"$FRONT_LOGO_WIDTH"'/g' /opt/traccar/web/app/view/dialog/Login.js
@@ -25,9 +32,7 @@ sed -i 's/27px/'"$BACK_LOGO_HEIGHT px"'/g' /opt/traccar/web/app.min.js
 
 exec "$@"
 
-######################################
-##Main Excecution####
 ################################
-exec java -Xms4096m -Xmx22024m -Djava.net.preferIPv4Stack=true -jar /opt/traccar/tracker-server.jar /opt/traccar/conf/traccar.xml
-
-
+##Main Excecution ##############
+################################
+exec java -Xms2048m -Xmx28024m -Djava.net.preferIPv4Stack=true -jar /opt/traccar/tracker-server.jar /opt/traccar/conf/traccar.xml
