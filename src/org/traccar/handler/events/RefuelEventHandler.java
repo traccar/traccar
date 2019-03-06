2c5cec800b9b81ae0f9c9447fdd5423fa4b07fbf
meteviasa <metevia.sam@gmail.com>
2019-03-06 16:54:50 -0500

    Initial creation of class for Refuel Event

---
+++ src/org/traccar/handler/events/RefuelEventHandler.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.handler.events;
+
+import io.netty.channel.ChannelHandler;
+import org.traccar.Context;
+import org.traccar.model.Device;
+import org.traccar.model.Event;
+import org.traccar.model.Position;
+
+import java.util.Collections;
+import java.util.Map;
+
+@ChannelHandler.Sharable
+public class RefuelEventHandler extends BaseEventHandler {
+
+    public static final String ATTRIBUTE_REFUEL_THRESHOLD = "refuelThreshold";
+
+    @Override
+    protected Map<Event, Position> analyzePosition(Position position) {
+
+        Device device = Context.getIdentityManager().getById(position.getDeviceId());
+        if (device == null) {
+            return null;
+        }
+        if (!Context.getIdentityManager().isLatestPosition(position)) {
+            return null;
+        }
+
+        double refuelThreshold = Context.getDeviceManager()
+                .lookupAttributeDouble(device.getId(), ATTRIBUTE_REFUEL_THRESHOLD, 0, false);
+
+        if (refuelThreshold > 0) {
+            Position lastPosition = Context.getIdentityManager().getLastPosition(position.getDeviceId());
+            if (position.getAttributes().containsKey(Position.KEY_FUEL_LEVEL)
+                    && lastPosition != null && lastPosition.getAttributes().containsKey(Position.KEY_FUEL_LEVEL)) {
+
+                double gain = position.getDouble(Position.KEY_FUEL_LEVEL)
+                        - lastPosition.getDouble(Position.KEY_FUEL_LEVEL); //Key Fuel is fuel tank level
+                if (gain >= refuelThreshold) {
+                    Event event = new Event(Event.TYPE_DEVICE_REFUEL, position.getDeviceId(), position.getId());
+                    event.set(ATTRIBUTE_REFUEL_THRESHOLD, refuelThreshold);
+                    return Collections.singletonMap(event, position);
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
