1816dcd505d0d5d8201ce2e8f78030569e70322f
Anton Tananaev <anton.tananaev@gmail.com>
2019-03-20 22:32:15 -0700

    Decode T800x satellites

--- src/org/traccar/protocol/T800xProtocolDecoder.java
+++ src/org/traccar/protocol/T800xProtocolDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 - 2018 Anton Tananaev (anton@traccar.org)
+ * Copyright 2015 - 2019 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,6 +112,7 @@ public class T800xProtocolDecoder extends BaseProtocolDecoder {
             buf.readUnsignedShort(); // speed alarm
 
             int status = buf.readUnsignedByte();
+            position.set(Position.KEY_SATELLITES, BitUtil.to(status, 5));
 
             buf.readUnsignedByte(); // gsensor manager status
             buf.readUnsignedByte(); // other flags
