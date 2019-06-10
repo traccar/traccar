a796c0317dc8c0598adb757eb6a77358c90767c3
Anton Tananaev <anton.tananaev@gmail.com>
2019-06-09 23:11:21 -0700

    Support JT600 protocol for JT701

--- src/main/java/org/traccar/protocol/Jt600ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/Jt600ProtocolDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 - 2018 Anton Tananaev (anton@traccar.org)
+ * Copyright 2012 - 2019 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -170,6 +170,7 @@ public class Jt600ProtocolDecoder extends BaseProtocolDecoder {
                 if (protocolVersion == 0x17) {
                     buf.readUnsignedByte(); // geofence id
                     buf.skipBytes(3); // reserved
+                    buf.skipBytes(buf.readableBytes() - 1);
                 }
 
             } else if (version == 1) {
