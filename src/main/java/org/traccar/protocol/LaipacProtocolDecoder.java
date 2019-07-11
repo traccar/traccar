9990000d67907cb41bdc18639d7dbe081c363f09
Edward Valley <ed.valley@yandex.com>
2019-07-10 21:29:27 -0400

    Fixed not receiving ignition off event

--- src/main/java/org/traccar/protocol/LaipacProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/LaipacProtocolDecoder.java
@@ -107,13 +107,17 @@ public class LaipacProtocolDecoder extends BaseProtocolDecoder {
 
     private String decodeEvent(String event, Position position) {
 
+        position.set(Position.KEY_IGNITION, false);
+
         if (event.length() == 1) {
             char inputStatus = event.charAt(0);
             if (inputStatus >= 'A' && inputStatus <= 'D') {
                 int inputStatusInt = inputStatus - 'A';
                 position.set(Position.PREFIX_IN + 1, inputStatusInt & 1);
                 position.set(Position.PREFIX_IN + 2, inputStatusInt & 2);
-                position.set(Position.KEY_IGNITION, ((inputStatusInt & 1) != 0) ? true : false);
+                if ((inputStatusInt & 1) != 0) {
+                    position.set(Position.KEY_IGNITION, true);
+                }
                 return null;
             }
         }
