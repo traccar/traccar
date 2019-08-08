b1d7e4d00072e8293051d502311a5f826eb95287
Anton Tananaev <anton.tananaev@gmail.com>
2019-08-08 00:13:18 -0700

    Add location test case

--- src/test/java/org/traccar/protocol/NavisetProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/NavisetProtocolDecoderTest.java
@@ -13,6 +13,9 @@ public class NavisetProtocolDecoderTest extends ProtocolTest {
         verifyNull(decoder, binary(
                 "1310e4073836383230343030353935383436362a060716"));
 
+        verifyPosition(decoder, binary(
+                "14501a2000a50c0955a64b5db8a92503fc2cf603000084ab"));
+
     }
 
 }
