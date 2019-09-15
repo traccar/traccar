1fe86dd41b3d20f2023844d56712b8d225160024
Anton Tananaev <anton.tananaev@gmail.com>
2019-09-15 13:48:39 -0700

    Fix analogue data decoding

--- src/main/java/org/traccar/protocol/DmtProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/DmtProtocolDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2018 Anton Tananaev (anton@traccar.org)
+ * Copyright 2017 - 2019 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -208,6 +208,7 @@ public class DmtProtocolDecoder extends BaseProtocolDecoder {
                                 position.set("solarPower", buf.readUnsignedShortLE() * 0.001);
                                 break;
                             default:
+                                buf.readUnsignedShortLE(); // other
                                 break;
                         }
                     }
