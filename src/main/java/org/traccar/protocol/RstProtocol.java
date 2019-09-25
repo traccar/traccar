3f4053552bde461ba2ef6686a83c08e2b1122472
Anton Tananaev <anton.tananaev@gmail.com>
2019-09-25 00:19:40 -0700

    Convert to UDP protocol

--- src/main/java/org/traccar/protocol/RstProtocol.java
+++ src/main/java/org/traccar/protocol/RstProtocol.java
@@ -18,17 +18,15 @@ package org.traccar.protocol;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import org.traccar.BaseProtocol;
-import org.traccar.CharacterDelimiterFrameDecoder;
 import org.traccar.PipelineBuilder;
 import org.traccar.TrackerServer;
 
 public class RstProtocol extends BaseProtocol {
 
     public RstProtocol() {
-        addServer(new TrackerServer(false, getName()) {
+        addServer(new TrackerServer(true, getName()) {
             @Override
             protected void addProtocolHandlers(PipelineBuilder pipeline) {
-                pipeline.addLast(new CharacterDelimiterFrameDecoder(1024, "FIM;"));
                 pipeline.addLast(new StringEncoder());
                 pipeline.addLast(new StringDecoder());
                 pipeline.addLast(new RstProtocolDecoder(RstProtocol.this));
