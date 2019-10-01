14f5711c4966f8fa38a49a5c9b62c7148ece06de
Anton Tananaev <anton.tananaev@gmail.com>
2019-09-30 23:34:10 -0700

    Add PT215 frame decoder

--- src/main/java/org/traccar/protocol/Pt215Protocol.java
+++ src/main/java/org/traccar/protocol/Pt215Protocol.java
@@ -15,7 +15,6 @@
  */
 package org.traccar.protocol;
 
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import org.traccar.BaseProtocol;
 import org.traccar.PipelineBuilder;
 import org.traccar.TrackerServer;
@@ -26,7 +25,7 @@ public class Pt215Protocol extends BaseProtocol {
         addServer(new TrackerServer(false, getName()) {
             @Override
             protected void addProtocolHandlers(PipelineBuilder pipeline) {
-                pipeline.addLast(new LengthFieldBasedFrameDecoder(1024, 2, 1, -1, 0));
+                pipeline.addLast(new Pt215FrameDecoder());
                 pipeline.addLast(new Pt215ProtocolDecoder(Pt215Protocol.this));
             }
         });
