e0d1934e8cadc6839064def68fdcc1187b50e3a9
RafaelMiquelino <rafaelmiquelino@gmail.com>
2019-10-23 21:08:21 -0300

    Added copyright header.

--- src/main/java/org/traccar/reports/model/TimeDistanceReport.java
+++ src/main/java/org/traccar/reports/model/TimeDistanceReport.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2016 - 2017 Anton Tananaev (anton@traccar.org)
  * Copyright 2016 - 2017 Andrey Kunitsyn (andrey@traccar.org)
+ * Copyright 2019 - Rafael Miquelino (rafaelmiquelino@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
