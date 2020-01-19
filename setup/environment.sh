8e6248b7ce258f6db50db6e66e829521b3ef1d1c
Anton Tananaev <anton.tananaev@gmail.com>
2020-01-18 21:59:00 -0800

    Update environment setup script

--- setup/environment.sh
+++ setup/environment.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+add-apt-repository ppa:openjdk-r/ppa
 apt update
-apt install openjdk-11-jdk-headless zip unzip innoextract wine-stable makeself
+apt install openjdk-11-jdk zip unzip innoextract wine makeself
+
+# /usr/bin/printf '\xfe\xed\xfe\xed\x00\x00\x00\x02\x00\x00\x00\x00\xe2\x68\x6e\x45\xfb\x43\xdf\xa4\xd9\x92\xdd\x41\xce\xb6\xb2\x1c\x63\x30\xd7\x92' > /etc/ssl/certs/java/cacerts
+# /var/lib/dpkg/info/ca-certificates-java.postinst configure
 
 git clone --recurse-submodules https://github.com/traccar/traccar.git
 (cd traccar/traccar-web && git checkout master)
-
-./gradlew assemble
+(cd traccar && ./gradlew assemble)
 
 wget http://cdn.sencha.com/ext/gpl/ext-6.2.0-gpl.zip
 unzip ext-*-gpl.zip ; rm ext-*-gpl.zip
 
-wget http://cdn.sencha.com/cmd/7.1.0.15/no-jre/SenchaCmd-7.1.0.15-linux-amd64.sh.zip
+wget http://cdn.sencha.com/cmd/7.1.0.15/no-jre/SenchaCmd-7.1.0.15-linux-i386.sh.zip
 unzip SenchaCmd-*.zip ; rm SenchaCmd-*.zip
 ./SenchaCmd-*.sh -q ; rm SenchaCmd-*
 export PATH=$PATH:~/bin/Sencha/Cmd/
