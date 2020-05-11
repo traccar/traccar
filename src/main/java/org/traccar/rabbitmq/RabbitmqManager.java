54f4a93658e6e5925e02967c2996a1a0fd3fe002
Parthib Dutta <duttaparthib@yahoo.in>
2020-05-11 23:29:37 +0530

    Make both queues and messages persistent and durable

--- src/main/java/org/traccar/rabbitmq/RabbitmqManager.java
+++ src/main/java/org/traccar/rabbitmq/RabbitmqManager.java
@@ -3,6 +3,7 @@ package org.traccar.rabbitmq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.traccar.config.Config;
@@ -31,15 +32,15 @@ public class RabbitmqManager {
         factory.setHost(config.getString(Keys.RABBITMQ_HOST_NAME));
         this.connection = factory.newConnection();
         this.channel = connection.createChannel();
-        this.channel.queueDeclare(this.config.getString(Keys.RABBITMQ_POSITIONS_QUEUE_NAME), false, false, false, null);
-        this.channel.queueDeclare(this.config.getString(Keys.RABBITMQ_EVENTS_QUEUE_NAME), false, false, false, null);
+        this.channel.queueDeclare(this.config.getString(Keys.RABBITMQ_POSITIONS_QUEUE_NAME), true, false, false, null);
+        this.channel.queueDeclare(this.config.getString(Keys.RABBITMQ_EVENTS_QUEUE_NAME), true, false, false, null);
     }
 
     public void sendToPositionsQueue(String message) throws IOException {
-        channel.basicPublish("", this.config.getString(Keys.RABBITMQ_POSITIONS_QUEUE_NAME), null, message.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish("", this.config.getString(Keys.RABBITMQ_POSITIONS_QUEUE_NAME), MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
     }
 
     public void sendToEventsQueue(String message) throws IOException {
-        channel.basicPublish("", this.config.getString(Keys.RABBITMQ_EVENTS_QUEUE_NAME), null, message.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish("", this.config.getString(Keys.RABBITMQ_EVENTS_QUEUE_NAME), MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
     }
 }
