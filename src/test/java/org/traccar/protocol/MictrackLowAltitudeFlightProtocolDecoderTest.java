5c4fbeb8f6f69c03dbc3c1100c619c132cc5edd3
Roeland Boeters <roeland@geodelta.com>
2020-06-26 10:23:11 +0200

    Add MictrackLowAltitudeFlightProtocolDecoderTest test for position

--- src/test/java/org/traccar/protocol/MictrackLowAltitudeFlightProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/MictrackLowAltitudeFlightProtocolDecoderTest.java
@@ -2,6 +2,7 @@ package org.traccar.protocol;
 
 import org.junit.Test;
 import org.traccar.ProtocolTest;
+import org.traccar.model.Position;
 
 public class MictrackLowAltitudeFlightProtocolDecoderTest extends ProtocolTest {
 
@@ -10,9 +11,16 @@ public class MictrackLowAltitudeFlightProtocolDecoderTest extends ProtocolTest {
 
         MictrackLowAltitudeFlightProtocolDecoder decoder = new MictrackLowAltitudeFlightProtocolDecoder(null);
 
-        verifyPositions(decoder, text("861108032038761$062232.00,A,2238.2832,N,11401.7381,E,0.01,309.62,95.0,131117"));
+        verifyPositions(decoder, text(
+                "861108032038761$062232.00,A,2238.2832,N,11401.7381,E,0.01,309.62,95.0,131117"));
 
-        verifyPositions(decoder, text("861108032038761$062232.00,A,2238.2832,N,11401.7381,E,0.01,309.62,95.0,131117$062332.00,A,2238.2836,N,11401.7386,E,0.06,209.62,95.0,131117"));
+        verifyPositions(decoder, text(
+                "861108032038761$062232.00,A,2238.2832,N,11401.7381,E,0.01,309.62,95.0,131117$"+
+                "062332.00,A,2238.2836,N,11401.7386,E,0.06,209.62,95.0,131117"));
+
+        verifyPositions(decoder, text(
+                "861108032038761$062232.00,A,2238.2832,N,11401.7381,E,0.01,309.62,95.0,131117"),
+                position("2017-11-13 06:22:32.000", true, 22.63806, 114.028976));
     }
 
 }
