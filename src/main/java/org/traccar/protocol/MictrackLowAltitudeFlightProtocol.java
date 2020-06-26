5e4a8f01d407122952968a1ce22903248d28d610
Roeland Boeters <roeland@geodelta.com>
2020-06-26 08:41:45 +0200

    Fix headers

--- src/main/java/org/traccar/protocol/MictrackLowAltitudeFlightProtocol.java
+++ src/main/java/org/traccar/protocol/MictrackLowAltitudeFlightProtocol.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2015 - 2018 Anton Tananaev (anton@traccar.org)
+ * Copyright 2020 Roeland Boeters (roeland@geodelta.com)
+ * Copyright 2020 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
