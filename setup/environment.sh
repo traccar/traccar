f4e218e232603fc6293db94dd62e7bc2239ec35b
Anton Tananaev <anton.tananaev@gmail.com>
2020-07-05 20:34:55 -0700

    Update env instructions

--- setup/environment.sh
+++ setup/environment.sh
@@ -2,14 +2,14 @@
 
 add-apt-repository ppa:openjdk-r/ppa
 apt update
-apt install openjdk-11-jdk zip unzip innoextract wine makeself
+apt install openjdk-11-jdk zip unzip innoextract wine-stable makeself
 
 # /usr/bin/printf '\xfe\xed\xfe\xed\x00\x00\x00\x02\x00\x00\x00\x00\xe2\x68\x6e\x45\xfb\x43\xdf\xa4\xd9\x92\xdd\x41\xce\xb6\xb2\x1c\x63\x30\xd7\x92' > /etc/ssl/certs/java/cacerts
 # /var/lib/dpkg/info/ca-certificates-java.postinst configure
 
 git clone --recurse-submodules https://github.com/traccar/traccar.git
 (cd traccar/traccar-web && git checkout master)
-(cd traccar && ./gradlew assemble)
+(cd traccar && ./gradlew build)
 
 wget http://cdn.sencha.com/ext/gpl/ext-6.2.0-gpl.zip
 unzip ext-*-gpl.zip ; rm ext-*-gpl.zip
