943cc8803f465bb53064df67c44c12533662e932
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-10-29 10:08:34 +0100

    Revert "chore: update commit changes"
    This reverts commit 813ae78f7b29747b614892f77e8b7dac66802050.

--- src/main/java/org/traccar/api/AsyncSocketServlet.java
+++ src/main/java/org/traccar/api/AsyncSocketServlet.java
@@ -15,30 +15,31 @@
  */
 package org.traccar.api;
 
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
+import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
-
 import org.traccar.Context;
+import org.traccar.api.resource.SessionResource;
 
-import static org.traccar.api.resource.SessionResource.USER_ID_KEY;
 public class AsyncSocketServlet extends WebSocketServlet {
 
     private static final long ASYNC_TIMEOUT = 10 * 60 * 1000;
+
     @Override
     public void configure(WebSocketServletFactory factory) {
         factory.getPolicy().setIdleTimeout(Context.getConfig().getLong("web.timeout", ASYNC_TIMEOUT));
-        factory.setCreator((req, resp) -> {
-            if (req.getSession() != null) {
-                long userId = (Long) req.getSession().getAttribute(USER_ID_KEY);
-                return new AsyncSocket(userId);
-            } else {
-                String userIdStr = req.getHeader("user_id");
-                if (userIdStr == null) {
+        factory.setCreator(new WebSocketCreator() {
+            @Override
+            public Object createWebSocket(ServletUpgradeRequest req, ServletUpgradeResponse resp) {
+                if (req.getSession() != null) {
+                    long userId = (Long) req.getSession().getAttribute(SessionResource.USER_ID_KEY);
+                    return new AsyncSocket(userId);
+                } else {
                     return null;
                 }
-                int userId = Integer.parseInt(userIdStr);
-                return new AsyncSocket(userId);
-	       }
+            }
         });
     }
 
