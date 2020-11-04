746cb693702b73f803e2394f01ed5566b7df059a
Anton Tananaev <anton.tananaev@gmail.com>
2020-11-03 16:27:54 -0800

    Another minor update

--- setup/environment.sh
+++ setup/environment.sh
@@ -4,7 +4,7 @@ add-apt-repository ppa:openjdk-r/ppa
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 dpkg --add-architecture i386
 apt update
-apt install -y git openjdk-11-jdk zip unzip innoextract wine32 makeself nodejs
+apt install -y git openjdk-11-jdk zip unzip innoextract wine wine32 makeself nodejs
 
 # /usr/bin/printf '\xfe\xed\xfe\xed\x00\x00\x00\x02\x00\x00\x00\x00\xe2\x68\x6e\x45\xfb\x43\xdf\xa4\xd9\x92\xdd\x41\xce\xb6\xb2\x1c\x63\x30\xd7\x92' > /etc/ssl/certs/java/cacerts
 # /var/lib/dpkg/info/ca-certificates-java.postinst configure
