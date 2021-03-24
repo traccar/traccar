be87b57c22b385bfc317d4e3e18e43877f46ca9b
Anton Tananaev <anton.tananaev@gmail.com>
2021-03-23 23:14:33 -0700

    Implement iStartek protocol

---
+++ src/main/java/org/traccar/protocol/StartekProtocolDecoder.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2021 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import io.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.DeviceSession;
+import org.traccar.Protocol;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.helper.UnitsConverter;
+import org.traccar.model.CellTower;
+import org.traccar.model.Network;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class StartekProtocolDecoder extends BaseProtocolDecoder {
+
+    public StartekProtocolDecoder(Protocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .text("&&")
+            .expression(".")                     // index
+            .number("d+,")                       // length
+            .number("(d+),")                     // imei
+            .number("xxx,")                      // command
+            .number("(d),")                      // event
+            .expression("[^,]*,")                // event data
+            .number("(dd)(dd)(dd)")              // date (yyymmdd)
+            .number("(dd)(dd)(dd),")             // time (hhmmss)
+            .expression("([AV]),")               // valid
+            .number("(-?d+.d+),")                // longitude
+            .number("(-?d+.d+),")                // latitude
+            .number("(d+),")                     // satellites
+            .number("(d+.d+),")                  // hdop
+            .number("(d+),")                     // speed
+            .number("(d+),")                     // course
+            .number("(-?d+),")                   // altitude
+            .number("(d+),")                     // odometer
+            .number("(d+)|")                     // mcc
+            .number("(d+)|")                     // mnc
+            .number("(x+)|")                     // lac
+            .number("(x+),")                     // cid
+            .number("(d+),")                     // rssi
+            .number("(x+),")                     // status
+            .number("(x+),")                     // inputs
+            .number("(x+),")                     // outputs
+            .number("(x+)|")                     // power
+            .number("(x+)|")                     // battery
+            .expression("([^,]+),")              // adc
+            .number("d,")                        // extended
+            .any()
+            .compile();
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        Parser parser = new Parser(PATTERN, (String) msg);
+        if (!parser.matches()) {
+            return null;
+        }
+
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, parser.next());
+        if (deviceSession == null) {
+            return null;
+        }
+
+        Position position = new Position(getProtocolName());
+        position.setDeviceId(deviceSession.getDeviceId());
+
+        position.set(Position.KEY_EVENT, parser.nextInt());
+
+        position.setTime(parser.nextDateTime());
+        position.setValid(parser.next().equals("A"));
+        position.setLatitude(parser.nextDouble());
+        position.setLongitude(parser.nextDouble());
+
+        position.set(Position.KEY_SATELLITES, parser.nextInt());
+        position.set(Position.KEY_HDOP, parser.nextDouble());
+
+        position.setSpeed(UnitsConverter.knotsFromKph(parser.nextInt()));
+        position.setCourse(parser.nextInt());
+        position.setAltitude(parser.nextInt());
+
+        position.set(Position.KEY_ODOMETER, parser.nextInt());
+
+        position.setNetwork(new Network(CellTower.from(
+                parser.nextInt(), parser.nextInt(), parser.nextHexInt(), parser.nextHexInt(), parser.nextInt())));
+
+        position.set(Position.KEY_STATUS, parser.nextHexInt());
+        position.set(Position.KEY_INPUT, parser.nextHexInt());
+        position.set(Position.KEY_OUTPUT, parser.nextHexInt());
+
+        position.set(Position.KEY_POWER, parser.nextHexInt() * 0.01);
+        position.set(Position.KEY_BATTERY, parser.nextHexInt() * 0.01);
+
+        String[] adc = parser.next().split("\\|");
+        for (int i = 0; i < adc.length; i++) {
+            position.set(Position.PREFIX_ADC + (i + 1), Integer.parseInt(adc[i], 16) * 0.01);
+        }
+
+        return position;
+    }
+
+}
---
+++ src/test/java/org/traccar/protocol/StartekProtocolDecoderTest.java
@@ -0,0 +1,21 @@
+package org.traccar.protocol;
+
+import org.junit.Test;
+import org.traccar.ProtocolTest;
+
+public class StartekProtocolDecoderTest extends ProtocolTest {
+
+    @Test
+    public void testDecode() throws Exception {
+
+        StartekProtocolDecoder decoder = new StartekProtocolDecoder(null);
+
+        verifyPosition(decoder, text(
+                "&&A147,021104023195429,000,0,,180106093046,A,22.646430,114.065730,8,0.9,54,86,76,326781,460|0|27B3|0EA7,27,0000000F,02,01,04E2|018C|01C8|0000,1,0104B0,01013D|02813546"));
+
+        verifyPosition(decoder, text(
+                "&&y139,860262050009146,000,0,,210323131512,A,22.678655,114.046223,14,1.1,0,231,71,5,460|0|249F|0099C257,28,0000003D,00,00,0493|0199|0000|0000,1,,33"));
+
+    }
+
+}
