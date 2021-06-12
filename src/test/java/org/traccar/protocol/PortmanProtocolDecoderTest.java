83c66842e675eee90f59080d37c919e0e7fed4df
Anton Tananaev <anton.tananaev@gmail.com>
2021-06-12 13:36:08 -0700

    Support extended Portman format

--- src/test/java/org/traccar/protocol/PortmanProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/PortmanProtocolDecoderTest.java
@@ -11,6 +11,9 @@ public class PortmanProtocolDecoderTest extends ProtocolTest {
         var decoder = new PortmanProtocolDecoder(null);
 
         verifyPosition(decoder, text(
+                "$EXT,P0RTMANGRANT,A,210609201710,N0951.6879W08357.0129,0,0,NA,NA,11,25,174700.25,NA,01820000,108"));
+
+        verifyPosition(decoder, text(
                 "$PTMLA,355854050074633,A,200612153351,N2543.0681W10009.2974,0,190,NA,C9830000,NA,108,8,2.66,16,GNA"));
 
     }
