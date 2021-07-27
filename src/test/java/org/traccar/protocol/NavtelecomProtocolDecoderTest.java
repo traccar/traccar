8e1b787a3a65bf05d820d27ca41b21476f21f496
Anton Tananaev <anton.tananaev@gmail.com>
2021-07-26 22:53:31 -0700

    Add test case

--- src/test/java/org/traccar/protocol/NavtelecomProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/NavtelecomProtocolDecoderTest.java
@@ -13,6 +13,9 @@ public class NavtelecomProtocolDecoderTest extends ProtocolTest {
         verifyNull(decoder, binary(
                 "404e5443010000000000000013004e452a3e533a383636373935303331343130363839"));
 
+        verifyNull(decoder, binary(
+                "404e544301000000000000001300f7fc2a3e464c4558b00a0a45fffe00000000000000"));
+
     }
 
 }
