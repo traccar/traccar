de76b91f467c2c4d1e393903ab3e3ae6c6167a5a
Anton Tananaev <anton.tananaev@gmail.com>
2021-09-24 23:21:50 -0700

    Remove unused import

--- src/main/java/org/traccar/protocol/SuntechProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/SuntechProtocolDecoder.java
@@ -34,7 +34,6 @@ import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.TimeZone;
 
 public class SuntechProtocolDecoder extends BaseProtocolDecoder {
