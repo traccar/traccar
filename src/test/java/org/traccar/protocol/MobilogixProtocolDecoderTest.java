f9823f2763689ee94645385dcd5e3427ec7255b0
jcardus <asklocation.net@gmail.com>
2021-10-12 00:48:48 +0100

    added a position with a T21 event in the test case

--- src/test/java/org/traccar/protocol/MobilogixProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/MobilogixProtocolDecoderTest.java
@@ -16,6 +16,9 @@ public class MobilogixProtocolDecoderTest extends ProtocolTest {
         verifyPosition(decoder, text(
                 "[2020-12-01 12:01:09,T3,1,V1.1.1,201951132031,3B,12.99,022,-23.563410,-46.588055,0,0"));
 
+        verifyPosition(decoder, text(
+                "[2021-09-30 20:06:35,T21,1,V1.3.5,201950130047,37,14.97,092,-23.494715,-46.851341,0,240,4.08,0,19516,4431,0.78,724,10,09111,00771,31,4680"));
+
     }
 
 }
