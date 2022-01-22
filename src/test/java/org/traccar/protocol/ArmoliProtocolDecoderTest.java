b918660950f459959c3a975c558448c907d6a3b4
Anton Tananaev <anton.tananaev@gmail.com>
2022-01-22 09:53:55 -0800

    Fix test case

--- src/test/java/org/traccar/protocol/ArmoliProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/ArmoliProtocolDecoderTest.java
@@ -11,12 +11,12 @@ public class ArmoliProtocolDecoderTest extends ProtocolTest {
 
         var decoder = new ArmoliProtocolDecoder(null);
 
-        verifyAttribute(decoder, text(
-                "[W869867038698074,O,1234,2657,1];"),
-                Position.KEY_RESULT, "O,1234,2657,1");
-
         verifyPosition(decoder, text(
                 "[M869867038698074210122125205N38.735641E035.4727751E003340000000C00000E9E07FF:106AG505283H60E];"));
+
+        verifyAttribute(decoder, text(
+                        "[W869867038698074,O,1234,2657,1];"),
+                Position.KEY_RESULT, "O,1234,2657,1");
 
         verifyNull(decoder, text(
                 "[Q010001088610010024363698990011101070608200,05XXXXXXXXX,10.49.182.53,C,1,20,19,0];"));
