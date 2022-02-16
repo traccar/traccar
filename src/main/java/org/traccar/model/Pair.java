b85d10bcc7baa7b7efafa82933a5abaaa8db2c92
Anton Tananaev <anton.tananaev@gmail.com>
2022-02-15 23:29:36 -0800

    Fix style issues

--- src/main/java/org/traccar/model/Pair.java
+++ src/main/java/org/traccar/model/Pair.java
@@ -23,22 +23,21 @@ public class Pair<K, V> {
     @SuppressWarnings("rawtypes")
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Pair pair = (Pair) o;
 
-        if (!Objects.equals(first, pair.first)) return false;
-        if (!Objects.equals(second, pair.second)) return false;
-
-        return true;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
     }
 
     @Override
     public int hashCode() {
-        int result = first != null ? first.hashCode() : 0;
-        result = 31 * result + (second != null ? second.hashCode() : 0);
-        return result;
+        return Objects.hash(first, second);
     }
 
 }
