2e769ee25bf7d134e4625cc5814661588adf7794
Anton Tananaev <anton.tananaev@gmail.com>
2022-02-15 23:17:31 -0800

    Implement memory permissions

---
+++ src/main/java/org/traccar/model/Pair.java
@@ -0,0 +1,44 @@
+package org.traccar.model;
+
+import java.util.Objects;
+
+public class Pair<K, V> {
+
+    private final K first;
+    private final V second;
+
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public K getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+
+        if (!Objects.equals(first, pair.first)) return false;
+        if (!Objects.equals(second, pair.second)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        return result;
+    }
+
+}
