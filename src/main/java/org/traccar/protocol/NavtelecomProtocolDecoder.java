90e50e3aafbbcdf3f2ac5698302b939c7149c578
Anton Tananaev <anton@traccar.org>
2022-03-01 22:22:04 -0800

    Fix Navtelecom checksum

--- src/main/java/org/traccar/protocol/NavtelecomProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/NavtelecomProtocolDecoder.java
@@ -239,7 +239,7 @@ public class NavtelecomProtocolDecoder extends BaseProtocolDecoder {
                     ByteBuf response = Unpooled.buffer();
                     response.writeCharSequence(type, StandardCharsets.US_ASCII);
                     response.writeByte(count);
-                    response.writeByte(Checksum.xor(response.nioBuffer()));
+                    response.writeByte(Checksum.crc8(Checksum.CRC8_ROHC, response.nioBuffer()));
                     channel.writeAndFlush(new NetworkMessage(response, remoteAddress));
                 }
 
