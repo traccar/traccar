bd51098a26f5d21cffe005f50d52f368d3e181c4
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-03-03 19:15:27 -0300

    position.set(Position.KEY_INPUT, BitUtil.check(status, 5));

--- src/main/java/org/traccar/protocol/MobilogixProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/MobilogixProtocolDecoder.java
@@ -127,8 +127,8 @@ public class MobilogixProtocolDecoder extends BaseProtocolDecoder {
         position.set(Position.KEY_BLOCKED, !BitUtil.check(status, 0));
         position.set(Position.KEY_IGNITION, BitUtil.check(status, 2));
         position.set(Position.KEY_MOTION, BitUtil.check(status, 3));
+        position.set(Position.KEY_INPUT, BitUtil.check(status, 5));
         position.set(Position.KEY_STATUS, status);
-
         position.set(Position.KEY_BATTERY, parser.nextDouble());
 
         if (parser.hasNext(7)) {
