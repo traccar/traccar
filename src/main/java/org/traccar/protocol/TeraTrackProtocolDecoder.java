18d8ec9610944aee95aa01ffbe402f3d0c30c39a
Anton Tananaev <anton@traccar.org>
2022-03-04 18:11:40 -0800

    Fix TeraTrack protocol

--- src/main/java/org/traccar/protocol/TeraTrackProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TeraTrackProtocolDecoder.java
@@ -16,9 +16,7 @@
 package org.traccar.protocol;
 
 import io.netty.channel.Channel;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import org.traccar.BaseHttpProtocolDecoder;
+import org.traccar.BaseProtocolDecoder;
 import org.traccar.DeviceSession;
 import org.traccar.Protocol;
 import org.traccar.helper.UnitsConverter;
@@ -28,12 +26,11 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import java.io.StringReader;
 import java.net.SocketAddress;
-import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
-public class TeraTrackProtocolDecoder extends BaseHttpProtocolDecoder {
+public class TeraTrackProtocolDecoder extends BaseProtocolDecoder {
 
     public TeraTrackProtocolDecoder(Protocol protocol) {
         super(protocol);
@@ -43,15 +40,12 @@ public class TeraTrackProtocolDecoder extends BaseHttpProtocolDecoder {
     protected Object decode(
             Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
 
-        FullHttpRequest request = (FullHttpRequest) msg;
-        JsonObject json = Json.createReader(
-                new StringReader(request.content().toString(StandardCharsets.US_ASCII))).readObject();
+        JsonObject json = Json.createReader(new StringReader((String) msg)).readObject();
 
         String deviceId = json.getString("MDeviceID");
         String imei = json.getString("IMEI");
         DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, deviceId, imei);
         if (deviceSession == null) {
-            sendResponse(channel, HttpResponseStatus.NOT_FOUND);
             return null;
         }
 
@@ -73,7 +67,6 @@ public class TeraTrackProtocolDecoder extends BaseHttpProtocolDecoder {
 
         position.set(Position.KEY_ODOMETER, Integer.parseInt(json.getString("Mileage")));
 
-        sendResponse(channel, HttpResponseStatus.OK);
         return position;
     }
 
--- src/test/java/org/traccar/protocol/TeraTrackProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/TeraTrackProtocolDecoderTest.java
@@ -1,6 +1,5 @@
 package org.traccar.protocol;
 
-import io.netty.handler.codec.http.HttpMethod;
 import org.junit.Test;
 import org.traccar.ProtocolTest;
 
@@ -11,8 +10,8 @@ public class TeraTrackProtocolDecoderTest extends ProtocolTest {
 
         var decoder = new TeraTrackProtocolDecoder(null);
 
-        verifyAttributes(decoder, request(HttpMethod.POST, "/",
-                buffer("{\"MDeviceID\":\"074054558620\",\"DeviceType\":\"1\",\"DataType\":\"2\",\"DataLength\":\"0913\",\"DateTime\":\"2022-02-22 23:35:35\",\"Latitude\":\"-6.826699\",\"Longitude\":\"39.279008\",\"LatitudeState\":\"0\",\"LongitudeState\":\"1\",\"Speed\":\"0\",\"Mileage\":\"0\",\"FenceAlarm\":\"0\",\"AreaAlarmID\":\"0\",\"LockCutOff\":\"0\",\"SealTempered\":\"1\",\"MessageAck\":\"1\",\"LockRope\":\"0\",\"LockStatus\":\"0\",\"LockOpen\":\"1\",\"PasswordError\":\"0\",\"CardNo\":\"60060198\",\"IllegalCard\":\"0\",\"LowPower\":\"0\",\"UnCoverBack\":\"1\",\"CoverStatus\":\"0\",\"LockStuck\":\"1\",\"Power\":\"90\",\"GSM\":\"14\",\"IMEI\":\"861774054558620\",\"Index\":\"39\",\"Slave\":[{\"SDeviceId\":\"685304\",\"SPower\":\"00\",\"SLockCutOff\":\"0\",\"SLockOpen\":\"1\",\"SUnCoverBack\":\"0\",\"SCoverStatus\":\"1\",\"STimeOut\":\"1\",\"SLockRope\":\"0\",\"SSealTempered\":\"0\",\"SLockStuck\":\"0\"},{\"SDeviceId\":\"224779\",\"SPower\":\"00\",\"SLockCutOff\":\"0\",\"SLockOpen\":\"1\",\"SUnCoverBack\":\"0\",\"SCoverStatus\":\"1\",\"STimeOut\":\"1\",\"SLockRope\":\"0\",\"SSealTempered\":\"0\",\"SLockStuck\":\"0\"}]}")));
+        verifyAttributes(decoder, text(
+                "{\"MDeviceID\":\"074054558620\",\"DeviceType\":\"1\",\"DataType\":\"2\",\"DataLength\":\"0913\",\"DateTime\":\"2022-02-22 23:35:35\",\"Latitude\":\"-6.826699\",\"Longitude\":\"39.279008\",\"LatitudeState\":\"0\",\"LongitudeState\":\"1\",\"Speed\":\"0\",\"Mileage\":\"0\",\"FenceAlarm\":\"0\",\"AreaAlarmID\":\"0\",\"LockCutOff\":\"0\",\"SealTempered\":\"1\",\"MessageAck\":\"1\",\"LockRope\":\"0\",\"LockStatus\":\"0\",\"LockOpen\":\"1\",\"PasswordError\":\"0\",\"CardNo\":\"60060198\",\"IllegalCard\":\"0\",\"LowPower\":\"0\",\"UnCoverBack\":\"1\",\"CoverStatus\":\"0\",\"LockStuck\":\"1\",\"Power\":\"90\",\"GSM\":\"14\",\"IMEI\":\"861774054558620\",\"Index\":\"39\",\"Slave\":[{\"SDeviceId\":\"685304\",\"SPower\":\"00\",\"SLockCutOff\":\"0\",\"SLockOpen\":\"1\",\"SUnCoverBack\":\"0\",\"SCoverStatus\":\"1\",\"STimeOut\":\"1\",\"SLockRope\":\"0\",\"SSealTempered\":\"0\",\"SLockStuck\":\"0\"},{\"SDeviceId\":\"224779\",\"SPower\":\"00\",\"SLockCutOff\":\"0\",\"SLockOpen\":\"1\",\"SUnCoverBack\":\"0\",\"SCoverStatus\":\"1\",\"STimeOut\":\"1\",\"SLockRope\":\"0\",\"SSealTempered\":\"0\",\"SLockStuck\":\"0\"}]}"));
 
     }
 
