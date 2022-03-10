35c2316a12113b29f00fa5ad735e59a88494c8bb
Anton Tananaev <anton@traccar.org>
2022-03-09 18:39:54 -0800

    Decode additional TeraTrack info

--- src/main/java/org/traccar/protocol/TeraTrackProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TeraTrackProtocolDecoder.java
@@ -62,6 +62,11 @@ public class TeraTrackProtocolDecoder extends BaseProtocolDecoder {
         position.setSpeed(UnitsConverter.knotsFromKph(Integer.parseInt(json.getString("Speed"))));
 
         position.set(Position.KEY_ODOMETER, Integer.parseInt(json.getString("Mileage")));
+        position.set(Position.KEY_BLOCKED, json.getString("LockOpen").equals("0"));
+        position.set(Position.KEY_DRIVER_UNIQUE_ID, json.getString("CardNo"));
+        position.set(Position.KEY_ALARM, json.getString("LowPower").equals("1") ? Position.ALARM_LOW_POWER : null);
+        position.set(Position.KEY_BATTERY_LEVEL, Integer.parseInt(json.getString("Power")));
+        position.set(Position.KEY_RSSI, Integer.parseInt(json.getString("GSM")));
 
         return position;
     }
