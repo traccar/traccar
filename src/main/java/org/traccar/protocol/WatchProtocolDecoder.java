5c7e7b086ebc9be1e797994c15d00e20814f23c7
wkhaksar <31837615+wkhaksar@users.noreply.github.com>
2022-06-28 15:41:02 +0200

    exclude mac address with zero value

--- src/main/java/org/traccar/protocol/WatchProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/WatchProtocolDecoder.java
@@ -166,7 +166,7 @@ public class WatchProtocolDecoder extends BaseProtocolDecoder {
                 index += 1; // wifi name
                 String macAddress = values[index++];
                 String rssi = values[index++];
-               if (!macAddress.isEmpty() && !rssi.isEmpty()) {
+               if (!macAddress.isEmpty() && macAddress!="0" && !rssi.isEmpty()) {
                    network.addWifiAccessPoint(WifiAccessPoint.from(macAddress, Integer.parseInt(rssi)));
                }
             }
