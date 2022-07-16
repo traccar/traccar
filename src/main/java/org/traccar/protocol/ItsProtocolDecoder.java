80bbf6e1b1903ba9218245c4fe94e5b482b59dad
Anton Tananaev <anton@traccar.org>
2022-07-16 07:13:21 -0700

    Support ITS custom timezone (fix #4232)

--- src/main/java/org/traccar/protocol/ItsProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/ItsProtocolDecoder.java
@@ -205,7 +205,8 @@ public class ItsProtocolDecoder extends BaseProtocolDecoder {
         if (parser.hasNext()) {
             position.setValid(parser.nextInt() == 1);
         }
-        position.setTime(parser.nextDateTime(Parser.DateTimeFormat.DMY_HMS));
+        position.setTime(parser.nextDateTime(
+                Parser.DateTimeFormat.DMY_HMS, getTimeZone(deviceSession.getDeviceId()).getID()));
         if (parser.hasNext()) {
             position.setValid(parser.next().matches("[1A]"));
         }
