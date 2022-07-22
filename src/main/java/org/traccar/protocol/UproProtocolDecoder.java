033e50725806fdd5597e37a24eff763800e1cdfa
Anton Tananaev <anton@traccar.org>
2022-07-21 18:36:12 -0700

    Fix validity decoding

--- src/main/java/org/traccar/protocol/UproProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/UproProtocolDecoder.java
@@ -71,7 +71,7 @@ public class UproProtocolDecoder extends BaseProtocolDecoder {
             position.setLongitude(parser.nextCoordinate(Parser.CoordinateFormat.DEG_MIN_MIN));
 
             int flags = parser.nextInt(0);
-            position.setValid(BitUtil.check(flags, 0));
+            position.setValid(!BitUtil.check(flags, 0));
             if (!BitUtil.check(flags, 1)) {
                 position.setLatitude(-position.getLatitude());
             }
