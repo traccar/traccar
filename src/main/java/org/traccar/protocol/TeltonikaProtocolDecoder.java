6ed644de74d6ab4770a0dd1968128bb854dd2114
jcardus <asklocation.net@gmail.com>
2022-09-29 14:19:52 -0300

    fix temp on teltonika

--- src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
@@ -230,7 +230,7 @@ public class TeltonikaProtocolDecoder extends BaseProtocolDecoder {
             case 26:
             case 27:
             case 28:
-                position.set(Position.PREFIX_TEMP + (id - 24), readValue(buf, length, true) * 0.1);
+                position.set(Position.PREFIX_TEMP + (id - 24), readValue(buf, length, true));
                 break;
             case 66:
                 position.set(Position.KEY_POWER, readValue(buf, length, false) * 0.001);
