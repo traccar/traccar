73b174094effa2a2c9c3a076b5c244ba832a90c2
Anton Tananaev <anton@traccar.org>
2022-10-20 05:54:29 -0700

    Add Minifinder HDOP

--- src/main/java/org/traccar/protocol/Minifinder2ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/Minifinder2ProtocolDecoder.java
@@ -198,7 +198,9 @@ public class Minifinder2ProtocolDecoder extends BaseProtocolDecoder {
                         position.setSpeed(UnitsConverter.knotsFromKph(buf.readUnsignedShortLE()));
                         position.setCourse(buf.readUnsignedShortLE());
                         position.setAltitude(buf.readShortLE());
-                        position.setValid(buf.readUnsignedShortLE() > 0);
+                        int hdop = buf.readUnsignedShortLE();
+                        position.setValid(hdop > 0);
+                        position.set(Position.KEY_HDOP, hdop * 0.1);
                         position.set(Position.KEY_ODOMETER, buf.readUnsignedIntLE());
                         position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
                         break;
