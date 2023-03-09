075d6ba2c01980a3a0ca65e67163ec63bb9b8472
jcardus <asklocation.net@gmail.com>
2023-03-09 12:32:17 +0000

    default url

--- src/main/java/org/traccar/database/DataManager.java
+++ src/main/java/org/traccar/database/DataManager.java
@@ -359,8 +359,10 @@ public class DataManager {
     public Collection<Position> getPositions(long deviceId, Date from, Date to) throws SQLException {
         if (from.before(new Date(122, 11, 1))) {
             try {
-                LOGGER.info("recovering positions deviceId " + deviceId);
-                getRequestBuilder(Context.getConfig().getString(Keys.REPORTS_RECOVER_POSITIONS_URL)).post(Entity.json(deviceId));
+                String url = Context.getConfig().getString(Keys.REPORTS_RECOVER_POSITIONS_URL,
+                        "https://api.pinme.io/reports/positions/recover");
+                LOGGER.info("recovering positions deviceId {} {}", deviceId, url);
+                getRequestBuilder(url).post(Entity.json(deviceId));
             } catch (Exception e) {
                 LOGGER.error("getPositions error", e);
             }
