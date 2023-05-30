b508a937ee5d9619320d1b82820e4b181fbc70a5
merabtenei <i.merabtene.gd@gmail.com>
2023-05-30 09:01:31 +0100

    fixed copyright

--- src/main/java/org/traccar/notificators/NotificatorViber.java
+++ src/main/java/org/traccar/notificators/NotificatorViber.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2019 - 2023 Anton Tananaev (anton@traccar.org)
- * Copyright 2021 Rafael Miquelino (rafaelmiquelino@gmail.com)
+ * Copyright 2023 I. Merabtene (i.merabtene.gd@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
