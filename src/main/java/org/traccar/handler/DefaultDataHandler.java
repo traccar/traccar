6f850c95a6b3dc7d4d19fa5c48ed9bbb081be653
jcardus <asklocation.net@gmail.com>
2023-06-07 21:53:40 +0100

    start with log

--- src/main/java/org/traccar/handler/DefaultDataHandler.java
+++ src/main/java/org/traccar/handler/DefaultDataHandler.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import org.traccar.BaseDataHandler;
 import org.traccar.database.DataManager;
 import org.traccar.model.Position;
+import redis.clients.jedis.Jedis;
 
 @ChannelHandler.Sharable
 public class DefaultDataHandler extends BaseDataHandler {
@@ -29,8 +30,11 @@ public class DefaultDataHandler extends BaseDataHandler {
 
     private final DataManager dataManager;
 
+    private final Jedis jedis;
+
     public DefaultDataHandler(DataManager dataManager) {
         this.dataManager = dataManager;
+        this.jedis = new Jedis("redis.pinme.io");
     }
 
     @Override
@@ -38,6 +42,7 @@ public class DefaultDataHandler extends BaseDataHandler {
 
         try {
             dataManager.addObject(position);
+            LOGGER.warn("redis position id would be {}", jedis.incr("dbid"));
         } catch(com.mysql.cj.jdbc.exceptions.MysqlDataTruncation error) {
             LOGGER.warn("Failed to store position, deviceId: {}, {}", position.getDeviceId(), error.getMessage());
         } catch (Exception error) {
