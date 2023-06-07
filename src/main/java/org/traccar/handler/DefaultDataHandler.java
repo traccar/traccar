65048bad73d8248455c2dfd9e3f4a41aba56fa55
jcardus <asklocation.net@gmail.com>
2023-06-07 22:11:51 +0100

    Revert "start with log"
    This reverts commit 6f850c95a6b3dc7d4d19fa5c48ed9bbb081be653.

--- src/main/java/org/traccar/handler/DefaultDataHandler.java
+++ src/main/java/org/traccar/handler/DefaultDataHandler.java
@@ -21,7 +21,6 @@ import org.slf4j.LoggerFactory;
 import org.traccar.BaseDataHandler;
 import org.traccar.database.DataManager;
 import org.traccar.model.Position;
-import redis.clients.jedis.Jedis;
 
 @ChannelHandler.Sharable
 public class DefaultDataHandler extends BaseDataHandler {
@@ -30,11 +29,8 @@ public class DefaultDataHandler extends BaseDataHandler {
 
     private final DataManager dataManager;
 
-    private final Jedis jedis;
-
     public DefaultDataHandler(DataManager dataManager) {
         this.dataManager = dataManager;
-        this.jedis = new Jedis("redis.pinme.io");
     }
 
     @Override
@@ -42,7 +38,6 @@ public class DefaultDataHandler extends BaseDataHandler {
 
         try {
             dataManager.addObject(position);
-            LOGGER.warn("redis position id would be {}", jedis.incr("dbid"));
         } catch(com.mysql.cj.jdbc.exceptions.MysqlDataTruncation error) {
             LOGGER.warn("Failed to store position, deviceId: {}, {}", position.getDeviceId(), error.getMessage());
         } catch (Exception error) {
