104e8a4a7829b4b4a4723a715cc2b15899fa8b9f
Anton Tananaev <anton@traccar.org>
2023-07-14 06:49:08 -0700

    Option to disable Topic commands

--- src/main/java/org/traccar/config/Keys.java
+++ src/main/java/org/traccar/config/Keys.java
@@ -234,6 +234,13 @@ public final class Keys {
             List.of(KeyType.CONFIG, KeyType.DEVICE));
 
     /**
+     * Disable commands for the protocol. Not all protocols support this option.
+     */
+    public static final ConfigSuffix<Boolean> PROTOCOL_DISABLE_COMMANDS = new BooleanConfigSuffix(
+            ".disableCommands",
+            List.of(KeyType.CONFIG));
+
+    /**
      * Protocol format. Used by protocols that have configurable message format.
      */
     public static final ConfigSuffix<String> PROTOCOL_FORMAT = new StringConfigSuffix(
