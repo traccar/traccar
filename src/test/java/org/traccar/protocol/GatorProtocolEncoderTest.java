1dfc61e3e981a23060d02c46451b19a600a87fa0
seym45 <seym45@gmail.com>
2023-07-27 11:27:44 +0400

    Remove explicit test encodeId
    - encodeId tested inside testEncode

--- src/test/java/org/traccar/protocol/GatorProtocolEncoderTest.java
+++ src/test/java/org/traccar/protocol/GatorProtocolEncoderTest.java
@@ -1,30 +1,13 @@
 package org.traccar.protocol;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import org.junit.jupiter.api.Test;
 import org.traccar.ProtocolTest;
 import org.traccar.model.Command;
 import org.traccar.model.Device;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 public class GatorProtocolEncoderTest extends ProtocolTest {
-
-    @Test
-    void testEncodeId() throws Exception {
-        var encoder = inject(new GatorProtocolEncoder(null));
-        var device = encoder.getCacheManager().getObject(Device.class, 1);
-        when(device.getUniqueId()).thenReturn("13332082112");
-
-        ByteBuf pseudoId = Unpooled.buffer();
-        pseudoId.writeByte(0x20);
-        pseudoId.writeByte(0x08);
-        pseudoId.writeByte(0x95);
-        pseudoId.writeByte(0x8C);
-        assertEquals(pseudoId, encoder.encodeId(1));
-    }
 
     @Test
     public void testEncode() throws Exception {
