36d319a81dc3d2c12222cce012c1a1a307a865b2
Anton Tananaev <anton@traccar.org>
2023-08-06 07:45:59 -0700

    Remove unused import

--- src/main/java/org/traccar/protocol/CastelProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/CastelProtocolDecoder.java
@@ -16,7 +16,6 @@
 package org.traccar.protocol;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import org.traccar.BaseProtocolDecoder;
