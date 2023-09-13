ace4f295159bc4d4ef22393fb5368b8053213483
jcardus <asklocation.net@gmail.com>
2023-09-13 22:24:54 +0100

    filter positions without jumps but with invalid speed

--- src/main/java/org/traccar/handler/FilterHandler.java
+++ src/main/java/org/traccar/handler/FilterHandler.java
@@ -159,7 +159,8 @@ public class FilterHandler extends ChannelInboundHandlerAdapter {
         if (filterMaxSpeed != 0 && last != null) {
             double distance = position.getDouble(Position.KEY_DISTANCE);
             double time = position.getFixTime().getTime() - last.getFixTime().getTime();
-            return UnitsConverter.knotsFromMps(distance / (time / 1000)) > filterMaxSpeed;
+            return UnitsConverter.knotsFromMps(distance / (time / 1000)) > filterMaxSpeed
+                || position.getSpeed() > filterMaxSpeed;
         }
         return false;
     }
