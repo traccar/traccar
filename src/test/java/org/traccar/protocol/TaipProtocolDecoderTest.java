88fdb4e19ba4f4f13deaefa64af1c5ad6896e655
Anton Tananaev <anton@traccar.org>
2023-11-15 07:43:01 -0800

    Add Lantrix t-1700 test case

--- src/test/java/org/traccar/protocol/TaipProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/TaipProtocolDecoderTest.java
@@ -11,6 +11,9 @@ public class TaipProtocolDecoderTest extends ProtocolTest {
 
         var decoder = inject(new TaipProtocolDecoder(null));
 
+        verifyNull(decoder, text(
+                ">RTT151123153149-4330468-06503640000009300DF2101 04101203 000 00000000130000040414;ID=8803;#1ABD;*2B<"));
+
         verifyAttribute(decoder, text(
                 ">RUS00,111220124402-3138067-06417623000012200FF,000000000000000000000000000,0000000111,15640422,00000,+25.5,00000,51;ID=CST3G0443;#IP1:089F;*34<"),
                 Position.PREFIX_TEMP + 1, 25.5);
