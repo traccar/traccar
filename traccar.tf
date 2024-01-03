f2512938a0a5a5d432d49bf1a3cd7d47af893a26
jcardus <asklocation.net@gmail.com>
2024-01-03 23:58:09 +0000

    security group env vars start single instance

--- traccar.tf
+++ traccar.tf
@@ -3,12 +3,54 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_security_group" "traccar-sg" {
+  name        = "traccar-sg"
+  ingress {
+    description = "devices tcp"
+    from_port   = 5000
+    to_port     = 5500
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "devices udp"
+    from_port   = 5000
+    to_port     = 5500
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "api"
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "nginx proxy"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "traccar-sg"
+  }
+}
+
 resource "aws_elastic_beanstalk_application" "traccar" {
   name        = "traccar-tf"
 }
 
 resource "aws_elastic_beanstalk_environment" "traccar-env" {
-  source = "."
   name                = "traccar-env"
   application         = aws_elastic_beanstalk_application.traccar.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.1.2 running Corretto 11"
@@ -16,6 +58,11 @@ resource "aws_elastic_beanstalk_environment" "traccar-env" {
         namespace = "aws:autoscaling:launchconfiguration"
         name = "IamInstanceProfile"
         value = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "SecurityGroups"
+    value = aws_security_group.traccar-sg.name
   }
   setting {
     namespace = "aws:elasticbeanstalk:cloudwatch:logs"
@@ -48,5 +95,22 @@ resource "aws_elastic_beanstalk_environment" "traccar-env" {
     namespace = "aws:elbv2:listener:443"
     name = "SSLCertificateArns"
     value = var.loadbalancer_certificate_arn
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance"
+  }
+
+  # Add environment variables if provided
+  dynamic "setting" {
+    for_each = var.env_vars
+    content {
+      namespace = "aws:elasticbeanstalk:application:environment"
+      name      = setting.key
+      value     = setting.value
+      resource  = ""
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,12 @@ variable "loadbalancer_certificate_arn" {
   default     = "arn:aws:acm:us-east-1:925447205804:certificate/f48c082e-04ff-4586-818a-53ec8a51eecd"
   description = "Load Balancer SSL certificate ARN. The certificate must be present in AWS Certificate Manager"
 }
+
+variable "env_vars" {
+  type        = map(string)
+  default     = {
+    CONFIG_USE_ENVIRONMENT_VARIABLES = "true"
+    DATABASE_PASSWORD = ""
+  }
+  description = "Map of custom ENV variables to be provided to the application running on Elastic Beanstalk, e.g. env_vars = { DB_USER = 'admin' DB_PASS = 'xxxxxx' }"
+}
