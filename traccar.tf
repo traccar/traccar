baf10aae157da796e5e5c1e46c1c8a5a75943517
jcardus <asklocation.net@gmail.com>
2024-01-04 01:15:28 +0000

    epel

--- traccar.tf
+++ traccar.tf
@@ -42,6 +42,14 @@ resource "aws_security_group" "traccar-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
