16a3d553f506940962a1fadcd085982113b6a81d
jcardus <asklocation.net@gmail.com>
2024-01-04 01:42:49 +0000

    use packages

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "loadbalancer_certificate_arn" {
-  type        = string
-  default     = "arn:aws:acm:us-east-1:925447205804:certificate/f48c082e-04ff-4586-818a-53ec8a51eecd"
-  description = "Load Balancer SSL certificate ARN. The certificate must be present in AWS Certificate Manager"
-}
-
 variable "env_vars" {
   type        = map(string)
   default     = {
