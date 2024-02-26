89fba5afc13ab885554a8eaab41722f31f3642f4
Anton Tananaev <anton@traccar.org>
2024-02-25 16:59:23 -0800

    Update notification message model

--- src/main/java/org/traccar/notification/NotificationMessage.java
+++ src/main/java/org/traccar/notification/NotificationMessage.java
@@ -16,12 +16,16 @@
  */
 package org.traccar.notification;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class NotificationMessage {
 
     private final String subject;
     private final String body;
 
-    public NotificationMessage(String subject, String body) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public NotificationMessage(@JsonProperty("subject") String subject, @JsonProperty("body") String body) {
         this.subject = subject;
         this.body = body;
     }
