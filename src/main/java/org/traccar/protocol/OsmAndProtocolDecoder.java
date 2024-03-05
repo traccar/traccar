5b587664f509a2ce9863df5d17ce0ca63af9f464
j <j>
2024-03-05 22:52:50 +0000

    allow receive positionid and protocol on osmand protocol

--- src/main/java/org/traccar/protocol/OsmAndProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/OsmAndProtocolDecoder.java
@@ -69,6 +69,9 @@ public class OsmAndProtocolDecoder extends BaseHttpProtocolDecoder {
         for (Map.Entry<String, List<String>> entry : params.entrySet()) {
             for (String value : entry.getValue()) {
                 switch (entry.getKey()) {
+                    case "positionid":
+                        position.setId(Long.parseLong(value));
+                        break;
                     case "id":
                     case "deviceid":
                         DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, value);
@@ -153,6 +156,9 @@ public class OsmAndProtocolDecoder extends BaseHttpProtocolDecoder {
                         break;
                     case "accuracy":
                         position.setAccuracy(Double.parseDouble(value));
+                        break;
+                    case "protocol":
+                        position.setProtocol(value);
                         break;
                     case "hdop":
                         position.set(Position.KEY_HDOP, Double.parseDouble(value));
