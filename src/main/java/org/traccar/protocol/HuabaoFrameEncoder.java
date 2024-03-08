43508913a7f3356324ec5af447980121d9c64dd0
Anton Tananaev <anton@traccar.org>
2024-03-08 09:38:03 -0800

    Add Huabao frame encoder

---
+++ src/main/java/org/traccar/protocol/HuabaoFrameEncoder.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class HuabaoFrameEncoder extends MessageToByteEncoder<ByteBuf> {
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) {
+
+        while (msg.isReadable()) {
+            int b = msg.readUnsignedByte();
+            switch (b) {
+                case 0x7d:
+                    out.writeByte(0x7d);
+                    out.writeByte(0x01);
+                    break;
+                case 0x7e:
+                    out.writeByte(0x7d);
+                    out.writeByte(0x02);
+                    break;
+                default:
+                    out.writeByte(b);
+                    break;
+            }
+        }
+    }
+}
