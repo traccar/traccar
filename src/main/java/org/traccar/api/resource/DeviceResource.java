b099b298f90074c825ba68ce73532933c7b9d901
Anton Tananaev <anton@traccar.org>
2024-04-04 08:03:16 -0700

    Remove extra dot

--- src/main/java/org/traccar/api/resource/DeviceResource.java
+++ src/main/java/org/traccar/api/resource/DeviceResource.java
@@ -175,15 +175,15 @@ public class DeviceResource extends BaseObjectResource<Device> {
     private String imageExtension(String type) {
         switch (type) {
             case "image/jpeg":
-                return ".jpg";
+                return "jpg";
             case "image/png":
-                return ".png";
+                return "png";
             case "image/gif":
-                return ".gif";
+                return "gif";
             case "image/webp":
-                return ".webp";
+                return "webp";
             case "image/svg+xml":
-                return ".svg";
+                return "svg";
             default:
                 throw new IllegalArgumentException("Unsupported image type");
         }
