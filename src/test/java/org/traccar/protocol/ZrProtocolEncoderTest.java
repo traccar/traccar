a0a31a2f39938fb059eb2ee8aba5e109813f30d0
KeDa <jiangqingtai@zhongruigroup.com>
2024-04-26 09:11:04 +0800

    rename decode2310 to decodeAlarmTag

--- src/test/java/org/traccar/protocol/ZrProtocolEncoderTest.java
+++ src/test/java/org/traccar/protocol/ZrProtocolEncoderTest.java
@@ -14,7 +14,7 @@ public class ZrProtocolEncoderTest extends ProtocolTest {
         Command command = new Command();
         command.setDeviceId(1);
         command.setType(Command.TYPE_SET_CONNECTION);
-        command.set(Command.KEY_PORT, 5252);
+        command.set(Command.KEY_PORT, 5257);
         command.set(Command.KEY_SERVER, "127.0.0.1");
 
         verifyCommand(encoder, command, binary("dddd0034104000000001234567890123450700110001202391000312345624c000150302011484093132372e302e302e3100000000012c12ffff"));
