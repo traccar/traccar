f74daa225dbd647720b5428e17f65a6a70fa435b
jcardus <asklocation.net@gmail.com>
2024-05-22 14:59:13 +0100

    80

--- src/main/java/org/traccar/reports/ReportUtils.java
+++ src/main/java/org/traccar/reports/ReportUtils.java
@@ -136,7 +136,7 @@ public final class ReportUtils {
 
         // can data can vary 200 meters in one second
         int maxSpeed = start.getAttributes().containsKey(Position.KEY_ODOMETER) && t < 5000 ?
-                1000 : (xpertPosition(start) ? 100 : 200);
+                1000 : (xpertPosition(start) ? 80 : 200);
 
         LOGGER.error("avgSpeed: " + averageSpeed + " " + maxSpeed + " " +  start.getDeviceId() + " " + start.getFixTime() +
                 " " + start.getAttributes().containsKey(Position.KEY_ODOMETER) + " " + start.getProtocol() + " " +
