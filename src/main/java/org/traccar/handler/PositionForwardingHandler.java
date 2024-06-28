39d1c04613312929f6d5e6d952edf0a0d54d087f
Michel Verbraak <michel.verbraak@politie.nl>
2024-06-28 08:45:21 +0200

    Minor cleanup

--- src/main/java/org/traccar/handler/PositionForwardingHandler.java
+++ src/main/java/org/traccar/handler/PositionForwardingHandler.java
@@ -20,7 +20,6 @@ import io.netty.util.Timer;
 import io.netty.util.TimerTask;
 import jakarta.annotation.Nullable;
 import jakarta.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.traccar.config.Config;
