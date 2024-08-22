51d29920fcd76713d029abc1a045bb345012ffbc
jcardus <joaquim.cardeira@gmail.com>
2024-08-22 15:42:33 +0100

    remove delete

--- src/main/java/org/traccar/api/resource/GeofenceResource.java
+++ src/main/java/org/traccar/api/resource/GeofenceResource.java
@@ -23,6 +23,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.PathParam;
 import java.sql.SQLException;
 
 @Path("geofences")
