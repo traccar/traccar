384af6b53aeaf5aa8c4e6758023c88b11b496d8c
Nikutrax <112165998+Nikutrax@users.noreply.github.com>
2024-09-27 16:20:05 +0200

    Update TrvProtocolDecoder.java

--- src/main/java/org/traccar/protocol/TrvProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TrvProtocolDecoder.java
@@ -180,7 +180,7 @@ public class TrvProtocolDecoder extends BaseProtocolDecoder {
                 channel.writeAndFlush(new NetworkMessage(responseHeader + "," + time + ",0#", remoteAddress));
             } else if (type.equals("AP14") && !id.equals("IW")) {
                 channel.writeAndFlush(new NetworkMessage(responseHeader + ",0.000,0.000#", remoteAddress));
-            } else if (!type.equals("AP12") && !type.equals("AP14")
+            } else if (!type.equals("AP12") && !type.equals("AP14") && !type.equals("AP33") && !type.equals("AP34") && !type.equals("AP84") && !type.equals("AP85")
                     && !sentence.substring(responseHeader.length() + 1).matches("^\\d{6}$")) {
                 channel.writeAndFlush(new NetworkMessage(responseHeader + "#", remoteAddress));
             }
