1476c7a8655bc4b8bc6b2e970479f66d33797c99
luntim <timothylundberg98@gmail.com>
2024-11-27 10:58:38 +0100

    merged new code into existing code

--- src/main/java/org/traccar/protocol/Gt06ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/Gt06ProtocolDecoder.java
@@ -665,7 +665,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
 
         } else if (type == MSG_LBS_MULTIPLE_1 || type == MSG_LBS_MULTIPLE_2 || type == MSG_LBS_MULTIPLE_3
                 || type == MSG_LBS_EXTEND || type == MSG_LBS_WIFI || type == MSG_LBS_2 || type == MSG_LBS_3
-                || (type == MSG_WIFI_3 && variant != Variant.LW4G) || type == MSG_WIFI_5) {
+                || (type == MSG_WIFI_3 && variant != Variant.LW4G) || type == MSG_WIFI_5 || type == MSG_WIFI_ALARM) {
 
             DateBuilder dateBuilder = new DateBuilder((TimeZone) deviceSession.get(DeviceSession.KEY_TIMEZONE))
                     .setDate(buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte())
@@ -680,13 +680,20 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
             int mcc = buf.readUnsignedShort();
             int mnc = BitUtil.check(mcc, 15) || variant == Variant.SL4X
                     ? buf.readUnsignedShort() : buf.readUnsignedByte();
+            if (type == MSG_WIFI_ALARM) {
+                mcc = mcc & 0x7FFF;
+            }
             Network network = new Network();
 
-            int cellCount = variant == Variant.WANWAY_S20 ? buf.readUnsignedByte() : type == MSG_WIFI_5 ? 6 : 7;
+            int cellType = 0;
+            if (type == MSG_WIFI_ALARM) {
+                cellType = buf.readUnsignedByte();
+            }
+            int cellCount = variant == Variant.WANWAY_S20 || type == MSG_WIFI_ALARM ? buf.readUnsignedByte() : type == MSG_WIFI_5 ? 6 : 7;
             for (int i = 0; i < cellCount; i++) {
                 int lac;
                 int cid;
-                if (type == MSG_LBS_2 || type == MSG_WIFI_3) {
+                if (type == MSG_LBS_2 || type == MSG_WIFI_3 || cellType == 0x01) {
                     lac = buf.readInt();
                     cid = (int) buf.readLong();
                 } else if (type == MSG_WIFI_5 || type == MSG_LBS_3) {
@@ -703,7 +710,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
             }
 
             if (variant != Variant.WANWAY_S20 && variant != Variant.SL4X) {
-                buf.readUnsignedByte(); // ta
+                position.set("timingAdvance", buf.readUnsignedByte()); // ta
             }
 
             if (type != MSG_LBS_MULTIPLE_1 && type != MSG_LBS_MULTIPLE_2 && type != MSG_LBS_MULTIPLE_3
@@ -711,12 +718,54 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
                 int wifiCount = buf.readUnsignedByte();
                 for (int i = 0; i < wifiCount; i++) {
                     String mac = ByteBufUtil.hexDump(buf.readSlice(6)).replaceAll("(..)", "$1:");
+                    int signalStrength = buf.readUnsignedByte();
                     network.addWifiAccessPoint(WifiAccessPoint.from(
-                            mac.substring(0, mac.length() - 1), buf.readUnsignedByte()));
+                            mac.substring(0, mac.length() - 1), signalStrength));
                 }
             }
 
             position.setNetwork(network);
+
+            if (type == MSG_WIFI_ALARM) {
+
+                int event = buf.readUnsignedByte();
+                int languageDetails = buf.readUnsignedByte();
+            
+                if (languageDetails == 0x01) {
+                    position.set("alertLanguage", "Chinese");
+                } else if (languageDetails == 0x02) {
+                    position.set("alertLanguage", "English");
+                } else if (languageDetails == 0x00) {
+                    // no reply needed
+                }
+                position.set(Position.KEY_EVENT, event);
+                switch (event) {
+                    case 0x00 -> position.addAlarm(Position.ALARM_GENERAL);
+                    case 0x01 -> position.addAlarm(Position.ALARM_SOS);
+                    case 0x02 -> position.addAlarm(Position.ALARM_POWER_CUT);
+                    case 0x03 -> position.addAlarm(Position.ALARM_VIBRATION);
+                    case 0x04 -> position.addAlarm(Position.ALARM_GEOFENCE_ENTER);
+                    case 0x05 -> position.addAlarm(Position.ALARM_GEOFENCE_EXIT);
+                    case 0x06 -> position.addAlarm(Position.ALARM_OVERSPEED);
+                    case 0x09 -> position.addAlarm(Position.ALARM_TOW);
+                    case 0x0A -> position.addAlarm(Position.ALARM_GPS_ANTENNA_CUT);
+                    case 0x0B -> position.addAlarm(Position.ALARM_GPS_ANTENNA_CUT);
+                    case 0x0C -> position.addAlarm(Position.ALARM_POWER_ON);
+                    case 0x0D -> position.addAlarm(Position.ALARM_GPS_ANTENNA_CUT);
+                    case 0x0E -> position.addAlarm(Position.ALARM_LOW_POWER);
+                    case 0x0F -> position.addAlarm(Position.ALARM_LOW_POWER);
+                    case 0x10 -> position.addAlarm(Position.ALARM_TAMPERING);
+                    case 0x11 -> position.addAlarm(Position.ALARM_POWER_OFF);
+                    case 0x12 -> position.addAlarm(Position.ALARM_GENERAL);
+                    case 0x13 -> position.addAlarm(Position.ALARM_TAMPERING);
+                    case 0x14 -> position.addAlarm(Position.ALARM_DOOR);
+                    case 0x15 -> position.addAlarm(Position.ALARM_POWER_OFF);
+                    case 0x16 -> position.addAlarm(Position.ALARM_TAMPERING);
+                    case 0x17 -> position.addAlarm(Position.ALARM_TAMPERING);
+                    case 0x18 -> position.addAlarm(Position.ALARM_REMOVING);
+                    case 0x19 -> position.addAlarm(Position.ALARM_LOW_BATTERY);
+                }
+            }
 
         } else if (type == MSG_STRING) {
 
@@ -1034,96 +1083,6 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
             if (filesLength > 0) {
                 position.set("eventFiles", buf.readCharSequence(filesLength, StandardCharsets.US_ASCII).toString());
             }
-
-        } else if (type == MSG_WIFI_ALARM) {
-
-            DateBuilder dateBuilder = new DateBuilder((TimeZone) deviceSession.get(DeviceSession.KEY_TIMEZONE))
-                    .setDate(buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte())
-                    .setTime(buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte());
-
-            position.setDeviceTime(dateBuilder.getDate());
-
-            int mcc = buf.readUnsignedShort();
-            int msb = (mcc & 0x8000) >> 15;
-            int actualMcc = mcc & 0x7FFF;
-
-            int mnc = 0;
-            if (msb == 1) {
-                mnc = buf.readUnsignedShort();
-            } else if (msb == 0) {
-                mnc = buf.readUnsignedByte();
-            }
-
-            int cellType = buf.readUnsignedByte();
-            int cellCount = buf.readUnsignedByte();
-            Network network = new Network();
-            if (cellType == 0x00) {
-                for (int i = 0; i < cellCount; i++) {
-                    int lac = buf.readUnsignedShort();
-                    long cid = buf.readUnsignedMedium();
-                    int rssi = buf.readUnsignedByte();
-                    network.addCellTower(CellTower.from(
-                            actualMcc, mnc, lac, cid, rssi));
-                }
-            } else if (cellType == 0x01) {
-                for (int i = 0; i < cellCount; i++) {
-                    long lac = buf.readUnsignedInt();
-                    long cid = buf.readLong();
-                    int rssi = buf.readUnsignedByte();
-                    network.addCellTower(CellTower.from(
-                            actualMcc, mnc, (int) lac, cid, rssi));
-                }
-            }
-            position.set("timingAdvance", buf.readUnsignedByte());
-
-            int wifiCount = buf.readUnsignedByte();
-            for (int i = 0; i < wifiCount; i++) {
-                String macAddress = ByteBufUtil.hexDump(buf.readSlice(6)).toUpperCase();
-                int signalStrength = buf.readUnsignedByte();
-                network.addWifiAccessPoint(WifiAccessPoint.from(
-                    macAddress, signalStrength));
-            }
-
-            int event = buf.readUnsignedByte();
-            int languageDetails = buf.readUnsignedByte();
-        
-            if (languageDetails == 0x01) {
-                position.set("alertLanguage", "Chinese");
-            } else if (languageDetails == 0x02) {
-                position.set("alertLanguage", "English");
-            } else if (languageDetails == 0x00) {
-                // no reply needed
-            }
-
-            position.set(Position.KEY_EVENT, event);
-            switch (event) {
-                case 0x00 -> position.addAlarm(Position.ALARM_GENERAL);
-                case 0x01 -> position.addAlarm(Position.ALARM_SOS);
-                case 0x02 -> position.addAlarm(Position.ALARM_POWER_CUT);
-                case 0x03 -> position.addAlarm(Position.ALARM_VIBRATION);
-                case 0x04 -> position.addAlarm(Position.ALARM_GEOFENCE_ENTER);
-                case 0x05 -> position.addAlarm(Position.ALARM_GEOFENCE_EXIT);
-                case 0x06 -> position.addAlarm(Position.ALARM_OVERSPEED);
-                case 0x09 -> position.addAlarm(Position.ALARM_TOW);
-                case 0x0A -> position.addAlarm(Position.ALARM_GPS_ANTENNA_CUT);
-                case 0x0B -> position.addAlarm(Position.ALARM_GPS_ANTENNA_CUT);
-                case 0x0C -> position.addAlarm(Position.ALARM_POWER_ON);
-                case 0x0D -> position.addAlarm(Position.ALARM_GPS_ANTENNA_CUT);
-                case 0x0E -> position.addAlarm(Position.ALARM_LOW_POWER);
-                case 0x0F -> position.addAlarm(Position.ALARM_LOW_POWER);
-                case 0x10 -> position.addAlarm(Position.ALARM_TAMPERING);
-                case 0x11 -> position.addAlarm(Position.ALARM_POWER_OFF);
-                case 0x12 -> position.addAlarm(Position.ALARM_GENERAL);
-                case 0x13 -> position.addAlarm(Position.ALARM_TAMPERING);
-                case 0x14 -> position.addAlarm(Position.ALARM_DOOR);
-                case 0x15 -> position.addAlarm(Position.ALARM_POWER_OFF);
-                case 0x16 -> position.addAlarm(Position.ALARM_TAMPERING);
-                case 0x17 -> position.addAlarm(Position.ALARM_TAMPERING);
-                case 0x18 -> position.addAlarm(Position.ALARM_REMOVING);
-                case 0x19 -> position.addAlarm(Position.ALARM_LOW_BATTERY);
-            }
-
-            position.setNetwork(network);
             
         } else {
 
