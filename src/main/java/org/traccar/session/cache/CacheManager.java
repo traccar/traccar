11a47eaac0a361a40b6158e3c9a712f77bb78ea2
jcardus <joaquim_cardeira@hotmail.com>
2024-12-03 00:16:21 +0000

    getUserObjects

--- src/main/java/org/traccar/session/cache/CacheManager.java
+++ src/main/java/org/traccar/session/cache/CacheManager.java
@@ -112,7 +112,7 @@ public class CacheManager implements BroadcastInterface {
     public <T extends BaseModel> Set<T> getUserObjects(long userId, Class<T> clazz) {
         try {
             lock.readLock().lock();
-            return graph.getObjects(Device.class, userId, clazz, Set.of(Group.class), true)
+            return graph.getObjects(User.class, userId, clazz, Set.of(Group.class), true)
                     .collect(Collectors.toUnmodifiableSet());
         } finally {
             lock.readLock().unlock();
