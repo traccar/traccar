09e53a62b3ee3bf4b2d7a13e38c4c7b81c973a47
gabriel <victorgaborges.svl@gmail.com>
2025-01-29 16:54:02 -0300

    Update ItrProtocol.java

--- src/main/java/org/traccar/protocol/ItrProtocol.java
+++ src/main/java/org/traccar/protocol/ItrProtocol.java
@@ -1,20 +1,19 @@
 package org.traccar.protocol;
 
 import org.traccar.BaseProtocol;
+import org.traccar.TrackerServer;
 import org.traccar.PipelineBuilder;
-import org.traccar.Config;
 import io.netty.channel.ChannelPipeline;
 
 public class ItrProtocol extends BaseProtocol {
 
-    public ItrProtocol(Config config) {
-        super(config);
-        addServer(new BaseProtocol.Server(this, false, new PipelineBuilder() {
+    public ItrProtocol() {
+        addServer(new TrackerServer(false, getName()) {
             @Override
-            protected void addHandlers(ChannelPipeline pipeline) {
+            protected void addProtocolHandlers(PipelineBuilder pipeline) {
                 pipeline.addLast(new ItrProtocolFrameDecoder());
                 pipeline.addLast(new ItrProtocolDecoder(ItrProtocol.this));
             }
-        }));
+        });
     }
 }
