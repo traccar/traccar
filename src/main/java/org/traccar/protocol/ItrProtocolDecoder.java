4b42b7dc9091d589b0a92a8bb9e30c5524467e74
gabriel <victorgaborges.svl@gmail.com>
2025-01-29 16:57:38 -0300

    Update ItrProtocolDecoder.java

--- src/main/java/org/traccar/protocol/ItrProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/ItrProtocolDecoder.java
@@ -1,52 +1,67 @@
 package org.traccar.protocol;
 
 import org.traccar.BaseProtocolDecoder;
-import org.traccar.Protocol;
+import org.traccar.helper.BitUtil;
+import org.traccar.helper.UnitsConverter;
 import org.traccar.model.Position;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+
 import java.net.SocketAddress;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ItrProtocolDecoder extends BaseProtocolDecoder {
 
-    public ItrProtocolDecoder(Protocol protocol) {
+    public ItrProtocolDecoder(BaseProtocol protocol) {
         super(protocol);
     }
 
     @Override
-    protected Object decode(Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+    protected Object decode(ChannelHandlerContext ctx, Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
-        
-        buf.skipBytes(2); // Marca inicial (0x2828)
-        int pid = buf.readUnsignedByte(); // Tipo do pacote
-        int length = buf.readUnsignedShort();
-        int sequence = buf.readUnsignedShort();
 
-        if (pid == 0x12) { // Pacote de localização
-            Position position = new Position(getProtocolName());
-            position.setDeviceId(getDeviceSession(channel, remoteAddress, buf.readBytes(8).toString()).getDeviceId());
-
-            long time = buf.readUnsignedInt();
-            position.setTime(new Date(time * 1000));
-
-            int mask = buf.readUnsignedByte();
-            if ((mask & 0x01) != 0) {  // Dados de GPS
-                position.setLatitude(buf.readInt() / 1800000.0);
-                position.setLongitude(buf.readInt() / 1800000.0);
-                position.setAltitude(buf.readShort());
-                position.setSpeed(buf.readUnsignedShort() * 1.852); // Convertendo para km/h
-                position.setCourse(buf.readUnsignedShort());
-                position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
-            }
-
-            position.set(Position.KEY_BATTERY, buf.readUnsignedShort() / 1000.0);
-            position.set(Position.KEY_ODOMETER, buf.readUnsignedInt());
-            position.set(Position.KEY_HOURS, buf.readUnsignedInt());
-
-            return position;
+        // Verifica o marcador do pacote (0x28 0x28)
+        if (buf.readUnsignedShort() != 0x2828) {
+            return null;
         }
 
-        return null;
+        int pid = buf.readUnsignedByte(); // Identificador do pacote
+        int size = buf.readUnsignedShort(); // Tamanho do pacote
+        int sequence = buf.readUnsignedShort(); // Número de sequência
+
+        Position position = new Position(getProtocolName());
+        position.setDeviceId(getDeviceId(channel, remoteAddress, buf.toString(StandardCharsets.US_ASCII)));
+
+        // Decodifica o pacote com base no PID
+        switch (pid) {
+            case 0x01: // Login package
+                // Implemente a decodificação do pacote de login
+                break;
+            case 0x12: // Location package
+                position.setTime(new Date(buf.readUnsignedInt() * 1000L)); // Tempo UTC
+                int mask = buf.readUnsignedByte(); // Máscara de dados válidos
+
+                if (BitUtil.check(mask, 0)) { // Dados GPS válidos
+                    position.setLatitude(buf.readInt() / 5000000.0);
+                    position.setLongitude(buf.readInt() / 5000000.0);
+                    position.setAltitude(buf.readShort());
+                    position.setSpeed(UnitsConverter.knotsFromKph(buf.readUnsignedShort()));
+                    position.setCourse(buf.readUnsignedShort());
+                    position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
+                }
+
+                // Adicione mais decodificações para outros campos, como status, bateria, etc.
+                break;
+            // Implemente outros casos para diferentes tipos de pacotes
+            default:
+                return null;
+        }
+
+        return position;
     }
 }
