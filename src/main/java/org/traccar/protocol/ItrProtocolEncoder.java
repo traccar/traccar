146652379ad7db54e7c9f3f4d4926b8edfecded8
gabriel <victorgaborges.svl@gmail.com>
2025-01-29 16:46:55 -0300

    Create ItrProtocolEncoder.java

---
+++ src/main/java/org/traccar/protocol/ItrProtocolEncoder.java
@@ -0,0 +1,23 @@
+package org.traccar.protocol;
+
+import org.traccar.BaseProtocolEncoder;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+
+public class ItrProtocolEncoder extends BaseProtocolEncoder {
+
+    @Override
+    protected Object encodeCommand(Channel channel, Object msg) {
+        String command = (String) msg;
+        ByteBuf buf = Unpooled.buffer();
+
+        buf.writeShort(0x2828);
+        buf.writeByte(0x80); // Código do pacote de comando
+        buf.writeShort(command.length());
+        buf.writeShort(1); // Número de sequência
+        buf.writeBytes(command.getBytes());
+
+        return buf;
+    }
+}
