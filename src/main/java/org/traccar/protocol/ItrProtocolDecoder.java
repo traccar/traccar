312643ed5e31d9ea20cc344b29868813f00f5bf7
gabriel <victorgaborges.svl@gmail.com>
2025-01-29 16:44:56 -0300

    Update ItrProtocolDecoder.java

--- src/main/java/org/traccar/protocol/ItrProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/ItrProtocolDecoder.java
@@ -1,32 +1,53 @@
 package org.traccar.protocol;
 
-import org.traccar.BaseFrameDecoder;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.Protocol;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.model.Position;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.Channel;
+import java.util.Date;
 
-public class ItrProtocolFrameDecoder extends BaseFrameDecoder {
+public class ItrProtocolDecoder extends BaseProtocolDecoder {
+
+    public ItrProtocolDecoder(Protocol protocol) {
+        super(protocol);
+    }
 
     @Override
-    protected Object decode(ChannelHandlerContext ctx, ByteBuf buf) throws Exception {
-        if (buf.readableBytes() < 5) {
-            return null;
-        }
-
-        buf.markReaderIndex();
-        int startMarker = buf.readUnsignedShort();
+    protected Object decode(Channel channel, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
         
-        if (startMarker != 0x2828) {  // Verifica o marcador do protocolo
-            buf.resetReaderIndex();
-            return null;
+        buf.skipBytes(2); // Marca inicial (0x2828)
+        int pid = buf.readUnsignedByte(); // Tipo do pacote
+        int length = buf.readUnsignedShort();
+        int sequence = buf.readUnsignedShort();
+
+        if (pid == 0x12) { // Pacote de localização
+            Position position = new Position(getProtocolName());
+            position.setDeviceId(getDeviceSession(channel, buf.readBytes(8)).getDeviceId());
+
+            long time = buf.readUnsignedInt();
+            position.setTime(new Date(time * 1000));
+
+            int mask = buf.readUnsignedByte();
+            if ((mask & 0x01) != 0) {  // Dados de GPS
+                position.setLatitude(buf.readInt() / 1800000.0);
+                position.setLongitude(buf.readInt() / 1800000.0);
+                position.setAltitude(buf.readShort());
+                position.setSpeed(buf.readUnsignedShort() * 1.852); // Convertendo para km/h
+                position.setCourse(buf.readUnsignedShort());
+                position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
+            }
+
+            position.set(Position.KEY_BATTERY, buf.readUnsignedShort() / 1000.0);
+            position.set(Position.KEY_ODOMETER, buf.readUnsignedInt());
+            position.set(Position.KEY_HOURS, buf.readUnsignedInt());
+
+            return position;
         }
 
-        int length = buf.getUnsignedShort(buf.readerIndex() + 2);
-        
-        if (buf.readableBytes() >= length + 5) {
-            return buf.readRetainedSlice(length + 5);
-        }
-
-        buf.resetReaderIndex();
         return null;
     }
 }
