2ba6dc7a09e776ea024b5c3c5a0a14b83d020eb5
gabriel <victorgaborges.svl@gmail.com>
2025-01-29 17:03:54 -0300

    Update ItrProtocol.java

--- src/main/java/org/traccar/protocol/ItrProtocol.java
+++ src/main/java/org/traccar/protocol/ItrProtocol.java
@@ -3,20 +3,20 @@ package org.traccar.protocol;
 import org.traccar.BaseProtocol;
 import org.traccar.PipelineBuilder;
 import org.traccar.TrackerServer;
-
-import io.netty.handler.codec.string.StringEncoder;
+import org.traccar.model.Command;
 
 import java.util.List;
 
 public class ItrProtocol extends BaseProtocol {
 
     public ItrProtocol() {
+        // Adiciona um servidor para dispositivos iTR
         addServer(new TrackerServer(false, getName()) {
             @Override
             protected void addProtocolHandlers(PipelineBuilder pipeline) {
-                pipeline.addLast(new ItrProtocolFrameDecoder());
-                pipeline.addLast(new StringEncoder());
-                pipeline.addLast(new ItrProtocolDecoder(ItrProtocol.this));
+                pipeline.addLast(new ItrProtocolFrameDecoder()); // Decodifica os quadros de dados
+                pipeline.addLast(new ItrProtocolEncoder()); // Codifica comandos para o dispositivo
+                pipeline.addLast(new ItrProtocolDecoder(ItrProtocol.this)); // Decodifica pacotes de dados
             }
         });
     }
