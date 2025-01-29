06c29cd24998ce6aaa363f8b6e7bb54781322722
gabriel <victorgaborges.svl@gmail.com>
2025-01-29 16:54:49 -0300

    Update ItrProtocolEncoder.java

--- src/main/java/org/traccar/protocol/ItrProtocolEncoder.java
+++ src/main/java/org/traccar/protocol/ItrProtocolEncoder.java
@@ -1,11 +1,16 @@
 package org.traccar.protocol;
 
 import org.traccar.BaseProtocolEncoder;
+import org.traccar.Protocol;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 
 public class ItrProtocolEncoder extends BaseProtocolEncoder {
+
+    public ItrProtocolEncoder(Protocol protocol) {
+        super(protocol);
+    }
 
     @Override
     protected Object encodeCommand(Channel channel, Object msg) {
