4f18b064767390fd478ed2fb98df7d261e6a5b39
gabriel <victorgaborges.svl@gmail.com>
2025-01-29 16:54:37 -0300

    Update ItrProtocolDecoder.java

--- src/main/java/org/traccar/protocol/ItrProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/ItrProtocolDecoder.java
@@ -2,11 +2,10 @@ package org.traccar.protocol;
 
 import org.traccar.BaseProtocolDecoder;
 import org.traccar.Protocol;
-import org.traccar.helper.Parser;
-import org.traccar.helper.PatternBuilder;
 import org.traccar.model.Position;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+import java.net.SocketAddress;
 import java.util.Date;
 
 public class ItrProtocolDecoder extends BaseProtocolDecoder {
@@ -16,7 +15,7 @@ public class ItrProtocolDecoder extends BaseProtocolDecoder {
     }
 
     @Override
-    protected Object decode(Channel channel, Object msg) throws Exception {
+    protected Object decode(Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         
         buf.skipBytes(2); // Marca inicial (0x2828)
@@ -26,7 +25,7 @@ public class ItrProtocolDecoder extends BaseProtocolDecoder {
 
         if (pid == 0x12) { // Pacote de localização
             Position position = new Position(getProtocolName());
-            position.setDeviceId(getDeviceSession(channel, buf.readBytes(8)).getDeviceId());
+            position.setDeviceId(getDeviceSession(channel, remoteAddress, buf.readBytes(8).toString()).getDeviceId());
 
             long time = buf.readUnsignedInt();
             position.setTime(new Date(time * 1000));
