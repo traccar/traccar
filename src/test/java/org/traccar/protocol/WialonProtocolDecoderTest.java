2aa0d1b82ea007f76a54672be2ad6400a0e29caa
Eduard Vlasov <driver.edro@gmail.com>
2025-02-05 00:24:30 +0100

    fix damn whitespaces

--- src/test/java/org/traccar/protocol/WialonProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/WialonProtocolDecoderTest.java
@@ -28,13 +28,13 @@ public class WialonProtocolDecoderTest extends ProtocolTest {
 
         verifyAttributes(decoder, text(
                 "#D#120319;112003;NA;NA;NA;NA;0.000;NA;NA;0;NA;NA;NA;NA;NA;101_521347:1:521246,101_158:1:510,101_521055:1:510,101_521055_2.9:1:509,101_521056:1:3;626B"));
-        
+
         verifyNull(decoder, text(
                 "#L#123456789012345;test"));
         
         verifyNull(decoder, text(
                 "#L#2002;NA"));
-
+        
         verifyNull(decoder, text(
                 "#P#"));
 
@@ -60,22 +60,22 @@ public class WialonProtocolDecoderTest extends ProtocolTest {
 
         verifyPosition(decoder, text(
                 "#SD#021214;065947;2237.7552;N;11404.8851;E;0.000;;170.9;5"));
-        
+
         verifyPosition(decoder, text(
                 "#D#270413;205601;5544.6025;N;03739.6834;E;1;2;3;4;0.0;0;0;14.77,0.02,3.6;NA;count1:1:564,fuel:2:45.8,hw:3:V4.5"));
         
         verifyPosition(decoder, text(
                 "#D#190114;051312;4459.6956;N;04105.9930;E;35;306;204.000000;12;NA;452986639;NA;106.000000;NA;sats_gps:1:9,sats_glonass:1:3,balance:2:12123.000000,stay_balance:1:0"));
-
+        
         verifyPosition(decoder, text(
                 "#D#021214;065947;2237.7552;N;11404.8851;E;0.000;;170.9;5;1.74;NA;NA;NA;NA;NA"));
 
         verifyPosition(decoder, text(
                 "#D#021214;065947;2237.7552;N;11404.8851;E;0.000;;170.9;5;1.74;NA;NA;;NA;NA"));
-        
+
         verifyPositions(decoder, text(
                 "#B#080914;073235;5027.50625;N;03026.19321;E;0.700;0.000;NA;4;NA;NA;NA;;NA;Батарея:3:100 %|080914;073420;5027.50845;N;03026.18854;E;1.996;292.540;NA;4;NA;NA;NA;;NA;Батарея:3:100 %"));
-
+        
         verifyPositions(decoder, text(
                 "#B#110914;102132;5027.50728;N;03026.20369;E;1.979;288.170;NA;NA;NA;NA;NA;;NA;Батарея:3:100 %"));
 
