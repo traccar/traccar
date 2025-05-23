8abdf7ad7e475f693ce6cf8384e16d0732265859
Anton Tananaev <anton@traccar.org>
2025-05-23 07:35:11 -0700

    Explicitly handle missing OpenID

--- src/main/java/org/traccar/api/resource/SessionResource.java
+++ src/main/java/org/traccar/api/resource/SessionResource.java
@@ -154,6 +154,9 @@ public class SessionResource extends BaseResource {
     @Path("openid/auth")
     @GET
     public Response openIdAuth() {
+        if (openIdProvider == null) {
+            throw new UnsupportedOperationException("OpenID not enabled");
+        }
         return Response.seeOther(openIdProvider.createAuthUri()).build();
     }
 
@@ -161,7 +164,10 @@ public class SessionResource extends BaseResource {
     @Path("openid/callback")
     @GET
     public Response requestToken() throws IOException, StorageException, ParseException, GeneralSecurityException {
-        String redirectUriOverride = request.getParameter("redirect_uri");
+        if (openIdProvider == null) {
+            throw new UnsupportedOperationException("OpenID not enabled");
+        }
         return Response.seeOther(openIdProvider.handleCallback(request.getQueryString(), request)).build();
     }
+
 }
