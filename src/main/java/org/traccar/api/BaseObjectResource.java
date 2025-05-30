e2d9ff87212a2e0affceb582b89a993e85347eaf
Cristiano Nascimeno <Cristiano@proserv.net.br>
2025-05-29 22:51:39 -0300

    Updated copyright year

--- src/main/java/org/traccar/api/BaseObjectResource.java
+++ src/main/java/org/traccar/api/BaseObjectResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2022 Anton Tananaev (anton@traccar.org)
+ * Copyright 2017 - 2025 Anton Tananaev (anton@traccar.org)
  * Copyright 2017 - 2018 Andrey Kunitsyn (andrey@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
