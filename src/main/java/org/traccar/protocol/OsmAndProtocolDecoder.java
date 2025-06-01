36832eda825ecdbda7e1af59034c4bd8762b78a9
Anton Tananaev <anton@traccar.org>
2025-05-31 21:41:37 -0700

    Fix OsmAnd alarm decoding

--- src/main/java/org/traccar/protocol/OsmAndProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/OsmAndProtocolDecoder.java
@@ -275,9 +275,13 @@ public class OsmAndProtocolDecoder extends BaseHttpProtocolDecoder {
             }
         }
 
-        JsonObject extras = location.containsKey("extras") ? location.getJsonObject("extras") : location;
-        if (extras.containsKey("alarm")) {
-            position.set(Position.KEY_ALARM, extras.getString("alarm"));
+        if (location.containsKey("alarm")) {
+            position.set(Position.KEY_ALARM, location.getString("alarm"));
+        } else if (location.containsKey("extras")) {
+            JsonObject extras = location.getJsonObject("extras");
+            if (extras.containsKey("alarm")) {
+                position.set(Position.KEY_ALARM, extras.getString("alarm"));
+            }
         }
 
         sendResponse(channel, HttpResponseStatus.OK);
