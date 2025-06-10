f57eb78b1b46a061e75cbd949b39f4b165111f5c
mohammad mazarei <mohammad.mazarei@gmail.com>
2025-06-10 14:22:49 +0330

    style: fix Checkstyle violations in RadshidProtocolDecoder

--- src/main/java/org/traccar/protocol/RadshidProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/RadshidProtocolDecoder.java
@@ -32,14 +32,21 @@ import java.util.List;
 
 public class RadshidProtocolDecoder extends BaseProtocolDecoder {
 
-    private static final Checksum.Algorithm CRC16_CCITT_A001 = new Checksum.Algorithm(16, 0xA001, 0x0000, false, false, 0x0000);
+    private static final Checksum.Algorithm CRC16_CCITT_A001 = new Checksum.Algorithm(
+        16,
+        0xA001,
+        0x0000,
+        false,
+        false,
+        0x0000
+        );
 
     public RadshidProtocolDecoder(Protocol protocol) {
         super(protocol);
     }
 
     private static void sendResponse(
-            Channel channel, SocketAddress remoteAddress, int length, Boolean crc_status) {
+            Channel channel, SocketAddress remoteAddress, int length, Boolean crcStatus) {
 
         if (channel != null) {
             ByteBuf buf = Unpooled.buffer();
@@ -48,16 +55,14 @@ public class RadshidProtocolDecoder extends BaseProtocolDecoder {
             buf.writeByte(0x82);
             buf.writeByte(0x01);
             buf.writeInt(length);
-            buf.writeByte((crc_status==true) ? 0x01:0x00);
+            buf.writeByte(crcStatus ? 0x01 : 0x00);
 
             channel.writeAndFlush(new NetworkMessage(buf, remoteAddress));
         }
     }
 
-    private String decodeAlarm(int event) 
-    {
-        switch (event) 
-        {
+    private String decodeAlarm(int event) {
+        switch (event) {
             case 0xD6: // GSM_Jamming_Event
             case 0xD7: // GPS_Jamming_Event
                 return Position.ALARM_JAMMING;
@@ -87,45 +92,43 @@ public class RadshidProtocolDecoder extends BaseProtocolDecoder {
     @Override
     protected Object decode(
             Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
-        
-        
+
         ByteBuf buf = (ByteBuf) msg;
         int length = buf.readableBytes();
-        
+
         /* read crc from end of packet */
         buf.readerIndex(length - 2);
         int packetChecksum = buf.readUnsignedShortLE();
         buf.readerIndex(0);
 
         /* copy data part of packet */
-        ByteBuf packetData = buf.slice(0,buf.readableBytes()-2);
+        ByteBuf packetData = buf.slice(0, buf.readableBytes() - 2);
 
         /* calc data Checksum*/
         int dataChecksum = Checksum.crc16(CRC16_CCITT_A001, packetData.nioBuffer());
-        
+
         /* check data crc and packet crc */
-        if(dataChecksum != packetChecksum)
-        {
+        if (dataChecksum != packetChecksum) {
             /* send response to device */
-            sendResponse(channel,remoteAddress,length,false);
+            sendResponse(channel, remoteAddress, length, false);
             return null;
         }
 
-        byte packet_tag = packetData.readByte(); // Packet Tag
-        long deviceSerial= (long)packetData.readUnsignedInt();
+        byte packetTag = packetData.readByte(); // Packet Tag
+        long deviceSerial = (long) packetData.readUnsignedInt();
         byte dataElement = packetData.readByte();
 
         DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, String.valueOf(deviceSerial));
-        if (deviceSession == null || packet_tag != 0x01) {
+        if (deviceSession == null || packetTag != 0x01) {
             return null;
         }
 
         /* Send response to device */
-        sendResponse(channel,remoteAddress,length,true);
+        sendResponse(channel, remoteAddress, length, true);
 
         List<Position> positions = new LinkedList<>();
         for (int i = 0; i < dataElement; i++) {
-            
+
             Position position = new Position(getProtocolName());
             position.setDeviceId(deviceSession.getDeviceId());
 
@@ -133,36 +136,36 @@ public class RadshidProtocolDecoder extends BaseProtocolDecoder {
             byte event = packetData.readByte(); // Event Code
             packetData.readBytes(5); // Driver Id
             packetData.readUnsignedShort(); // Total Driving Tim
-            long timeStamp = packetData.readUnsignedInt();   // Timestamp 
+            long timeStamp = packetData.readUnsignedInt();  // Timestamp
             byte gpsSpeed = packetData.readByte();  // GPS Speed
             packetData.readByte();  // Vehicle Sensor Speed
             packetData.readByte();  // Max Speed (GPS)
-            packetData.readByte();  // Max Speed (Vehicle Sensor) 
+            packetData.readByte();  // Max Speed (Vehicle Sensor)
             packetData.readUnsignedShort(); // Engine RPM
             packetData.readUnsignedInt();   // Total Traveled Distance (GPS)
             packetData.readUnsignedInt();   // otal Traveled Distance (Vehicle Sensor)
-            byte IOstatus = packetData.readByte();  // IO Status
+            byte ioStatus = packetData.readByte();  // IO Status
             byte gpsStatus = packetData.readByte(); // GPS Status
             long latitude = packetData.readInt();   // Latitude
-            long longitude= packetData.readInt();   // Longitude 
+            long longitude = packetData.readInt();   // Longitude
             int altitude = packetData.readShort();  //Altitude
             int bearing = packetData.readShort();   //Bearing
-            byte numberOfSatellites = packetData.readByte();// Number Of Satellites
-            byte PDOP = packetData.readByte(); // PDOP
-            byte xLen = packetData.readByte(); // Extra Data Length 
+            byte numberOfSatellites = packetData.readByte(); // Number Of Satellites
+            byte pdop = packetData.readByte(); // PDOP
+            byte xLen = packetData.readByte(); // Extra Data Length
             packetData.readBytes(xLen); // scape extra data
 
-            position.setValid((gpsStatus==0x00) ? true:false);
+            position.setValid((gpsStatus == 0x00));
             position.setTime(new Date(timeStamp * 1000L));
             position.setLatitude(latitude * 0.0000001);
             position.setLongitude(longitude * 0.0000001);
             position.setAltitude(altitude);
             position.setSpeed(UnitsConverter.knotsFromKph(gpsSpeed));
             position.setCourse(bearing);
-            position.set(Position.KEY_SATELLITES,numberOfSatellites);
-            position.set(Position.KEY_PDOP, PDOP * 0.1);
-            position.set(Position.KEY_IGNITION, (IOstatus & 0x40)!=0 ? true:false);
-            position.set(Position.KEY_ALARM, decodeAlarm((int)event));
+            position.set(Position.KEY_SATELLITES, numberOfSatellites);
+            position.set(Position.KEY_PDOP, pdop * 0.1);
+            position.set(Position.KEY_IGNITION, (ioStatus & 0x40) != 0);
+            position.set(Position.KEY_ALARM, decodeAlarm((int) event));
 
             positions.add(position);
         }
