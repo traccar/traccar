7319ab4cd5fb33b4385057e6f7687579c2623738
Renan S <renan.souza@supermix.com.br>
2025-06-26 09:09:05 -0300

    Code adjustments - gradlew check OK

--- src/main/java/org/traccar/protocol/JmakProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/JmakProtocolDecoder.java
@@ -182,9 +182,9 @@ public class JmakProtocolDecoder extends BaseProtocolDecoder {
         Position position = new Position(getProtocolName());
         position.setDeviceId(deviceSession.getDeviceId());
 
-        int event_id = 0;
-        int event_status = 0;
-        String event_name = "";
+        int eventId = 0;
+        int eventStatus = 0;
+        String eventName = "";
         Map<Integer, String> stdMap = parseFlaggedData(standardParts[0], standardParts);
         for (Map.Entry<Integer, String> e : stdMap.entrySet()) {
             int key = e.getKey();
@@ -229,16 +229,16 @@ public class JmakProtocolDecoder extends BaseProtocolDecoder {
                     position.set(Position.KEY_ODOMETER, Double.parseDouble(value) * 1000);
                     break;
                 case 19:
-                    event_id = Integer.parseInt(value);
-                    position.set(Position.KEY_EVENT, event_id);
-                    position.set(name, event_id);
+                    eventId = Integer.parseInt(value);
+                    position.set(Position.KEY_EVENT, eventId);
+                    position.set(name, eventId);
                     break;
                 case 20:
-                    event_status = Integer.parseInt(value);
-                    position.set(name, event_status);
+                    eventStatus = Integer.parseInt(value);
+                    position.set(name, eventStatus);
                     break;
                 case 21:
-                    event_name = value;
+                    eventName = value;
                     position.set(name, value);
                     break;
                 case 22:
@@ -274,9 +274,8 @@ public class JmakProtocolDecoder extends BaseProtocolDecoder {
             }
         }
 
-        if(event_id == 126 && event_status == 4)
-        {
-            position.set(Position.KEY_CARD, event_name);
+        if (eventId == 126 && eventStatus == 4) {
+            position.set(Position.KEY_CARD, eventName);
         }
 
         if (parts.length > 1) {
