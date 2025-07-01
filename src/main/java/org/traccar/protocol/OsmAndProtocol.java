c0f9d628b38ce3b271eb8a076f9b0d496ea5f601
Anton Tananaev <anton@traccar.org>
2025-07-01 11:24:33 -0700

    Add more command types

--- src/main/java/org/traccar/protocol/OsmAndProtocol.java
+++ src/main/java/org/traccar/protocol/OsmAndProtocol.java
@@ -31,7 +31,9 @@ public class OsmAndProtocol extends BaseProtocol {
     @Inject
     public OsmAndProtocol(Config config) {
         setSupportedPushCommands(
-                Command.TYPE_POSITION_SINGLE);
+                Command.TYPE_POSITION_SINGLE,
+                Command.TYPE_POSITION_PERIODIC,
+                Command.TYPE_POSITION_STOP);
         addServer(new TrackerServer(config, getName(), false) {
             @Override
             protected void addProtocolHandlers(PipelineBuilder pipeline, Config config) {
