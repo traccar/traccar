c0ee1eaed8a320ecf1410697b6daceddaf3da8e5
Anton Tananaev <anton@traccar.org>
2025-07-14 19:23:41 -0700

    Minor cleanup

--- src/main/java/org/traccar/api/StreamWriter.java
+++ src/main/java/org/traccar/api/StreamWriter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.traccar.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,7 +26,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.util.Iterator;
 import java.util.stream.Stream;
 
 @Provider
@@ -26,12 +40,12 @@ public class StreamWriter implements MessageBodyWriter<Stream<?>> {
     }
 
     @Override
-    public void writeTo(Stream<?> stream, Class<?> type, Type genericType, Annotation[] annotations,
-                        MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
-            throws IOException {
+    public void writeTo(
+            Stream<?> stream, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+            MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException {
         try (stream; var generator = objectMapper.createGenerator(entityStream)) {
             generator.writeStartArray();
-            for (Iterator<?> it = stream.iterator(); it.hasNext();) {
+            for (var it = stream.iterator(); it.hasNext();) {
                 objectMapper.writeValue(generator, it.next());
             }
             generator.writeEndArray();
