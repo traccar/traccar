3649907c69f930c7bfe54c5832a84efc8b292f82
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-09-28 19:30:38 +0100

    always log severe

--- src/main/java/org/traccar/helper/Log.java
+++ src/main/java/org/traccar/helper/Log.java
@@ -206,9 +206,11 @@ public final class Log {
         Level level = Level.parse(levelString.toUpperCase());
         rootLogger.setLevel(level);
         handler.setLevel(level);
-
         if (filter != null && !filter.isEmpty()) {
-            handler.setFilter(record -> record != null && record.getLoggerName().contains(filter));
+            handler.setFilter(record -> record != null && (
+                record.getLoggerName().contains(filter) ||
+                record.getLevel()==Level.SEVERE
+            ));
         } else {
             handler.setFilter(record -> record != null && !record.getLoggerName().startsWith("sun"));
         }
