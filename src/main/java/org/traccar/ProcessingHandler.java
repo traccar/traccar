9c86b75c166fa7706f13001ec4bb911e589dc79b
Quentin Young <qlyoung@users.noreply.github.com>
2025-10-10 16:14:58 -0400

    Fix stack overflow under heavy load (#5685)
    The call path for processing new points is recursive, with recursion depth
    controlled by the depth of the point queue. This results in a stack overflow
    under heavy load. Fix it by eliminating the problematic recursive path.
    
    The previous path was:
    
      processNextPosition()
        -> processPositionHandlers()
           -> finishedProcessing()
              -> processNextPosition()
    
    The base case was "no more positions to process"; so with a large queue this
    could easily exceed the stack limit.
    
    It is now:
    
      processNextPosition()
        -> schedule(processPositionHandlers())
    
      processPositionHandlers()
      -> finishedProcessing()
         -> processNextPosition()
    
    Points are still processed one at a time in order, but each processing job
    happens in a new stack.

--- src/main/java/org/traccar/ProcessingHandler.java
+++ src/main/java/org/traccar/ProcessingHandler.java
@@ -211,7 +211,7 @@ public class ProcessingHandler extends ChannelInboundHandlerAdapter implements B
             nextPosition = queue.peek();
         }
         if (nextPosition != null) {
-            processPositionHandlers(ctx, nextPosition);
+            ctx.executor().execute(() -> processPositionHandlers(ctx, nextPosition));
         }
     }
 
