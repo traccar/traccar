fc40c1fdfccdd78c70946335d96e2ebdd31254e0
Amnuay <amnuay@the-xsense.com>
2025-10-24 15:59:51 +0700

    disable outdated

--- src/main/java/org/traccar/protocol/XsenseGtr9ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/XsenseGtr9ProtocolDecoder.java
@@ -375,11 +375,13 @@ public class XsenseGtr9ProtocolDecoder extends BaseProtocolDecoder {
                             deviceSession.getDeviceId());
                     continue; // Skip this position
                 }
+                /*
                 boolean outdated = messageType == M_TINI_BATCH_OFFLINE_POSITION_REPORT_ENHIO;
                 position.setOutdated(outdated);
                 if (outdated) {
                    position.setDeviceTime(position.getFixTime());
                 }
+                */
                 positions.add(position);
             } else {
                 break; // Stop if we can't decode a record
