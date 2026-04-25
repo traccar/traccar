18d760c3672463c51367249e42a89487a348c7c0
Ivan Martinez <ivanfmartinez@users.noreply.github.com>
2026-04-24 21:27:45 -0300

    correct battery voltage for x3tech xt40 devices

--- src/main/java/org/traccar/protocol/Gt06ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/Gt06ProtocolDecoder.java
@@ -237,7 +237,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
             case MSG_LBS_ALARM:
                 return true;
             case MSG_GPS_LBS_2:
-                return model != null && Set.of("NT20", "VL100").contains(model.toUpperCase());
+                return model != null && Set.of("NT20", "VL100","XT40").contains(model.toUpperCase());
             case 0xA3: // MSG_FENCE_SINGLE / MSG_STATUS_3
                 return variant == Variant.SEEWORLD;
             default:
@@ -479,7 +479,8 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
         String model = deviceSession != null ? getDeviceModel(deviceSession) : null;
         boolean modelLW = model != null && model.toUpperCase().startsWith("LW");
         boolean modelSW = "SEEWORLD".equalsIgnoreCase(model);
-        boolean modelNT = model != null && Set.of("NT20", "VL100").contains(model.toUpperCase());
+        boolean modelXT = "XT40".equalsIgnoreCase(model);
+        boolean modelNT = modelXT || (model != null && Set.of("NT20", "VL100").contains(model.toUpperCase()));
         boolean modelVL = model != null && Set.of("VL103", "LL303", "VL512", "G18").contains(model.toUpperCase());
 
         if (type == MSG_LOGIN) {
@@ -876,13 +877,18 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
                         position.set(Position.KEY_POWER, buf.readUnsignedShort() * 0.01);
                     } else {
                         int battery = buf.readUnsignedByte();
-                        if (modelNT && type == MSG_GPS_LBS_2) {
-                            battery = battery / 10;
-                        }
-                        if (battery <= 6) {
-                            position.set(Position.KEY_BATTERY_LEVEL, battery * 100 / 6);
-                        } else if (battery <= 100) {
-                            position.set(Position.KEY_BATTERY_LEVEL, battery);
+                        if (modelXT && type == MSG_GPS_LBS_2) {
+                            position.set(Position.KEY_BATTERY, battery / 10.0);
+                        } else {
+                            if (modelNT && type == MSG_GPS_LBS_2) {
+                                battery = battery / 10;
+                            }
+
+                            if (battery <= 6) {
+                                position.set(Position.KEY_BATTERY_LEVEL, battery * 100 / 6);
+                            } else if (battery <= 100) {
+                                position.set(Position.KEY_BATTERY_LEVEL, battery);
+                            }
                         }
                     }
                     position.set(Position.KEY_RSSI, buf.readUnsignedByte());
