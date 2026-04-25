5ff93624ef3005d218348dc21184ba26e84dc82b
Ross Golder <ross@golder.org>
2026-04-25 12:22:00 +0700

    Fix NPE in OpenID login when user has no groups (#5861)
    * Fix NPE in OpenID login when user has no groups
    
    * Fix line length in OpenID authorization check

--- src/main/java/org/traccar/database/OpenIdProvider.java
+++ src/main/java/org/traccar/database/OpenIdProvider.java
@@ -174,9 +174,12 @@ public class OpenIdProvider {
         UserInfo userInfo = getUserInfo(bearerToken);
 
         List<String> userGroups = userInfo.getStringListClaim(groupsClaimName);
-        Boolean administrator = adminGroup != null ? userGroups.contains(adminGroup) : null;
+        Boolean administrator = adminGroup != null && userGroups != null ? userGroups.contains(adminGroup) : null;
 
-        if (!(Boolean.TRUE.equals(administrator) || allowGroup == null || userGroups.contains(allowGroup))) {
+        boolean authorized = Boolean.TRUE.equals(administrator)
+                || allowGroup == null
+                || (userGroups != null && userGroups.contains(allowGroup));
+        if (!authorized) {
             throw new GeneralSecurityException("Your OpenID Groups do not permit access");
         }
 
