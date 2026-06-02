5ae0698047ab45637348ce4e30d58db0ebf776c6
nilei <nils.leideck@leidex.net>
2026-06-02 23:15:29 +0200

    Update Jt808ProtocolEncoderTest.java
    Reduced redundancy.

--- src/test/java/org/traccar/protocol/Jt808ProtocolEncoderTest.java
+++ src/test/java/org/traccar/protocol/Jt808ProtocolEncoderTest.java
@@ -14,34 +14,16 @@ public class Jt808ProtocolEncoderTest extends ProtocolTest {
 
         Command command = new Command();
         command.setDeviceId(1);
-        command.setType(Command.TYPE_ENGINE_STOP);
 
+        command.setType(Command.TYPE_ENGINE_STOP);
         verifyCommand(encoder, decoder, command, binary("7e810500010b3a73ce2ff20000f0247e"));
 
-    }
-
-    @Test
-    public void testEncodeCustom() throws Exception {
-
-        var decoder = inject(new Jt808ProtocolDecoder(null));
-        var encoder = inject(new Jt808ProtocolEncoder(null));
-
-        Command command = new Command();
-        command.setDeviceId(1);
         command.setType(Command.TYPE_CUSTOM);
 
         command.set(Command.KEY_DATA, "7e830000140b3a73ce2ff2000001546573742c20436f6d6d616e642c2031323323a57e");
         verifyCommand(encoder, decoder, command, binary("7e830000140b3a73ce2ff2000001546573742c20436f6d6d616e642c2031323323a57e"));
 
         encoder.setModelOverride("BSJ");
-        command.set(Command.KEY_DATA, "Test, Command, 123#");
-        verifyCommand(encoder, decoder, command, binary("7e830000140b3a73ce2ff2000001546573742c20436f6d6d616e642c2031323323a57e"));
-
-        encoder.setModelOverride("C5");
-        command.set(Command.KEY_DATA, "Test, Command, 123#");
-        verifyCommand(encoder, decoder, command, binary("7e830000140b3a73ce2ff2000001546573742c20436f6d6d616e642c2031323323a57e"));
-
-        encoder.setModelOverride("C5L");
         command.set(Command.KEY_DATA, "Test, Command, 123#");
         verifyCommand(encoder, decoder, command, binary("7e830000140b3a73ce2ff2000001546573742c20436f6d6d616e642c2031323323a57e"));
 
