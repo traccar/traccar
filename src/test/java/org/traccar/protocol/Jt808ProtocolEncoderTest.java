b4348b20137110fe990191fb34fad3ea0c3fb663
Anton Tananaev <anton@traccar.org>
2026-06-01 21:36:01 -0700

    Fix JT808 encoder test

--- src/test/java/org/traccar/protocol/Jt808ProtocolEncoderTest.java
+++ src/test/java/org/traccar/protocol/Jt808ProtocolEncoderTest.java
@@ -1,23 +1,22 @@
 package org.traccar.protocol;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.traccar.ProtocolTest;
 import org.traccar.model.Command;
 
 public class Jt808ProtocolEncoderTest extends ProtocolTest {
 
-    @Disabled
     @Test
     public void testEncode() throws Exception {
 
+        var decoder = inject(new Jt808ProtocolDecoder(null));
         var encoder = inject(new Jt808ProtocolEncoder(null));
 
         Command command = new Command();
         command.setDeviceId(1);
         command.setType(Command.TYPE_ENGINE_STOP);
 
-        verifyCommand(encoder, command, binary("7e81050001080201000027001ff0467e"));
+        verifyCommand(encoder, decoder, command, binary("7e810500010b3a73ce2ff20000f0247e"));
 
     }
 
