b006795bf9d33f73b0f6462eb8f289da1fbf70f0
harrowmykel <harrowwestmykel@gmail.com>
2026-06-18 05:09:28 +0200

    converted old switch to new format and added test

--- src/test/java/org/traccar/protocol/OsmAndProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/OsmAndProtocolDecoderTest.java
@@ -4,6 +4,7 @@ import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.ReadOnlyHttpHeaders;
 import org.junit.jupiter.api.Test;
 import org.traccar.ProtocolTest;
+import org.traccar.model.Position;
 
 public class OsmAndProtocolDecoderTest extends ProtocolTest {
 
@@ -42,18 +43,22 @@ public class OsmAndProtocolDecoderTest extends ProtocolTest {
 
         verifyPosition(decoder, request(
                 "/?lat=49.60688&lon=6.15788&timestamp=2014-06-04+09%3A10%3A11&altitude=384.7&speed=0.0&id=353861053849681"));
-        
+
         verifyPosition(decoder, request(
                 "/?id=123456&timestamp=1377177267&lat=60.0&lon=30.0&speed=0.0&bearing=0.0&altitude=0&hdop=0.0"));
-        
+
         verifyPosition(decoder, request(
                 "/?id=123456&timestamp=1377177267&lat=60.0&lon=30.0"));
-        
+
         verifyPosition(decoder, request(
                 "/?lat=60.0&lon=30.0&speed=0.0&heading=0.0&vacc=0&hacc=0&altitude=0&deviceid=123456"));
 
         verifyPosition(decoder, request(
                 "/?id=861001000719969&lat=41.666667&lon=-0.883333&altitude=350.059479&speed=0.000000&batt=87"));
+
+        verifyAttribute(decoder, request(
+                "/?id=861001000719969&lat=41.666667&lon=-0.883333&altitude=350.059479&speed=0.000000&batt=87"),
+            Position.KEY_BATTERY_LEVEL, 87.0);
 
         verifyPosition(decoder, request(
                 "/?id=123456&timestamp=1377177267&location=60.0,30.0"));
