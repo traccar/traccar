f274bd4ad85683639848c6771e54cd75d9481431
nilei <nils.leideck@leidex.net>
2026-06-25 16:11:27 +0200

    Fix null check for model-specific F8 decoding + add read device data
    Fix null check for model-specific F8 decoding. Prevent NPE when device model is not available
    Added test data from real device upload instead of old mockup data.

--- src/main/java/org/traccar/protocol/Jt808ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/Jt808ProtocolDecoder.java
@@ -1272,7 +1272,7 @@ public class Jt808ProtocolDecoder extends BaseProtocolDecoder {
                     }
                     break;
                 case 0xF8:
-                    if (Set.of("C5", "C5L").contains(model)) {
+                    if (model != null && Set.of("C5", "C5L").contains(model)) {
                         position.set("steps", buf.readUnsignedShort());
                     } else {
                         position.set(Position.PREFIX_TEMP + 2, buf.readUnsignedShort() / 10.0 - 50);
