b165de5fbe8bc7de76e23938664c10d329514d15
Anton Tananaev <anton@traccar.org>
2026-08-13 21:13:49 -0700

    Add intersection for circle geofences

--- src/test/java/org/traccar/geofence/GeofenceCircleTest.java
+++ src/test/java/org/traccar/geofence/GeofenceCircleTest.java
@@ -14,7 +14,7 @@ public class GeofenceCircleTest {
     public void testCircleWkt() throws ParseException {
         String test = "CIRCLE (55.75414 37.6204, 100)";
         GeofenceGeometry geofenceGeometry = new GeofenceCircle(test);
-        assertEquals(geofenceGeometry.toWkt(), test);
+        assertEquals(test, geofenceGeometry.toWkt());
     }
 
     @Test
@@ -22,6 +22,13 @@ public class GeofenceCircleTest {
         GeofenceGeometry geofenceGeometry = new GeofenceCircle("CIRCLE (55.75414 37.6204, 100)");
         assertTrue(geofenceGeometry.containsPoint(55.75477, 37.62025));
         assertFalse(geofenceGeometry.containsPoint(55.75545, 37.61921));
+    }
+
+    @Test
+    public void testIntersectsCircle() throws ParseException {
+        GeofenceGeometry geofenceGeometry = new GeofenceCircle("CIRCLE (55.75414 37.6204, 100)");
+        assertTrue(geofenceGeometry.intersectsSegment(55.75214, 37.6204, 55.75614, 37.6204));
+        assertFalse(geofenceGeometry.intersectsSegment(55.76414, 37.6204, 55.77414, 37.6204));
     }
 
 }
