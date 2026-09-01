362208c98c827cd535f117ea059e652925be96ee
Kevin Yang <kevin16win@gmail.com>
2026-09-01 23:57:53 +0630

    Consolidate encoder command tests into testEncode

--- src/test/java/org/traccar/protocol/Jt808ProtocolEncoderTest.java
+++ src/test/java/org/traccar/protocol/Jt808ProtocolEncoderTest.java
@@ -35,35 +35,6 @@ public class Jt808ProtocolEncoderTest extends ProtocolTest {
             binary("7e830000140b3a73ce2ff2000001546573742c20436f6d6d616e642c2031323323a57e"),
             encodeCommand(encoder, decoder, command));
 
-    }
-
-    @Test
-    public void testEncodeJimiCustom() throws Exception {
-
-        var decoder = inject(new Jt808ProtocolDecoder(null));
-        var encoder = inject(new Jt808ProtocolEncoder(null));
-        encoder.setModelOverride("JC371");
-
-        Command command = new Command();
-        command.setDeviceId(1);
-        command.setType(Command.TYPE_CUSTOM);
-        command.set(Command.KEY_DATA, "TEST");
-
-        verifyFrame(
-            binary("7e890000050b3a73ce2ff20000f0544553543b7e"),
-            encodeCommand(encoder, decoder, command));
-
-    }
-
-    @Test
-    public void testEncodeCommands() throws Exception {
-
-        var decoder = inject(new Jt808ProtocolDecoder(null));
-        var encoder = inject(new Jt808ProtocolEncoder(null));
-
-        Command command = new Command();
-        command.setDeviceId(1);
-
         command.setType(Command.TYPE_POSITION_SINGLE);
         verifyFrame(
             binary("7e820100000b3a73ce2ff20000d27e"),
@@ -116,16 +87,22 @@ public class Jt808ProtocolEncoderTest extends ProtocolTest {
             binary("7e82014000030000012345678901234500002e7e"),
             encodeCommand(encoder, decoder, command));
 
-        command.setType(Command.TYPE_SET_SPEED_LIMIT);
-        command.set(Command.KEY_DATA, 100);
-        verifyFrame(
-            binary("7e8103401303000001234567890123450000020000005504000000640000005604000000237a7e"),
-            encodeCommand(encoder, decoder, command));
+    }
 
-        command.setType(Command.TYPE_MESSAGE);
-        command.set(Command.KEY_MESSAGE, "Hello");
+    @Test
+    public void testEncodeJimiCustom() throws Exception {
+
+        var decoder = inject(new Jt808ProtocolDecoder(null));
+        var encoder = inject(new Jt808ProtocolEncoder(null));
+        encoder.setModelOverride("JC371");
+
+        Command command = new Command();
+        command.setDeviceId(1);
+        command.setType(Command.TYPE_CUSTOM);
+        command.set(Command.KEY_DATA, "TEST");
+
         verifyFrame(
-            binary("7e83004006030000012345678901234500000448656c6c6f6e7e"),
+            binary("7e890000050b3a73ce2ff20000f0544553543b7e"),
             encodeCommand(encoder, decoder, command));
 
     }
