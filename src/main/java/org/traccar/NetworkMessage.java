4a53cf319dfe475104af64ea9395a4b2bd7fe732
Anton Tananaev <anton@traccar.org>
2026-09-26 11:31:48 -0700

    Reference counter network message

--- src/main/java/org/traccar/NetworkMessage.java
+++ src/main/java/org/traccar/NetworkMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Anton Tananaev (anton@traccar.org)
+ * Copyright 2018 - 2026 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,12 @@
  */
 package org.traccar;
 
+import io.netty.util.ReferenceCounted;
+import io.netty.util.ReferenceCountUtil;
+
 import java.net.SocketAddress;
 
-public class NetworkMessage {
+public class NetworkMessage implements ReferenceCounted {
 
     private final SocketAddress remoteAddress;
     private final Object message;
@@ -33,6 +36,45 @@ public class NetworkMessage {
 
     public Object getMessage() {
         return message;
+    }
+
+    @Override
+    public int refCnt() {
+        return message instanceof ReferenceCounted resource ? resource.refCnt() : 1;
+    }
+
+    @Override
+    public NetworkMessage retain() {
+        ReferenceCountUtil.retain(message);
+        return this;
+    }
+
+    @Override
+    public NetworkMessage retain(int increment) {
+        ReferenceCountUtil.retain(message, increment);
+        return this;
+    }
+
+    @Override
+    public NetworkMessage touch() {
+        ReferenceCountUtil.touch(message);
+        return this;
+    }
+
+    @Override
+    public NetworkMessage touch(Object hint) {
+        ReferenceCountUtil.touch(message, hint);
+        return this;
+    }
+
+    @Override
+    public boolean release() {
+        return ReferenceCountUtil.release(message);
+    }
+
+    @Override
+    public boolean release(int decrement) {
+        return ReferenceCountUtil.release(message, decrement);
     }
 
 }
